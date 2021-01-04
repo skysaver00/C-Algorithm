9543a4f47df8a95595f4105c306d399d1b1deee9
skysaver00 <skysaver00@naver.com>
2021-01-04 17:19:26 +0900

    solved

--- 5635 Birthday/main.cpp
+++ 5635 Birthday/main.cpp
@@ -20,27 +20,33 @@ int main() {
     struct profile oldest;
     struct profile youngest;
 
-    oldest.year = -9999, oldest.month = -99, oldest.day = -99;
-    youngest.year = 9999, youngest.month = 99, youngest.day = 99;
+    oldest.year = 9999, oldest.month = 99, oldest.day = 99;
+    youngest.year = -9999, youngest.month = -99, youngest.day = -99;
 
     for(int i = 0; i < t; i++) {
-        if(oldest.year < birthday[i].year) {
-            if(oldest.month < birthday[i].month) {
-                if(oldest.day < birthday[i].day) {
-                    memcpy(&oldest, &birthday[i], sizeof(struct profile));
-                }
+        if(oldest.year > birthday[i].year) {
+            memcpy(&oldest, &birthday[i], sizeof(struct profile));
+        }
+        else if(oldest.year == birthday[i].year) {
+            if(oldest.month > birthday[i].month) {
+                memcpy(&oldest, &birthday[i], sizeof(struct profile));
+            }
+            else if(oldest.month == birthday[i].month) {
+                if(oldest.day > birthday[i].day) memcpy(&oldest, &birthday[i], sizeof(struct profile));
             }
         }
 
-        if(youngest.year > birthday[i].year) {
-            if(youngest.month > birthday[i].month) {
-                if(youngest.day > birthday[i].day) {
-                    memcpy(&oldest, &birthday[i], sizeof(struct profile));
-                }
+        if(youngest.year < birthday[i].year) {
+            memcpy(&youngest, &birthday[i], sizeof(struct profile));
+        }
+        else if(youngest.year == birthday[i].year) {
+            if(youngest.month < birthday[i].month) {
+                memcpy(&youngest, &birthday[i], sizeof(struct profile));
+            }
+            else if(youngest.month == birthday[i].month) {
+                if(youngest.day > birthday[i].day) memcpy(&youngest, &birthday[i], sizeof(struct profile));
             }
         }
-
-        printf("%s\n%s\n", youngest.name, oldest.name);
     }
 
     printf("%s\n%s\n", youngest.name, oldest.name);
