fe1fa16c2fca2c1cc9670dfbe04b288b68eabef6
skysaver00 <skysaver00@naver.com>
2021-01-04 17:09:27 +0900

    5365

---
+++ 5635 Birthday/cmake-build-debug/CMakeFiles/5635_Birthday.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5635_Birthday.exe"
+  "5635_Birthday.exe.manifest"
+  "5635_Birthday.pdb"
+  "CMakeFiles/5635_Birthday.dir/main.cpp.obj"
+  "lib5635_Birthday.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5635_Birthday.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 5635 Birthday/main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+struct profile {
+    char name[21];
+    short day;
+    short month;
+    short year;
+};
+
+int main() {
+    int t;
+    struct profile birthday[101];
+
+    scanf("%d", &t);
+
+    for(int i = 0; i < t; i++) {
+        scanf("%s %d %d %d", birthday[i].name, &birthday[i].day, &birthday[i].month, &birthday[i].year);
+    }
+    struct profile oldest;
+    struct profile youngest;
+
+    oldest.year = -9999, oldest.month = -99, oldest.day = -99;
+    youngest.year = 9999, youngest.month = 99, youngest.day = 99;
+
+    for(int i = 0; i < t; i++) {
+        if(oldest.year < birthday[i].year) {
+            if(oldest.month < birthday[i].month) {
+                if(oldest.day < birthday[i].day) {
+                    memcpy(&oldest, &birthday[i], sizeof(struct profile));
+                }
+            }
+        }
+
+        if(youngest.year > birthday[i].year) {
+            if(youngest.month > birthday[i].month) {
+                if(youngest.day > birthday[i].day) {
+                    memcpy(&oldest, &birthday[i], sizeof(struct profile));
+                }
+            }
+        }
+
+        printf("%s\n%s\n", youngest.name, oldest.name);
+    }
+
+    printf("%s\n%s\n", youngest.name, oldest.name);
+    return 0;
+}
