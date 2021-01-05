e48b4e0cf844ce1843908dcde21d3ffd7bbd8574
skysaver00 <skysaver00@naver.com>
2021-01-05 16:06:19 +0900

    solved Silver V 나중에 꼭 이분탐색으로 풀어보기

--- 1789 Sum_of_Numbers/main.cpp
+++ 1789 Sum_of_Numbers/main.cpp
@@ -4,30 +4,16 @@ int main() {
     long long S;
     scanf("%lld", &S);
 
-    long long num = 100000;
-    long long left = 0 , right = 100000;
+    long long dummy = S;
+    int N = 1;
+    while(dummy > 0) {
+        dummy = dummy - N;
 
-    while(left <= right) {
-        long long sum = (right + 1) * (right / 2);
-        long long mid = (left + right) / 2;
-        if (sum > S) right = mid - 1;
-        else left = mid + 1, right = right * 2 + 1;
-
-        printf("%lld %lld\n", sum, S);
-        printf("%lld %lld\n", left, right);
+        if(dummy > 0) N++;
     }
 
-    long long leftsum;
-    long long rightsum;
-
-    if(left % 2 != 0) leftsum = (left + 1) * (left / 2) + ((left / 2) + 1);
-    else leftsum = (left + 1) * (left / 2);
-
-    if(right % 2 != 0) rightsum = (right + 1) * (right / 2) + ((right / 2) + 1);
-    else rightsum = (right + 1) * (right / 2);
-
-    if(S >= leftsum) printf("ll%d\n", left);
-    else printf("%lld\n", right);
+    if(dummy == 0) printf("%d\n", N);
+    else printf("%d\n", N - 1);
 
     return 0;
 }
