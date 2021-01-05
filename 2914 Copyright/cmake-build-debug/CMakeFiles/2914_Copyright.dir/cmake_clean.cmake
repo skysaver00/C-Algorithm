ff411ab1c13bdc1b47fb64c84759e7694fe17dea
skysaver00 <skysaver00@naver.com>
2021-01-05 14:57:29 +0900

    solved Bronze V

---
+++ 2914 Copyright/cmake-build-debug/CMakeFiles/2914_Copyright.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2914_Copyright.exe"
+  "2914_Copyright.exe.manifest"
+  "2914_Copyright.pdb"
+  "CMakeFiles/2914_Copyright.dir/main.cpp.obj"
+  "lib2914_Copyright.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2914_Copyright.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
