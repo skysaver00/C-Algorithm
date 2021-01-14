39078d3a9052a7707509c0cad7d82321ddfc49ed
skysaver00 <skysaver00@naver.com>
2021-01-14 19:08:34 +0900

    solved Bronze V

---
+++ 10171 Cat/cmake-build-debug/CMakeFiles/10171_Cat.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "10171_Cat.exe"
+  "10171_Cat.exe.manifest"
+  "10171_Cat.pdb"
+  "CMakeFiles/10171_Cat.dir/main.cpp.obj"
+  "lib10171_Cat.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10171_Cat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
