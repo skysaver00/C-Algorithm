96b43763b81862d30bfd74dfc5f031ddbfc25b1c
skysaver00 <skysaver00@naver.com>
2021-01-17 20:50:03 +0900

    solved Bronze IV

---
+++ 2752 Ordering_3_Numbers/cmake-build-debug/CMakeFiles/2752_Ordering_3_Numbers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2752_Ordering_3_Numbers.exe"
+  "2752_Ordering_3_Numbers.exe.manifest"
+  "2752_Ordering_3_Numbers.pdb"
+  "CMakeFiles/2752_Ordering_3_Numbers.dir/main.cpp.obj"
+  "lib2752_Ordering_3_Numbers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2752_Ordering_3_Numbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
