366242f50e8ce7fdb0e267229af60a0f82bc366d
skysaver00 <skysaver00@naver.com>
2021-01-29 18:03:31 +0900

    solved Silver V

--- 10158 Ant/main.cpp
+++ 10158 Ant/main.cpp
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-int NE();
-int NW();
-int SE();
-int SW();
-
 int main() {
     int w, h;
     scanf("%d %d", &w, &h);
@@ -12,5 +7,20 @@ int main() {
     int p, q;
     scanf("%d %d", &p, &q);
 
+    int t;
+    scanf("%d", &t);
+
+    int x = (p + t) / w;
+    int y = (q + t) / h;
+
+    int _x;
+    int _y;
+
+    if (x % 2 == 0) _x = (p + t) % w;
+    else _x = w - (p + t) % w;
+    if (y % 2 == 0) _y = (q+t) % h;
+    else _y = h - (q + t) % h;
+
+    printf("%d %d\n", _x, _y);
     return 0;
 }
