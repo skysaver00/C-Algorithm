6b9b443d31ff3565f788488895aa3dfb67f0181b
skysaver00 <skysaver00@naver.com>
2021-01-29 18:22:37 +0900

    solved Silver IV

---
+++ 10826 Fibonacci_4/cmake-build-debug/CMakeFiles/10826_Fibonacci_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "10826_Fibonacci_4.exe"
+  "10826_Fibonacci_4.exe.manifest"
+  "10826_Fibonacci_4.pdb"
+  "CMakeFiles/10826_Fibonacci_4.dir/main.cpp.obj"
+  "lib10826_Fibonacci_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10826_Fibonacci_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
