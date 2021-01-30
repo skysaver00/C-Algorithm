adf4067a665817c6f39716ce611508f211333b20
skysaver00 <skysaver00@naver.com>
2021-01-30 23:36:13 +0900

    solved Bronze V

---
+++ 2238 Long_Digit_Calculation/cmake-build-debug/CMakeFiles/2238_Long_Digit_Calculation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2238_Long_Digit_Calculation.exe"
+  "2238_Long_Digit_Calculation.exe.manifest"
+  "2238_Long_Digit_Calculation.pdb"
+  "CMakeFiles/2238_Long_Digit_Calculation.dir/main.cpp.obj"
+  "lib2238_Long_Digit_Calculation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2238_Long_Digit_Calculation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2238 Long_Digit_Calculation/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    /*이 문제는 파이썬으로 쉽게 풀 수있다.
+     * C언어의 경우에는 풀기가 쉽지 않다.
+     * 풀이 코드
+     * a = int(input())
+     * b = int(input())
+     * print(a + b)
+     * print(a - b)
+     * print(a * b)*/
+}
