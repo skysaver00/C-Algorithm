1d0ef79b6c5e1ef27ea232592d112b115b546cbd
skysaver00 <skysaver00@naver.com>
2021-01-30 23:39:51 +0900

    solved Bronze V

---
+++ 3003 King_Queen_Look_Chesspiece/cmake-build-debug/CMakeFiles/3003_King_Queen_Look_Chesspiece.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3003_King_Queen_Look_Chesspiece.exe"
+  "3003_King_Queen_Look_Chesspiece.exe.manifest"
+  "3003_King_Queen_Look_Chesspiece.pdb"
+  "CMakeFiles/3003_King_Queen_Look_Chesspiece.dir/main.cpp.obj"
+  "lib3003_King_Queen_Look_Chesspiece.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3003_King_Queen_Look_Chesspiece.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3003 King_Queen_Look_Chesspiece/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int k, q, l, b, kn, p;
+    scanf("%d %d %d %d %d %d", &k, &q, &l, &b, &kn, &p);
+
+    printf("%d %d %d %d %d %d\n", 1 - k, 1 - q, 2 - l, 2 - b, 2 - kn, 8 - p);
+}
