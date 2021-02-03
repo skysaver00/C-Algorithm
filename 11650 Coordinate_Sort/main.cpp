fe59fcdbd51d85909e3162e2ed159463f7bf05e6
skysaver00 <skysaver00@naver.com>
2021-02-03 23:57:14 +0900

    solved Silver V

--- 11650 Coordinate_Sort/main.cpp
+++ 11650 Coordinate_Sort/main.cpp
@@ -1,99 +1,29 @@
 #include <stdio.h>
+#include <algorithm>
+using namespace std;
+struct coordinate {
+    int x;
+    int y;
+};
 
-int arr[100001];
-int arr2[100001];
-
-void QuickSort(int start, int end) {
-    int pivot = arr[start];
-    int left = start + 1;
-    int right = end;
-
-    while(left <= right) {
-        while(arr[left] <= pivot) left++;
-        while(arr[right] > pivot) right--;
-
-        if(left <= right) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-
-            temp = arr2[left];
-            arr2[left] = arr2[right];
-            arr2[right] = temp;
-        }
+bool cmp(coordinate a, coordinate b) {
+    if(a.x < b.x) return true;
+    else if(a.x == b.x) {
+        if(a.y < b.y) return true;
+        else return false;
     }
-
-    if(start < end) {
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-
-        temp = arr2[left];
-        arr2[left] = arr2[right];
-        arr2[right] = temp;
-
-        QuickSort(start, right - 1);
-        QuickSort(right + 1, end);
-    }
-
-    return;
+    else return false;
 }
 
-void QuickSort_2(int start, int end) {
-    int pivot = arr2[start];
-    int left = start + 1;
-    int right = end;
-
-    while(left <= right) {
-        while(arr2[left] <= pivot) left++;
-        while(arr2[right] > pivot) right--;
-
-        if(left <= right) {
-            int temp = arr2[left];
-            arr2[left] = arr2[right];
-            arr2[right] = temp;
-        }
-    }
-
-    if(start < end) {
-        int temp = arr2[start];
-        arr2[start] = arr2[right];
-        arr2[right] = temp;
-
-        QuickSort(start, right - 1);
-        QuickSort(right + 1, end);
-    }
-
-    return;
-}
-
+coordinate x_y[100001];
 int main() {
     int N;
     scanf("%d", &N);
 
-    for(int i = 0; i < N; i++) {
-        scanf("%d %d", &arr[i], &arr2[i]);
-    }
+    for(int i = 0; i < N; i++) scanf("%d %d", &x_y[i].x, &x_y[i].y);
 
-    QuickSort(0, N - 1);
+    sort(x_y, x_y + N, cmp);
 
-    int x = arr2[0];
-    int left = 0;
-    int right = 0;
-    for(int i = 1; i < N; i++) {
-        if(x == arr2[i]) {
-            right = i;
-        }
-        else {
-            QuickSort_2(left, right - 1);
-            left = right;
-            right = i + 1;
-        }
-    }
-
-    for(int i = 0; i < N; i++) {
-        printf("%d %d\n", arr[i], arr2[i]);
-    }
-
+    for(int i = 0; i < N; i++) printf("%d %d\n", x_y[i].x, x_y[i].y);
     return 0;
 }
