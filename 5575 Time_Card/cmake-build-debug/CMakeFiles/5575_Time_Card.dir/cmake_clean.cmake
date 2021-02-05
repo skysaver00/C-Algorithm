a179d1c18fe797a73045cb35ff6730867cafa7bb
skysaver00 <skysaver00@naver.com>
2021-02-05 16:01:42 +0900

    2021-02-05

---
+++ 5575 Time_Card/cmake-build-debug/CMakeFiles/5575_Time_Card.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5575_Time_Card.exe"
+  "5575_Time_Card.exe.manifest"
+  "5575_Time_Card.pdb"
+  "CMakeFiles/5575_Time_Card.dir/main.cpp.obj"
+  "lib5575_Time_Card.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5575_Time_Card.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 5575 Time_Card/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int arr[6];
+void workedhours();
+
+int main() {
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 6; j++) {
+            scanf("%d", &arr[j]);
+        }
+
+        workedhours();
+    }
+
+    return 0;
+}
+
+void workedhours() {
+
+    int s = arr[5] - arr[2];
+    if(s < 0) {
+        s += 60;
+        arr[4]--;
+    }
+    int m = arr[4] - arr[1];
+    if(m < 0) {
+        m += 60;
+        arr[3]--;
+    }
+    int h = arr[3] - arr[0];
+
+    printf("%d %d %d\n", h, m, s);
+
+    return;
+}
