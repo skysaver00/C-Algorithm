e0c18856df3e13a5a843553f22bc8df3908e468a
skysaver00 <skysaver00@naver.com>
2021-02-07 18:13:52 +0900

    solved Silver V 이 문제는 벡터를 이용, 참고자료를 이용해서 풀었다. 다시 풀어봐야 한다.

--- 1181 Word_Sort/main.cpp
+++ 1181 Word_Sort/main.cpp
@@ -1,66 +1,32 @@
 #include <stdio.h>
-#include <string>
-#include <string.h>
+#include <iostream>
+#include <vector>
 #include <algorithm>
-
 using namespace std;
 
-bool cmp (char *s1, char *s2) {
-    return strcmp(s1, s2) < 0;
+bool cmp(string a, string b) {
+    if(a.size() == b.size()) return a < b;
+    else return a.size() < b.size();
 }
 
-struct dict {
-    char word[51];
-};
-
-int range[51];
-bool T_F[20001];
 int main() {
-    struct dict dict[20001];
+    vector <string> a;
+    string temp;
 
-    int t;
-    scanf("%d", &t);
+    int N;
+    scanf("%d", &N);
 
-    for(int i = 0; i < t; i++) {
-        scanf("%s", dict[i].word);
+    for(int i = 0; i < N; i++) {
+        string word;
+        cin >> word;
+        a.push_back(word);
     }
 
-    char temp[51];
-    int len = 0;
+    sort(a.begin(), a.end(), cmp);
 
-    for(int i = 1; i <= 50; i++) {
-        for(int j = 0; j < t; j++) {
-            if(strlen(dict[j].word) == i) {
-                strcpy(temp, dict[len].word);
-                strcpy(dict[len].word, dict[j].word);
-                strcpy(dict[j].word, temp);
-
-                len++;
-            }
-
-            range[i] = len;
-        }
+    for(int i = 0; i < N; i++) {
+        if(temp == a[i]) continue;
+        cout << a[i] << "\n";
+        temp = a[i];
     }
-
-    char *s[20001];
-    for(int i = 0; i < t; i++) {
-        strcpy(s[i], dict[i].word);
-    }
-
-
-    for(int i = 0; i < 51; i++) {
-        for(int j = range[i]; j < range[i + 1]; j++) {
-            sort(s[range[i]], s[range[i]], cmp);
-        }
-    }
-
-    for(int i = 0; i < t - 1; i++) {
-        if(strcmp(dict[i].word, dict[i + 1].word) == 0) T_F[i + 1] = true;
-    }
-
-    for(int i = 0; i < t; i++) {
-        if(T_F[i] != true) printf("%s\n", dict[i].word);
-    }
-
-    return 0;
 }
