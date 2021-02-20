c4f3c02c270402fecb2cd8acb7480acdfe8d1984
skysaver00 <skysaver00@naver.com>
2021-02-20 17:46:55 +0900

    not Solved

---
+++ ALGO_PICNIC_easy/cmake-build-debug/CMakeFiles/ALGO_PICNIC_easy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ALGO_PICNIC_easy.exe"
+  "ALGO_PICNIC_easy.exe.manifest"
+  "ALGO_PICNIC_easy.pdb"
+  "CMakeFiles/ALGO_PICNIC_easy.dir/main.cpp.obj"
+  "libALGO_PICNIC_easy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ALGO_PICNIC_easy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ALGO_PICNIC_easy/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int friends[11][11];
+
+int arefriends(int a, int b) {
+    
+}
+
+int main() {
+    int C;
+    scanf("%d", &C);
+
+    while(C--) {
+        int n, m;
+        scanf("%d %d", &n, &m);
+        for(int i = 0; i < m; i++) {
+            int a, b;
+            scanf("%d %d", &a, &b);
+        }
+    }
+}
