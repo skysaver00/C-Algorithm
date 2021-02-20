7453098fa2f6bfac355e9dc7daad05bdf963fd96
skysaver00 <skysaver00@naver.com>
2021-02-20 18:45:02 +0900

    solved Gild IV

--- 17298_Oken_Number/main.cpp
+++ 17298_Oken_Number/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int arr[1000001];
+int ans[1000001];
 
 int main() {
     int N;
@@ -13,27 +14,16 @@ int main() {
     }
 
     stack<int> s;
-
-    s.push(-1);
-    int max = arr[N - 1];
-    for(int i = N - 2; i >= 0; i--) {
-        if(arr[i] > s.top()) {
-            if(arr[i] > max) {
-                s.push(-1);
-                max = arr[i];
-            }
-            else s.push(max);
-        }
-        else {
-            if (arr[i] < arr[i + 1]) {
-                s.push(arr[i + 1]);
-            } else s.push(max);
-        }
+    s.push(1e7 + 1);
+    for(int i = N - 1; i >= 0; i--) {
+        while(s.top() <= arr[i]) s.pop();
+        if(s.top() > 1e7) ans[i] = -1;
+        else ans[i] = s.top();
+        s.push(arr[i]);
     }
 
-    while(!s.empty()) {
-        printf("%d ", s.top());
-        s.pop();
+    for(int i = 0; i < N; i++) {
+        printf("%d ", ans[i]);
     }
     printf("\n");
 
