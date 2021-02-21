808224118f2c074aeec8310d4e17bf57dea8644e
skysaver00 <skysaver00@naver.com>
2021-02-21 23:09:07 +0900

    2021-02-21

---
+++ 18108_1998_2541/cmake-build-debug/CMakeFiles/18108_1998_2541.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "18108_1998_2541.exe"
+  "18108_1998_2541.exe.manifest"
+  "18108_1998_2541.pdb"
+  "CMakeFiles/18108_1998_2541.dir/main.cpp.obj"
+  "lib18108_1998_2541.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/18108_1998_2541.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
