a1ee6b7b3cd4640705c5afbb89330a5628006d0f
skysaver00 <skysaver00@naver.com>
2021-02-24 23:50:41 +0900

    solved Bronze V

---
+++ 5337_Welcome/cmake-build-debug/CMakeFiles/5337_Welcome.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5337_Welcome.exe"
+  "5337_Welcome.exe.manifest"
+  "5337_Welcome.pdb"
+  "CMakeFiles/5337_Welcome.dir/main.cpp.obj"
+  "lib5337_Welcome.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5337_Welcome.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
