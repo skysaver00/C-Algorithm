2b5a2d06773d10ad270b9c17aaa706d137b746d6
skysaver00 <skysaver00@naver.com>
2021-03-12 22:04:33 +0900

    not solved

--- 1652_Find_Space_To_Sleep/main.cpp
+++ 1652_Find_Space_To_Sleep/main.cpp
@@ -1,72 +1,57 @@
 #include <stdio.h>
+
 bool arr[101][101];
 
 int main() {
     int N;
     scanf("%d", &N);
 
-    int x_[4] = {1, 0, -1, 0};
-    int y_[4] = {0, 1, 0, -1};
+    int x_[4] = {1, 0};
+    int y_[4] = {0, 1};
 
-    for(int i = 0 ; i < N; i++) {
-        for(int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             char X;
             scanf(" %c", &X);
 
-            if(X == 'X') arr[i][j] = true;
+            if (X == '.') arr[i][j] = true;
         }
     }
     int xmin = -9999;
     int ymin = -9999;
 
-    int cmp1, cmp2, ans;
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            for(int k = 0; k < 4; k++) {
-                cmp1 = j; cmp2 = i;
-                for(int l = i; l < N - i; l++) {
-                    if(arr[cmp2][cmp1] == true) {
-                        ans = cmp1 - j;
-                        if(xmin < ans) xmin = ans;
-                        break;
-                    }
-                    cmp1 += x_[0], cmp2 += y_[0];
+    int cmp1, cmp2, ans = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cmp1 = j;
+            cmp2 = i;
+            for (int l = i; l <= N; l++) {
+                if (arr[cmp2][cmp1] == 0) {
+                    if (xmin < ans) xmin = ans;
+                    break;
                 }
-
-                cmp1 = j; cmp2 = i;
-                for(int l = j; l < N - j; l++) {
-                    if(arr[cmp2][cmp1] == true) {
-                        ans = cmp2 - i;
-                        if(ymin < ans) ymin = ans;
-                        break;
-                    }
-                    cmp1 += x_[1], cmp2 += y_[1];
-                }
-
-                cmp1 = j; cmp2 = i;
-                for(int l = i; l > 0; l--) {
-                    if(arr[cmp2][cmp1] == true) {
-                        ans = cmp1 - j;
-                        if(xmin < ans) xmin = ans;
-                        break;
-                    }
-                    cmp1 += x_[0], cmp2 += y_[0];
-                }
-
-                cmp1 = j; cmp2 = i;
-                for(int l = j; l > 0; l--) {
-                    if(arr[cmp2][cmp1] == true) {
-                        ans = cmp2 - i;
-                        if(ymin < ans) ymin = ans;
-                        break;
-                    }
-                    cmp1 += x_[1], cmp2 += y_[1];
-                }
+                ans++;
+                cmp1 += x_[0], cmp2 += y_[0];
             }
+            printf("%d %d %d\n", i, j, ans);
+            ans = 0;
+            cmp1 = j;
+            cmp2 = i;
+            for (int l = j; l <= N; l++) {
+                if (arr[cmp2][cmp1] == 0) {
+                    if (ymin < ans) ymin = ans;
+                    break;
+                }
+                ans++;
+                cmp1 += x_[1], cmp2 += y_[1];
+            }
+            printf("%d %d %d\n", i, j, ans);
+            ans = 0;
         }
     }
+}
 
-    printf("%d %d\n", xmin, ymin);
+printf("%d %d\n", xmin, ymin);
 
-    return 0;
+return 0;
 }
