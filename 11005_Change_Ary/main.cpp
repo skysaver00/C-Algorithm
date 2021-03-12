e086b8c7bf194db8f9666b93c2e110c65c2abdb0
skysaver00 <skysaver00@naver.com>
2021-03-12 21:28:09 +0900

    solved Bronze I

--- 11005_Change_Ary/main.cpp
+++ 11005_Change_Ary/main.cpp
@@ -1,12 +1,25 @@
 #include <stdio.h>
+char arr[100];
 
 int main() {
     int N, B;
     scanf("%d %d", &N, &B);
 
+    int i = 0;
     while(N) {
-        printf("%d\n", N % 36);
-        N /= 36;
+        int remain = N % B;
+        if(remain >= 10) {
+            arr[i] = remain + 55;
+        } else arr[i] = remain;
+        N /= B;
+        i++;
+    }
+
+    int j = i - 1;
+    while(j >= 0) {
+        if(arr[j] <= 90 && arr[j] >= 65) printf("%c", arr[j]);
+        else printf("%d", arr[j]);
+        j--;
     }
 
     return 0;
