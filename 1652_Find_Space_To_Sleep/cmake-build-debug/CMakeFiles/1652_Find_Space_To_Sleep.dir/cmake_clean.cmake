2f99d13aaa0eb67b97ae2841da2ddd4a2e4047be
skysaver00 <skysaver00@naver.com>
2021-03-12 21:50:35 +0900

    not solved

---
+++ 1652_Find_Space_To_Sleep/cmake-build-debug/CMakeFiles/1652_Find_Space_To_Sleep.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1652_Find_Space_To_Sleep.exe"
+  "1652_Find_Space_To_Sleep.exe.manifest"
+  "1652_Find_Space_To_Sleep.pdb"
+  "CMakeFiles/1652_Find_Space_To_Sleep.dir/main.cpp.obj"
+  "lib1652_Find_Space_To_Sleep.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1652_Find_Space_To_Sleep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
