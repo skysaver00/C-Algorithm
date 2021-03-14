2491fce3196660f2ee60eb7801ec8969ff14d98f
skysaver00 <skysaver00@naver.com>
2021-03-14 21:23:53 +0900

    not solved

--- 2579_Climbing_Stairs/main.cpp
+++ 2579_Climbing_Stairs/main.cpp
@@ -15,9 +15,10 @@ int main() {
     }
 
     dp[0] = arr[0];
-    dp[1] = arr[1];
+    dp[1] = Max(arr[0] + arr[1], arr[1]);
+    dp[2] = Max(arr[0] + arr[2], arr[1] + arr[2]);
 
-    for(int i = 2; i < t; i++) {
+    for(int i = 3; i < t; i++) {
         int max = Max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
         dp[i] = max;
     }
