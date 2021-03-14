4b5c4ec5831aba14569739d7e53a08835eabcbd8
skysaver00 <skysaver00@naver.com>
2021-03-14 21:33:37 +0900

    not solved

---
+++ 14720_Milk_Festival/cmake-build-debug/CMakeFiles/14720_Milk_Festival.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14720_Milk_Festival.exe"
+  "14720_Milk_Festival.exe.manifest"
+  "14720_Milk_Festival.pdb"
+  "CMakeFiles/14720_Milk_Festival.dir/main.cpp.obj"
+  "lib14720_Milk_Festival.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14720_Milk_Festival.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14720_Milk_Festival/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+char store[1001];
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    for(int i = 0; i < t; i++) {
+        scanf(" %c", &store[i]);
+    }
+
+    
+
+    return 0;
+}
