77a5e97d90fbb5acd902a73e516a97a455043c27
skysaver00 <skysaver00@naver.com>
2021-03-14 21:41:37 +0900

    solved Bronze III

--- 14720_Milk_Festival/main.cpp
+++ 14720_Milk_Festival/main.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
 char store[1001];
+int dp[1001][3];
+
+int Max(int a, int b) {
+    return a > b ? a : b;
+}
 
 int main() {
     int t;
@@ -9,7 +14,24 @@ int main() {
         scanf(" %c", &store[i]);
     }
 
-    
+    int check = 0;
+    int cnt = 0;
+    for(int i = 0; i < t; i++) {
+        if(store[i] == '0' && check == 0) {
+            check = 1;
+            cnt++;
+        }
+        if(store[i] == '1' && check == 1) {
+            check = 2;
+            cnt++;
+        }
+        if(store[i] == '2' && check == 2) {
+            check = 0;
+            cnt++;
+        }
+    }
+
+    printf("%d\n", cnt);
 
     return 0;
 }
