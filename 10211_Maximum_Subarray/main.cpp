772bc77140bdf17b1123dc1e42bb59c031c8e99c
skysaver00 <skysaver00@naver.com>
2021-03-15 21:09:59 +0900

    solved Silver III

--- 10211_Maximum_Subarray/main.cpp
+++ 10211_Maximum_Subarray/main.cpp
@@ -1,37 +1,34 @@
 #include <stdio.h>
 int arr[1001];
+int dp[1001];
+
+int max(int a, int b){
+    return a > b ? a : b;
+}
 
 int main() {
-    int t;
-    scanf("%d", &t);
+    int n;
+    scanf("%d", &n);
 
-    for(int i = 0; i < t; i++) {
-        int n;
-        scanf("%d", &n);
-        for(int j = 0; j < n; j++) {
+    for(int i = 0; i < n; i++) {
+        int t;
+        scanf("%d", &t);
+
+        for(int j = 0; j < t; j++) {
             scanf("%d", &arr[j]);
-            if(j != 0) arr[j] = arr[j] + arr[j - 1];
-            else {}
-        }
-
-        int min = 0;
-        int max = arr[0];
-
-        int flag = 0;
-        for(int i = 0; i < n; i++) {
-            if(max < arr[i]) {
-                flag = i;
-                max = arr[i];
+            if(j == 0) dp[0] = arr[0];
+            else {
+                dp[j] = max(dp[j - 1] + arr[j], arr[j]);
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            if(min > arr[i]) {
-                if(i < flag) min = arr[i];
-            } else {}
+        int max = dp[0];
+        for(int j = 0; j < t; j++) {
+            if(max < dp[j]) max = dp[j];
         }
 
-        printf("%d\n", max - min);
+        printf("%d\n", max);
     }
+
     return 0;
 }
