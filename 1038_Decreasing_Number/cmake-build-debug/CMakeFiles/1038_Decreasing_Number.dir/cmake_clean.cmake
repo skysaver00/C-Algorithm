ae4187c8640c30d31036e1f19a79a197ac64163a
skysaver00 <skysaver00@naver.com>
2021-03-20 21:24:00 +0900

    not solved

---
+++ 1038_Decreasing_Number/cmake-build-debug/CMakeFiles/1038_Decreasing_Number.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1038_Decreasing_Number.exe"
+  "1038_Decreasing_Number.exe.manifest"
+  "1038_Decreasing_Number.pdb"
+  "CMakeFiles/1038_Decreasing_Number.dir/main.cpp.obj"
+  "lib1038_Decreasing_Number.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1038_Decreasing_Number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
