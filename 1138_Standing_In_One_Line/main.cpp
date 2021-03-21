e03896a66f8cc8e76bc9e615793f9f4b4ed0302a
skysaver00 <skysaver00@naver.com>
2021-03-21 21:37:56 +0900

    not solved

--- 1138_Standing_In_One_Line/main.cpp
+++ 1138_Standing_In_One_Line/main.cpp
@@ -5,6 +5,7 @@ struct person {
     int right;
 };
 
+int stand[10];
 int main() {
     int N;
     scanf("%d", &N);
@@ -12,16 +13,29 @@ int main() {
     int num;
     struct person person[10];
     for(int i = 0; i < N; i++) {
-        scanf("%d\n", &num);
+        scanf("%d", &num);
 
         person[i].number = i + 1;
         person[i].left = num;
-        person[i].right = N - num - i;
+        person[i].right = N - num - i - 1;
     }
 
     for(int i = 0; i < N; i++) {
         printf("%d %d %d\n", person[i].number, person[i].left, person[i].right);
     }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            if(person[i].left == 0 && stand[j] == 0) {
+                stand[j] = i + 1;
+                break;
+            } else if(stand[j] == 0) person[i].left--;
+        }
+    }
+
+    for(int i = 0; i < N; i++) {
+        printf("%d ", stand[i]);
+    } printf("\n");
 
     return 0;
 }
