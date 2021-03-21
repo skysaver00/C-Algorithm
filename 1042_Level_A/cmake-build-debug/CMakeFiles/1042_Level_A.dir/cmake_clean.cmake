2c44339b6fa2dce1281b30f700ee5d4678fa9969
skysaver00 <skysaver00@naver.com>
2021-03-21 22:25:08 +0900

    solved Bronze I 이문제는 O(N)으로 푸는 문제가 아니다

---
+++ 1042_Level_A/cmake-build-debug/CMakeFiles/1042_Level_A.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1042_Level_A.exe"
+  "1042_Level_A.exe.manifest"
+  "1042_Level_A.pdb"
+  "CMakeFiles/1042_Level_A.dir/main.cpp.obj"
+  "lib1042_Level_A.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1042_Level_A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1042_Level_A/main.cpp
@@ -0,0 +1,31 @@
+/*#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << "yes\n";
+    }
+
+    return 0;
+}
+*/
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while(t--) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        printf("yes\n");
+    }
+}
