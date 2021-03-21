5580afc662cbfdb26581524981eb1b2e1b62dcba
skysaver00 <skysaver00@naver.com>
2021-03-21 20:39:49 +0900

    solved Bronze II

---
+++ 2743_Word_Length_Measure/cmake-build-debug/CMakeFiles/2743_Word_Length_Measure.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2743_Word_Length_Measure.exe"
+  "2743_Word_Length_Measure.exe.manifest"
+  "2743_Word_Length_Measure.pdb"
+  "CMakeFiles/2743_Word_Length_Measure.dir/main.cpp.obj"
+  "lib2743_Word_Length_Measure.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2743_Word_Length_Measure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2743_Word_Length_Measure/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;
+
+    cout << word.length() << "\n";
+
+    return 0;
+}
