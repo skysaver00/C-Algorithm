467feefc2a37130f445bca4ba52557fe95990029
skysaver00 <skysaver00@naver.com>
2021-03-22 21:30:11 +0900

    solved Gold II

--- 1655_Shout_Median/main.cpp
+++ 1655_Shout_Median/main.cpp
@@ -1,39 +1,52 @@
-#include <iostream>
+#include<stdio.h>
 #include <queue>
 
 using namespace std;
 
 int main() {
     int N;
-    cin >> N;
+    scanf("%d", &N);
 
     priority_queue<int> bigHeap;
     priority_queue<int, vector<int>, greater<int>> smallHeap;
 
     int median;
+
+    bigHeap.push(-99999999);
+    smallHeap.push(99999999);
     for(int i = 0; i < N; i++) {
         int number;
-        cin >> number;
+        scanf("%d", &number);
 
         if(i == 0) median = number;
 
-        cout << median << "\n";
+        if(i != 0) {
+            if (median >= number) bigHeap.push(number);
+            else smallHeap.push(number);
+        }
+        if(i % 2 == 0 && i != 0) {
 
-        if(median >= number) bigHeap.push(number);
-        else smallHeap.push(number);
-        if(i % 2 == 0) {
-            cout << smallHeap.size() << " " << bigHeap.size() << "\n";
-            if(bigHeap.size() <= smallHeap.size()) {
+            if(bigHeap.size() < smallHeap.size()) {
                 bigHeap.push(median);
+                median = smallHeap.top();
+                smallHeap.pop();
+            } else if(bigHeap.size() > smallHeap.size()) {
+                smallHeap.push(median);
                 median = bigHeap.top();
                 bigHeap.pop();
             } else {
-                smallHeap.push(median);
-                median = smallHeap.top();
-                smallHeap.pop();
-            }
 
-            cout << smallHeap.top() << " " << bigHeap.top() << "\n";
+            }
+        } else if(i % 2 != 0) {
+            if(bigHeap.size() > smallHeap.size()) {
+                if(bigHeap.top() < median) {
+                    smallHeap.push(median);
+                    median = bigHeap.top();
+                    bigHeap.pop();
+                }
+            }
         }
+        printf("%d\n", median);
     }
+    return 0;
 }
