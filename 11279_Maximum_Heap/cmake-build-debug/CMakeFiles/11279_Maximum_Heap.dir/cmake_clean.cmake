e89bd2d43a0317d89fd98f482d3b232930d019ba
skysaver00 <skysaver00@naver.com>
2021-03-22 19:39:30 +0900

    solved Silver II

---
+++ 11279_Maximum_Heap/cmake-build-debug/CMakeFiles/11279_Maximum_Heap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11279_Maximum_Heap.exe"
+  "11279_Maximum_Heap.exe.manifest"
+  "11279_Maximum_Heap.pdb"
+  "CMakeFiles/11279_Maximum_Heap.dir/main.cpp.obj"
+  "lib11279_Maximum_Heap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11279_Maximum_Heap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11279_Maximum_Heap/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    priority_queue<int> queue;
+    while(N--) {
+        int x;
+        scanf("%d", &x);
+
+        if(x == 0) {
+            if(queue.empty()) printf("0\n");
+            else {
+                printf("%d\n", queue.top());
+                queue.pop();
+            }
+        } else {
+            queue.push(x);
+        }
+    }
+
+    return 0;
+}
