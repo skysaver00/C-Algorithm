6d150084353d9ae01df48089196138d063add91b
skysaver00 <skysaver00@naver.com>
2021-03-22 22:17:19 +0900

    not solved

---
+++ 11286_Abs_Heap/cmake-build-debug/CMakeFiles/11286_Abs_Heap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11286_Abs_Heap.exe"
+  "11286_Abs_Heap.exe.manifest"
+  "11286_Abs_Heap.pdb"
+  "CMakeFiles/11286_Abs_Heap.dir/main.cpp.obj"
+  "lib11286_Abs_Heap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11286_Abs_Heap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
