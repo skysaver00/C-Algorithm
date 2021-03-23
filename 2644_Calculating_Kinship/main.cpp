c4d728c11fa2a45b17faa9de43e1bfa0ba6bcf31
skysaver00 <skysaver00@naver.com>
2021-03-23 21:07:41 +0900

    not solved

--- 2644_Calculating_Kinship/main.cpp
+++ 2644_Calculating_Kinship/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <stdlib.h>
 
 using namespace std;
 
 int degree = 0;
 vector<int> dfs[101];
+queue<int> bfs;
 int check[101];
 
 void startDFS(int start, int end) {
@@ -21,11 +23,11 @@ void startDFS(int start, int end) {
         degree--;
         return;
     } check[start] = 1;
+    printf("checked the value %d\n", start);
 
     if(degree != 0 && dfs[start].size() == 1) {
         printf("size 0: %d %d %d\n", start, end, degree);
         degree--;
-        return;
     }
 
     for(int i = 0; i < dfs[start].size(); i++) {
@@ -35,6 +37,43 @@ void startDFS(int start, int end) {
         else degree--;
     }
 }
+
+void startBFS(int start, int end) {
+    if(start == end) {
+        cout << degree << "\n";
+        exit(0);
+    }
+    bfs.push(start);
+    check[start] = 1;
+
+    while(!bfs.empty()) {
+        int x = bfs.front();
+        bfs.pop();
+        printf("mother is %d ", x);
+
+        if(x == end) {
+            cout << degree << "\n";
+            exit(0);
+        }
+
+
+        if(dfs[x].size() == 1) {
+            degree--;
+        }
+
+        degree++;
+        for(int i = 0; i < dfs[x].size(); i++) {
+            int y = dfs[x][i];
+
+            if(check[y] == 0) {
+                bfs.push(y);
+                check[y] = 1;
+            }
+            printf("%d %d\n", y, degree);
+        }
+    }
+}
+
 
 int main() {
     int n;
