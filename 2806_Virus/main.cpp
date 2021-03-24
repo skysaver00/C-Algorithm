6a54766ef1359a791d60335e64af0163df236a24
skysaver00 <skysaver00@naver.com>
2021-03-24 18:13:39 +0900

    solved Silver III BFS로도 품

--- 2806_Virus/main.cpp
+++ 2806_Virus/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -20,6 +21,27 @@ void startDFS(int s) {
     }
 }
 
+queue<int> bfs;
+void startBFS(int s) {
+    bfs.push(s);
+    check[s] = 1;
+
+    while(!bfs.empty()) {
+        int x = bfs.front();
+        bfs.pop();
+
+        for(int i = 0; i < graph[x].size(); i++) {
+            int y = graph[x][i];
+
+            if(check[y] != 1) {
+                bfs.push(y);
+                check[y] = 1;
+                cnt[y] = 1;
+            }
+        }
+    }
+}
+
 int main() {
     int N;
     cin >> N;
@@ -35,7 +57,8 @@ int main() {
         graph[to].push_back(from);
     }
 
-    startDFS(1);
+    //startDFS(1);
+    startBFS(1);
 
     int count = 0;
     for(int i = 1; i <= N; i++) if(cnt[i] == 1) count++;
