c9e98f340aad77caa29811a7c1a94403ba464334
skysaver00 <skysaver00@naver.com>
2021-03-25 17:21:15 +0900

    solved Gold V 꼭 다시 한번 살펴볼 것! 시간 제한에 대해 고민해볼 만한 문제

--- 9019_DSLR/main.cpp
+++ 9019_DSLR/main.cpp
@@ -1,124 +1,64 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <cstring>
 #include <string>
 
 using namespace std;
-queue<pair<int, string>> bfs;
-int A, B;
-
-void ClearQueue(queue<pair<int, string>> &queue) {
-    std::queue<pair<int, string>> empty;
-    std::swap(queue, empty);
-}
-
-int startBFS(int num) {
-    int numAlt = (num * 2) % 10000;
-    if(numAlt == B) {
-        cout << "D" << "\n";
-        return 0;
-    } else {
-        bfs.push(make_pair(numAlt, "D"));
-    }
-
-
-    numAlt = num - 1;
-    if(num == 0) numAlt = 9999;
-    if(numAlt == B) {
-        cout << "S" << "\n";
-        return 0;
-    } else {
-        bfs.push(make_pair(numAlt, "S"));
-    }
-
-    numAlt = num % 1000;
-    numAlt *= 10;
-    numAlt += num / 1000;
-    if(numAlt == B) {
-        cout << "L" << "\n";
-        return 0;
-    } else {
-        bfs.push(make_pair(numAlt, "L"));
-    }
-
-    numAlt = num / 10;
-    numAlt += (num % 10) * 1000;
-    if(numAlt == B) {
-        cout << "R" << "\n";
-        return 0;
-    } else {
-        bfs.push(make_pair(numAlt, "R"));
-    }
-
-    while(!bfs.empty()) {
-        int number = bfs.front().first;
-        string ins = bfs.front().second;
-        bfs.pop();
-
-        for(int i = 0; i < 4; i++) {
-            int altNumber;
-            if(i == 0) {
-                string newIns = ins + "D";
-                altNumber = (number * 2) % 10000;
-
-                if(altNumber == B) {
-                    cout << newIns << "\n";
-                    return 0;
-                } else {
-                    bfs.push(make_pair(altNumber, newIns));
-                }
-            }
-            else if(i == 1) {
-                string newIns = ins + "S";
-                altNumber = number - 1;
-                if(number == 0) altNumber = 9999;
-
-                if(altNumber == B) {
-                    cout << newIns << "\n";
-                    return 0;
-                } else {
-                    bfs.push(make_pair(altNumber, newIns));
-                }
-            }
-            else if(i == 2) {
-                string newIns = ins + "L";
-                altNumber = number % 1000;
-                altNumber *= 10;
-                altNumber += number / 1000;
-
-                if(altNumber == B) {
-                    cout << newIns << "\n";
-                    return 0;
-                } else {
-                    bfs.push(make_pair(altNumber, newIns));
-                }
-            }
-            else {
-                string newIns = ins + "R";
-                altNumber = number / 10;
-                altNumber += (number % 10) * 1000;
-
-                if(altNumber == B) {
-                    cout << newIns << "\n";
-                    return 0;
-                } else {
-                    bfs.push(make_pair(altNumber, newIns));
-                }
-            }
-        }
-    }
-}
 
 int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int T;
     cin >> T;
 
     while(T--) {
+        int A, B;
         cin >> A >> B;
 
-        startBFS(A);
+        queue<pair<int, string>> bfs;
+        bool visit[10000] = {false};
+        visit[A] = true;
 
-        ClearQueue(bfs);
+        bfs.push(make_pair(A, ""));
+        while(!bfs.empty()) {
+            int num = bfs.front().first;
+            string ins = bfs.front().second;
+            bfs.pop();
+
+            if(num == B) {
+                cout << ins;
+                break;
+            }
+
+            int numAlt;
+            numAlt = (num * 2) % 10000; //D
+            if(!visit[numAlt]) {
+                bfs.push(make_pair(numAlt, ins + "D"));
+                visit[numAlt] = true;
+            }
+
+            numAlt = num - 1; //S
+            if(num == 0) numAlt = 9999;
+            if(!visit[numAlt]) {
+                bfs.push(make_pair(numAlt, ins + "S"));
+                visit[numAlt] = true;
+            }
+
+            numAlt = (num % 1000) * 10 + num / 1000;
+            if(!visit[numAlt]) {
+                bfs.push(make_pair(numAlt, ins + "L"));
+                visit[numAlt] = true;
+            }
+
+            numAlt = (num / 10) + (num % 10) * 1000;
+            if(!visit[numAlt]) {
+                bfs.push(make_pair(numAlt, ins + "R"));
+                visit[numAlt] = true;
+            }
+        }
+
+        cout << "\n";
     }
 }
