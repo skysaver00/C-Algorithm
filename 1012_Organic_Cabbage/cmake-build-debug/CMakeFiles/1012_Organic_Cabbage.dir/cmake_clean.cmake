9751da965c9990754b4fa6df10c22c0f510d3232
skysaver00 <skysaver00@naver.com>
2021-03-26 23:57:53 +0900

    Recap solved Silver II

---
+++ 1012_Organic_Cabbage/cmake-build-debug/CMakeFiles/1012_Organic_Cabbage.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1012_Organic_Cabbage.exe"
+  "1012_Organic_Cabbage.exe.manifest"
+  "1012_Organic_Cabbage.pdb"
+  "CMakeFiles/1012_Organic_Cabbage.dir/main.cpp.obj"
+  "lib1012_Organic_Cabbage.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1012_Organic_Cabbage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
