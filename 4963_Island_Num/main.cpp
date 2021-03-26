3778ce144e12a02146857d459ffba38b14aaaf16
skysaver00 <skysaver00@naver.com>
2021-03-27 00:06:43 +0900

    solved Silver II

--- 4963_Island_Num/main.cpp
+++ 4963_Island_Num/main.cpp
@@ -1,51 +1,48 @@
 #include <stdio.h>
 #include <memory.h>
 
-using namespace std;
 int land[51][51];
 int cnt = 1;
 bool flag = false;
 int check[51][51];
 int m, n, k;
 
-int _x[4] = {1, 0, -1, 0};
-int _y[4] = {0, 1, 0, -1};
+int _x[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+int _y[8] = {0, 1, 0, -1, 1, -1, 1, -1};
 
 void startDFS(int y, int x) {
-    if(check[y][x] != 0) return;
-    if(land[y][x] == 0) return;
+    if (check[y][x] != 0) return;
+    if (land[y][x] == 0) return;
     flag = true;
     check[y][x] = cnt;
 
-    for(int i = 0; i < 4; i++) {
+    for (int i = 0; i < 8; i++) {
         int newY = y + _y[i];
         int newX = x + _x[i];
 
-        if(check[newY][newX] == 0 && land[newY][newX] != 0 && newY < n && newY >= 0 && newX < m && newX >= 0) {
+        if (check[newY][newX] == 0 && land[newY][newX] != 0 && newY < n && newY >= 0 && newX < m && newX >= 0) {
             startDFS(newY, newX);
         }
     }
 }
 
 int main() {
-    int t;
-    scanf("%d", &t);
+    while(1) {
+        scanf("%d %d", &m, &n);
 
-    while(t--) {
-        scanf("%d %d %d", &m, &n, &k);
+        if (m == 0 && n == 0) return 0;
 
-        for(int i = 0; i < k; i++) {
-            int x, y;
-            scanf("%d %d", &x, &y);
-
-            land[y][x] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d", &land[i][j]);
+            }
         }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 startDFS(i, j);
 
-                if(flag) {
+                if (flag) {
                     cnt++;
                     flag = false;
                 }
@@ -53,20 +50,18 @@ int main() {
         }
 
         int max = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(check[i][j] > max) max = check[i][j];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (check[i][j] > max) max = check[i][j];
             }
         }
 
         printf("%d\n", max);
 
-        for(int i = 0; i < 50; i++) {
+        for (int i = 0; i < 50; i++) {
             memset(land[i], 0, sizeof(int) * 50);
             memset(check[i], 0, sizeof(int) * 50);
         }
         cnt = 1;
     }
-    
-    return 0;
 }
