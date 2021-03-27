0dea2a30b932f18ac80d031add2720336057a4d4
skysaver00 <skysaver00@naver.com>
2021-03-28 02:03:24 +0900

    solved Only 1st Google Code Jam #3

--- 2021_3_Reversort_Engineering/main.cpp
+++ 2021_3_Reversort_Engineering/main.cpp
@@ -2,15 +2,57 @@
 #include <algorithm>
 
 using namespace std;
+int n, c;
+int b[7];
+int cnt = 1;
+bool flag = false;
+
+void findPermutations(int a[], int n) {
+    do {
+        for(int i = 0; i < n; i++) {
+            b[i] = a[i];
+        }
+
+        int sum = 0;
+        for(int i = 0; i < n -1 ; i++) {
+            int min = b[i];
+            int min_arr = -1;
+            for(int j = i; j < n; j++) {
+                if(min > b[j]) {
+                    min = b[j];
+                    min_arr = j;
+                }
+            }
+
+            if(min_arr != -1) {
+                sum += min_arr - i + 1;
+                reverse(b + i, b + min_arr + 1);
+            }
+            else sum++;
+        }
+        if(sum == c) {
+            for (int i = 0; i < n; i++) {
+                printf("%d ", a[i]);
+            }
+            printf("\n");
+            flag = true;
+            return;
+        }
+    } while(next_permutation(a, a + n));
+
+    printf("IMPOSSIBLE\n");
+}
 
 int main() {
     int t;
     scanf("%d", &t);
 
     while(t--) {
-        int n, c;
         scanf("%d %d", &n, &c);
-
-        
+        int a[] = {1, 2, 3, 4, 5, 6, 7};
+        printf("Case #%d: ", cnt++);
+        findPermutations(a, n);
     }
+
+    return 0;
 }
