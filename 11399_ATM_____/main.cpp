a0b887c9de03f01c275e6541583fef2857352491
skysaver00 <skysaver00@naver.com>
2021-03-29 23:15:17 +0900

    Recap solved Silver III

--- 11399_ATM_____/main.cpp
+++ 11399_ATM_____/main.cpp
@@ -12,17 +12,12 @@ int main() {
 
     sort(arr, arr + t);
 
-    for(int i = 0; i < t; i++) {
-        cout << arr[i];
-    }
-
     int sum = 0;
     brr[0] = arr[0];
     sum += brr[0];
     for(int i = 1; i < t; i++) {
-        brr[i] = arr[i] + arr[i - 1];
+        brr[i] = arr[i] + brr[i - 1];
         sum += brr[i];
-        cout << sum << "\n";
     }
 
     cout << sum;
