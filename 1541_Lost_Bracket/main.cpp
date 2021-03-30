fce3bff9c9f421befa6d8f73474bec070a4159af
skysaver00 <skysaver00@naver.com>
2021-03-30 20:52:40 +0900

    solved Silver II

--- 1541_Lost_Bracket/main.cpp
+++ 1541_Lost_Bracket/main.cpp
@@ -4,26 +4,36 @@ using namespace std;
 string str;
 int main() {
     cin >> str;
+    str += '-';
 
     int len = str.length();
 
     int at = 0;
     for(int i = 0; i < len; i++) {
-        if(str.at(i) == '-') at = i;
+        if(str.at(i) == '-') {
+            at = i;
+            break;
+        }
     }
-
-    if(at == 0) at = len - 1;
 
     int sum = 0;
     int sub = 0;
 
     int start = 0;
-    char number[6];
     for(int i = 0; i <= at; i++) {
         if(str.at(i) == '+' || str.at(i) == '-') {
             sum += stoi(str.substr(start, i - start));
             start = i + 1;
-            cout << sum << "\n";
         }
     }
+
+    for(int i = at + 1; i < len; i++) {
+        if(str.at(i) == '+' || str.at(i) == '-') {
+            sub += stoi(str.substr(start, i - start));
+            start = i + 1;
+        }
+    }
+
+    cout << sum - sub << "\n";
+    return 0;
 }
