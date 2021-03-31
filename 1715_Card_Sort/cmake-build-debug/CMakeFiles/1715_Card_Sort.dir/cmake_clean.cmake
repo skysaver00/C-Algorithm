85cdf2c63e586f9e3e510914f589d32a3afd7600
skysaver00 <skysaver00@naver.com>
2021-03-31 22:46:00 +0900

    not solved

---
+++ 1715_Card_Sort/cmake-build-debug/CMakeFiles/1715_Card_Sort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1715_Card_Sort.exe"
+  "1715_Card_Sort.exe.manifest"
+  "1715_Card_Sort.pdb"
+  "CMakeFiles/1715_Card_Sort.dir/main.cpp.obj"
+  "lib1715_Card_Sort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1715_Card_Sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1715_Card_Sort/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    priority_queue<int, vector<int>, greater<int>> cards;
+
+    for(int i = 0; i < n; i++) {
+        int t;
+
+        cin >> t;
+        cards.push(t);
+    }
+
+    long long sum = 0;
+
+    long long preCard = cards.top();
+    if(cards.size() == 1) {
+        cout << preCard << "\n";
+        return 0;
+    }
+    cards.pop();
+    preCard += cards.top();
+    cards.pop();
+
+    sum = preCard;
+
+    int len = cards.size();
+    for(int i = 0; i < len; i++) {
+        int nextCard = preCard + cards.top();
+        cards.pop();
+        sum += nextCard;
+        preCard = nextCard;
+    }
+    cout << sum << "\n";
+
+    return 0;
+}
