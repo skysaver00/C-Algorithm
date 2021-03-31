91109c82af0f913b47100e64b8422ac0769e1a34
skysaver00 <skysaver00@naver.com>
2021-03-31 21:59:05 +0900

    not solved

---
+++ 11651_Coordinate_Sort_2/cmake-build-debug/CMakeFiles/11651_Coordinate_Sort_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11651_Coordinate_Sort_2.exe"
+  "11651_Coordinate_Sort_2.exe.manifest"
+  "11651_Coordinate_Sort_2.pdb"
+  "CMakeFiles/11651_Coordinate_Sort_2.dir/main.cpp.obj"
+  "lib11651_Coordinate_Sort_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11651_Coordinate_Sort_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11651_Coordinate_Sort_2/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct co {
+    int x;
+    int y;
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    struct co point[100001];
+
+    for(int i = 0; i < n; i++) {
+        cin >> point[i].x >> point[i].y;
+    }
+
+
+    return 0;
+}
