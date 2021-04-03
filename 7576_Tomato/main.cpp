90a64f0f6e4e29bff768a4c67740a80c7a706588
skysaver00 <skysaver00@naver.com>
2021-04-04 03:11:05 +0900

    not solved

--- 7576_Tomato/main.cpp
+++ 7576_Tomato/main.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int arr[1001][1001];
 int check[1001][1001];
 bool visited[1001][1001];
-
-queue<int> bfs[2];
+queue<pair<int, int>> bfs;
 
 int x[4] = {1, 0, -1, 0};
 int y[4] = {0, 1, 0, -1};
@@ -16,33 +15,29 @@ void startBFS(int i, int j) {
     if (check[i][j] <= 1) return;
     check[i][j] = 1;
     visited[i][j] = true;
-    bfs[0].push(i);
-    bfs[1].push(j);
+    bfs.push(make_pair(i, j));
 
-    while (!bfs[0].empty() && !bfs[1].empty()) {
-        int ix = bfs[0].front();
-        int jy = bfs[1].front();
-        bfs[0].pop();
-        bfs[1].pop();
+    while (!bfs.empty()) {
+        pair<int, int> xy = bfs.front();
+        bfs.pop();
 
         for (int i = 0; i < 4; i++) {
-            int nextX = ix + x[i];
-            int nextY = jy + y[i];
+            int nextX = xy.first + x[i];
+            int nextY = xy.second + y[i];
             if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= m) continue;
 
-            if (check[ix][jy] + 1 < check[nextX][nextY] && check[nextX][nextY] != 1 && !visited[nextX][nextY]) {
-                check[nextX][nextY] = check[ix][jy] + 1;
+            if (check[xy.first][xy.second] + 1 < check[nextX][nextY] && check[nextX][nextY] != 1 && !visited[nextX][nextY]) {
+                check[nextX][nextY] = check[xy.first][xy.second] + 1;
                 visited[nextX][nextY] = true;
-                bfs[0].push(nextX);
-                bfs[1].push(nextY);
-                cout << nextX << nextY << bfs[0].size() << bfs[1].size() << endl;
+                bfs.push(make_pair(nextX, nextY));
+                //cout << nextX << nextY << bfs[0].size() << bfs[1].size() << endl;
             }
         }
     }
 }
 
 int main() {
-    cin >> m >> n;
+    scanf("%d %d", &m, &n);
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
@@ -52,7 +47,7 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cin >> arr[i][j];
+            scanf("%d", &arr[i][j]);
             if (arr[i][j] == -1) check[i][j] = -1;
         }
     }
@@ -74,7 +69,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (check[i][j] == 999999) {
-                cout << -1 << endl;
+                printf("-1\n");
                 return 0;
             }
             if (max < check[i][j] && check[i][j] != -1) max = check[i][j];
@@ -83,6 +78,6 @@ int main() {
 
     if(max == -999999999) max = 1;
 
-    cout << max - 1 << endl;
+    printf("%d\n", max - 1);
     return 0;
 }
