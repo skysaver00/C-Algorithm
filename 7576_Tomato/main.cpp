d401f19e67a80906bb13b041b9fa12f8c83a166b
skysaver00 <skysaver00@naver.com>
2021-04-03 22:05:10 +0900

    not solved

--- 7576_Tomato/main.cpp
+++ 7576_Tomato/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 short arr[1001][1001];
@@ -8,52 +9,55 @@ queue<int> bfs[2];
 
 int x[4] = {1, 0, -1, 0};
 int y[4] = {0, 1, 0, -1};
+int m, n;
 
 void startBFS(int i, int j) {
-    if(check[i][j] <= 1) return;
+    if (check[i][j] <= 1) return;
     check[i][j] = 1;
     bfs[0].push(i);
     bfs[1].push(j);
 
-    while(!bfs[0].empty() && !bfs[1].empty()) {
+    while (!bfs[0].empty() && !bfs[1].empty()) {
         int ix = bfs[0].front();
         int jy = bfs[1].front();
         bfs[0].pop();
         bfs[1].pop();
 
-        for(int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) {
             int nextX = ix + x[i];
             int nextY = jy + y[i];
+            if (nextX < 0 && nextX >= n && nextY < 0 && nextY >= m) continue;
 
-            if(check[ix][jy] + 1 <= check[nextX][nextY] && check[nextX][nextY] != -1) {
+            if (check[ix][jy] + 1 <= check[nextX][nextY] && check[nextX][nextY] != -1) {
                 check[nextX][nextY] = check[ix][jy] + 1;
                 bfs[0].push(nextX);
                 bfs[1].push(nextY);
+                cout << nextX << nextY << bfs[0].size() << bfs[1].size() << endl;
+
             }
         }
     }
 }
 
 int main() {
-    int m, n;
     cin >> m >> n;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             check[i][j] = 999999;
         }
     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cin >> arr[i][j];
-            if(arr[i][j] == -1) check[i][j] = -1;
+            if (arr[i][j] == -1) check[i][j] = -1;
         }
     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(arr[i][j] == 1) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] == 1) {
                 startBFS(i, j);
                 /*for (int a = 0; a < n; a++) {
                     for (int b = 0; b < m; b++) printf("%d ", check[a][b]);
@@ -64,13 +68,13 @@ int main() {
     }
 
     int max = -999999999;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(check[i][j] == 999999) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (check[i][j] == 999999) {
                 cout << -1 << endl;
                 return 0;
             }
-            if(max < check[i][j]&& check[i][j] != -1) max = check[i][j];
+            if (max < check[i][j] && check[i][j] != -1) max = check[i][j];
         }
     }
 
