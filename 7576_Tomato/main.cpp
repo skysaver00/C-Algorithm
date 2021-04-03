7a62f6e0d105023479c52a65e8acbcd33f0897de
skysaver00 <skysaver00@naver.com>
2021-04-03 21:51:41 +0900

    not solveD

--- 7576_Tomato/main.cpp
+++ 7576_Tomato/main.cpp
@@ -1,14 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[1001][1001];
-int check[1001][1001];
+short arr[1001][1001];
+short check[1001][1001];
+
+queue<int> bfs[2];
 
 int x[4] = {1, 0, -1, 0};
 int y[4] = {0, 1, 0, -1};
 
-void startBFS() {
+void startBFS(int i, int j) {
+    if(check[i][j] <= 1) return;
+    check[i][j] = 1;
+    bfs[0].push(i);
+    bfs[1].push(j);
 
+    while(!bfs[0].empty() && !bfs[1].empty()) {
+        int ix = bfs[0].front();
+        int jy = bfs[1].front();
+        bfs[0].pop();
+        bfs[1].pop();
+
+        for(int i = 0; i < 4; i++) {
+            int nextX = ix + x[i];
+            int nextY = jy + y[i];
+
+            if(check[ix][jy] + 1 <= check[nextX][nextY] && check[nextX][nextY] != -1) {
+                check[nextX][nextY] = check[ix][jy] + 1;
+                bfs[0].push(nextX);
+                bfs[1].push(nextY);
+            }
+        }
+    }
 }
 
 int main() {
@@ -28,7 +51,29 @@ int main() {
         }
     }
 
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(arr[i][j] == 1) {
+                startBFS(i, j);
+                /*for (int a = 0; a < n; a++) {
+                    for (int b = 0; b < m; b++) printf("%d ", check[a][b]);
+                    printf("\n");
+                }*/
+            }
+        }
+    }
 
+    int max = -999999999;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(check[i][j] == 999999) {
+                cout << -1 << endl;
+                return 0;
+            }
+            if(max < check[i][j]&& check[i][j] != -1) max = check[i][j];
+        }
+    }
 
+    cout << max - 1 << endl;
     return 0;
 }
