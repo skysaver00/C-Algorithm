d557d068f948baa682d9bdc964d2236ba5586936
skysaver00 <skysaver00@naver.com>
2021-04-05 21:48:04 +0900

    not solved

--- 15649 N_and_M/main.cpp
+++ 15649 N_and_M/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <algorithm>
+using namespace std;
+
+void swap(int i, int j) {
+    int temp = i;
+    i = j;
+    j = temp;
+}
+
+void permutation(int arr[], int depth, int k) {
+    if(depth == k) {
+        for(int i = 0; i <= k; i++) {
+            printf("%d ", arr[i]);
+        }printf("\n");
+    }
+    else {
+        for(int i = depth; i <= k; i++) {
+            swap(arr[depth], arr[i]);
+            permutation(arr, depth + 1, k);
+            swap(arr[k], arr[i]);
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int
+
     return 0;
 }
