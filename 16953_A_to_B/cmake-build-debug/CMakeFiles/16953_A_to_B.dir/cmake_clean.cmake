8174ad8ab8dce095b0e6f926aa90dadee35101d4
skysaver00 <skysaver00@naver.com>
2021-04-07 01:22:17 +0900

    not solved

---
+++ 16953_A_to_B/cmake-build-debug/CMakeFiles/16953_A_to_B.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "16953_A_to_B.exe"
+  "16953_A_to_B.exe.manifest"
+  "16953_A_to_B.pdb"
+  "CMakeFiles/16953_A_to_B.dir/main.cpp.obj"
+  "lib16953_A_to_B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/16953_A_to_B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
