6e7c632584a2803e41639e12b428bed085a64032
skysaver00 <skysaver00@naver.com>
2021-04-07 01:57:11 +0900

    not solved

---
+++ 1697_Hide_And_Seek/cmake-build-debug/CMakeFiles/1697_Hide_And_Seek.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1697_Hide_And_Seek.exe"
+  "1697_Hide_And_Seek.exe.manifest"
+  "1697_Hide_And_Seek.pdb"
+  "CMakeFiles/1697_Hide_And_Seek.dir/main.cpp.obj"
+  "lib1697_Hide_And_Seek.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1697_Hide_And_Seek.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1697_Hide_And_Seek/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> vec[2000];
+queue<int> bfs;
+int check[100001];
+
+void startBFS(int to, int depth) {
+    int 
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vec[0].push_back(n);
+
+    for(int i = 1; i < 50000; i++) {
+        startBFS(k, i);
+    }
+
+    return 0;
+}
