8334c62ebcc830220381d96e5af879d24efb47df
skysaver00 <skysaver00@naver.com>
2021-04-07 17:08:17 +0900

    solved Gold V

--- 14719_Rainwater/main.cpp
+++ 14719_Rainwater/main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int height[501];
 int miMax[501][2];
 
+int min(int i, int j) {
+    return (i > j) ? j : i;
+}
+
 int main() {
     int h, w;
     cin >> h >> w;
@@ -13,7 +17,7 @@ int main() {
     }
 
     for(int i = 0; i < w; i++) {
-        for(int j = 0; j < i; j++) {
+        for(int j = 0; j <= i; j++) {
             if(miMax[i][0] < height[j]) miMax[i][0] = height[j];
         }
 
@@ -22,11 +26,11 @@ int main() {
         }
     }
 
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < w; j++) {
-            cout << miMax[j][i] << " ";
-        }cout << "\n";
+    int cnt = 0;
+    for(int i = 0; i < w; i++) {
+        cnt += min(miMax[i][0], miMax[i][1]) - height[i];
     }
 
+    cout << cnt << endl;
     return 0;
 }
