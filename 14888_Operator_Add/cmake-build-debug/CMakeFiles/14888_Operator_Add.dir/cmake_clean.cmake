08b254cc13ef135c34de71216a9e938fe0dc5f94
skysaver00 <skysaver00@naver.com>
2021-04-07 20:02:38 +0900

    not solved

---
+++ 14888_Operator_Add/cmake-build-debug/CMakeFiles/14888_Operator_Add.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14888_Operator_Add.exe"
+  "14888_Operator_Add.exe.manifest"
+  "14888_Operator_Add.pdb"
+  "CMakeFiles/14888_Operator_Add.dir/main.cpp.obj"
+  "lib14888_Operator_Add.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14888_Operator_Add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14888_Operator_Add/cmake-build-debug/CMakeFiles/14888_Operator_Add.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 14888_Operator_Add.
+# This may be replaced when dependencies are built.
