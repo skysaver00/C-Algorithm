9f8a3111741056404f3de7c9e0d794f1ffee7d28
skysaver00 <skysaver00@naver.com>
2021-04-07 17:11:05 +0900

    not solved

---
+++ 12851_Hide_And_Seek_2/cmake-build-debug/CMakeFiles/12851_Hide_And_Seek_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "12851_Hide_And_Seek_2.exe"
+  "12851_Hide_And_Seek_2.exe.manifest"
+  "12851_Hide_And_Seek_2.pdb"
+  "CMakeFiles/12851_Hide_And_Seek_2.dir/main.cpp.obj"
+  "lib12851_Hide_And_Seek_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12851_Hide_And_Seek_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 12851_Hide_And_Seek_2/cmake-build-debug/CMakeFiles/12851_Hide_And_Seek_2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 12851_Hide_And_Seek_2.
+# This may be replaced when dependencies are built.
