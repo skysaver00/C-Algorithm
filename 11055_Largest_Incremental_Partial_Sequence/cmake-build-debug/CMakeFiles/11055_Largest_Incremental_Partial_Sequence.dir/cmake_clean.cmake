5c68e26ff2783eb47b46d6735dd76866f1ddfe9c
skysaver00 <skysaver00@naver.com>
2021-04-08 17:25:14 +0900

    solved Silver II

---
+++ 11055_Largest_Incremental_Partial_Sequence/cmake-build-debug/CMakeFiles/11055_Largest_Incremental_Partial_Sequence.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11055_Largest_Incremental_Partial_Sequence.exe"
+  "11055_Largest_Incremental_Partial_Sequence.exe.manifest"
+  "11055_Largest_Incremental_Partial_Sequence.pdb"
+  "CMakeFiles/11055_Largest_Incremental_Partial_Sequence.dir/main.cpp.obj"
+  "lib11055_Largest_Incremental_Partial_Sequence.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11055_Largest_Incremental_Partial_Sequence.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 11055_Largest_Incremental_Partial_Sequence/main.cpp
+++ 11055_Largest_Incremental_Partial_Sequence/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+using namespace std;
+
+int arr[1001];
+int check[1001];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    check[0] = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(arr[i] > arr[j]) {
+                if(check[i] <= check[j] + arr[i]) check[i] = check[j] + arr[i];
+            }
+        }
+        if(check[i] == 0) check[i] = arr[i];
+    }
+
+    int max = 0;
+    for(int i = 0; i < n; i++) {
+        if(max < check[i]) max = check[i];
+    }
+    cout << max << endl;
+
     return 0;
 }
