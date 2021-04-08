85bd3588304ea0630ab5e291eb250be1c5a730a5
skysaver00 <skysaver00@naver.com>
2021-04-08 16:56:46 +0900

    not solved

---
+++ 11053_Longest_Increasing_Partial_Sequence/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int arr[1001];
+int check[1001];
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    check[0] = 1;
+
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(arr[i] > arr[j]) {
+                if(check[i] <= check[j]) {
+                    check[i] = check[j] + 1;
+                }
+            }
+        }
+        if(check[i] == 0) check[i] = 1;
+    }
+
+    cout << check[n - 1] << endl;
+    return 0;
+}
