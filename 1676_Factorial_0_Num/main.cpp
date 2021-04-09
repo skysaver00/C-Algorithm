5b152586c1da5389f551e8c2dd1def2e351c2097
skysaver00 <skysaver00@naver.com>
2021-04-09 23:33:23 +0900

    solved Silver III

--- 1676_Factorial_0_Num/main.cpp
+++ 1676_Factorial_0_Num/main.cpp
@@ -4,14 +4,9 @@ int main() {
     int t;
     scanf("%d", &t);
 
-    int twoNum = 0;
     int fiveNum = 0;
     for(int i = 2; i <= t; i++) {
         int check = i;
-        while(check % 2 == 0) {
-            twoNum++;
-            check /= 2;
-        }
         while(check % 5 == 0) {
             fiveNum++;
             check /= 5;
@@ -19,6 +14,5 @@ int main() {
     }
 
     printf("%d\n", fiveNum);
-
     return 0;
 }
