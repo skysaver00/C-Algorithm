4801d9f931bfc61db3774844c11209ae234ab374
skysaver00 <skysaver00@naver.com>
2021-04-11 00:36:58 +0900

    not solved

---
+++ 713C_A_B_Palindrome/cmake-build-debug/CMakeFiles/713C_A_B_Palindrome.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "713C_A_B_Palindrome.exe"
+  "713C_A_B_Palindrome.exe.manifest"
+  "713C_A_B_Palindrome.pdb"
+  "CMakeFiles/713C_A_B_Palindrome.dir/main.cpp.obj"
+  "lib713C_A_B_Palindrome.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/713C_A_B_Palindrome.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 713C_A_B_Palindrome/main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+        cin >> str;
+
+        int len = str.length();
+        bool questionFlag = false;
+        for(int i = 0; i < len; i++) {
+            if(str.at(i) == '?') {
+                questionFlag = true;
+                break;
+            }
+        }
+
+        int len2 = ceil(len / 2.0);
+        if(questionFlag) {
+            for(int i = 0; i < len2; i++) {
+                if(str.at(i) == '0' && str.at(len - i - 1) == '?') {
+                    str.at(len - i - 1) = '0';
+                }
+                else if(str.at(i) == '1' && str.at(len - i - 1) == '?') {
+                    str.at(len - i - 1) = '1';
+                }
+                else if(str.at(len - i - 1) == '0' && str.at(i) == '?') {
+                    str.at(i) = '0';
+                }
+                else if(str.at(len - i - 1) == '1' && str.at(i) == '?') {
+                    str.at(i) = '1';
+                }
+            }
+        }
+
+        for(int i = 0; i < len; i++) {
+            if(str.at(i) == '0') a--;
+            else if(str.at(i) == '1') b--;
+        }
+        if(a % 2 != 0 && b % 2 != 0) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        for(int i = 0 ; i < len; i++) {
+            if(a == 0 && b == 0) break;
+
+            if(str.at(i) == '?') {
+                if(a > 0) {
+                    str.at(i) = '0';
+                    str.at(len - i - 1) = '0';
+                } else {
+                    str.at(i) = '1';
+                    str.at(len - i - 1) = '1';
+                }
+            }
+        }
+
+        cout << str << endl;
+    }
+}
