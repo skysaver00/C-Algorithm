3023f3a1ecb7b840dfbf63033966c6dc39686f4a
skysaver00 <skysaver00@naver.com>
2021-04-12 23:52:03 +0900

    not solved

---
+++ Edu107B_GCD_Length/cmake-build-debug/CMakeFiles/Edu107B_GCD_Length.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Edu107B_GCD_Length.dir/main.cpp.obj"
+  "Edu107B_GCD_Length.exe"
+  "Edu107B_GCD_Length.exe.manifest"
+  "Edu107B_GCD_Length.pdb"
+  "libEdu107B_GCD_Length.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Edu107B_GCD_Length.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Edu107B_GCD_Length/cmake-build-debug/CMakeFiles/Edu107B_GCD_Length.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Edu107B_GCD_Length.
+# This may be replaced when dependencies are built.
