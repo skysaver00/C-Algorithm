7ab63efc1cfa0dc0516de10b7f9ab33c56ce0426
skysaver00 <skysaver00@naver.com>
2021-04-12 23:48:18 +0900

    solved

--- Edu107A_Review_Site/main.cpp
+++ Edu107A_Review_Site/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        int up, down, fun;
+        up = down = fun = 0;
+        for(int i = 0; i < n; i++) {
+            int r;
+            cin >> r;
+
+            if(r == 1) up++;
+            else if(r == 2) down++;
+            else {
+                up++;
+            }
+        }
+        cout << up << endl;
+    }
+
     return 0;
 }
