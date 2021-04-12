661765b3796850a841b88b40d676558ce774ec48
skysaver00 <skysaver00@naver.com>
2021-04-13 00:18:23 +0900

    not solved

--- Edu107B_GCD_Length/main.cpp
+++ Edu107B_GCD_Length/main.cpp
@@ -1,29 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-bool arr[100000001];
-
 int main() {
     int t;
     scanf("%d", &t);
-
     int num  = 100000001;
-
-    arr[1] = 1;
-
-    for(int i = 2; i <= sqrt(num); i++) {
-        if(arr[i] == 1) continue;
-        for(int j = i * i; j <= num; j+= i) arr[j] = 1;
-    }
-
-    for(int i = 2; i <= 10000; i++) {
-        printf("%d\n", arr[i]);
-    }
 
     while(t--) {
         int a, b, c;
         scanf("%d %d %d", &a, &b, &c);
 
+        int x = floor(pow(10, a - 1));
+        int y = floor(pow(10, b - 1));
 
+        int flag;
+        if(x > y) flag = 1;
+        else if(x <= y) flag = 2;
+
+        int t;
+        while(1) {
+            t = gcd(x, y);
+            int tPow = t / floor(pow(10, c  - 1));
+            printf("%d %d %d %d\n", x, y, t, tPow);
+            if(tPow >= 1 && tPow <= 9) {
+                printf("%d %d\n", x, y);
+                break;
+            }
+
+            if(flag == 1) {
+                if(y < floor(pow(10, b))) y++;
+                else {
+                    y = floor(pow(10, b-1));
+                    x++;
+                }
+            } else {
+                if(x < floor(pow(10, a))) x++;
+                else {
+                    x = floor(pow(10, a - 1));
+                    y++;
+                }
+            }
+        }
     }
+
+    return 0;
 }
