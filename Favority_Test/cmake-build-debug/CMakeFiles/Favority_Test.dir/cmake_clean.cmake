ccc084247e365380587ed14dff3c22e47ae89db3
skysaver00 <skysaver00@naver.com>
2021-04-14 19:04:22 +0900

    check

---
+++ Favority_Test/cmake-build-debug/CMakeFiles/Favority_Test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Favority_Test.dir/main.cpp.obj"
+  "Favority_Test.exe"
+  "Favority_Test.exe.manifest"
+  "Favority_Test.pdb"
+  "libFavority_Test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Favority_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
