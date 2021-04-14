3aa0d33c3f13ec17468f68ad942141999ca1ff4d
skysaver00 <skysaver00@naver.com>
2021-04-14 19:08:46 +0900

    check

--- Favority_Test/main.cpp
+++ Favority_Test/main.cpp
@@ -1,2 +1,17 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+int arr[10];
 
+int main() {
+    int t;
+
+    while(1) {
+        random_device rd;
+        mt19937  mt(rd());
+        uniform_int_distribution<int> dist(1, 10);
+
+        cout << dist(mt) << endl;
+    }
+
+    return 0;
+}
