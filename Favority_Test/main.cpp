1b6d8afebbc1f8b5c1154822413d1ab714ce49fc
skysaver00 <skysaver00@naver.com>
2021-04-14 19:12:56 +0900

    count

--- Favority_Test/main.cpp
+++ Favority_Test/main.cpp
@@ -1,16 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[10];
+int arr[11];
 
 int main() {
     int t;
 
-    while(1) {
+    while(arr[9] != 100000) {
         random_device rd;
         mt19937  mt(rd());
         uniform_int_distribution<int> dist(1, 10);
 
-        cout << dist(mt) << endl;
+        for(int i = 0; i < 8000; i++) {
+            int t = dist(mt);
+
+            if(t == 9) {
+                arr[t]++;
+                break;
+            }
+            else arr[t]++;
+        }
+    }
+
+    for(int i = 1; i <= 10; i++) {
+        cout << arr[i] << endl;
     }
 
     return 0;
