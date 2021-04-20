d8da46fb3b55bee1eb8c86a613cb952a4ebd872f
skysaver00 <skysaver00@naver.com>
2021-04-20 16:08:54 +0900

    not solved

--- 1463_Makeinto_1/main.cpp
+++ 1463_Makeinto_1/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+using namespace std;
+
+int arr[1000001];
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= 1000000; i++) arr[i] = 99999999;
+
+    arr[n] = 0;
+
+    for(int i = n - 1; i >= 1; i--) {
+        arr[i] = min(arr[i + 1], min(arr[i * 2], arr[i * 3])) + 1;
+    }
+
+    for(int i = n; i >= 1; i--) {
+        cout << arr[i] << endl;
+    }
     return 0;
 }
