a0dd5ab4461688e467ac6ba6dc633d1163834c4a
skysaver00 <skysaver00@naver.com>
2021-04-20 17:00:39 +0900

    not solved

---
+++ 9251_LCS/cmake-build-debug/CMakeFiles/9251_LCS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "9251_LCS.exe"
+  "9251_LCS.exe.manifest"
+  "9251_LCS.pdb"
+  "CMakeFiles/9251_LCS.dir/main.cpp.obj"
+  "lib9251_LCS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9251_LCS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9251_LCS/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str1;
+string str2;
+
+int main() {
+    cin >> str1;
+    cin >> str2;
+
+    string mirStr;
+    if(str1.length() < str2.length()) {
+        
+    } else {
+
+    }
+}
