b461de46f8bb3844e3b6372abb0f8877ff6dcd7c
skysaver00 <skysaver00@naver.com>
2021-04-22 15:51:02 +0900

    not solved

---
+++ 1753_Shortest_Path/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[300005][3];
+int val[300005];
+
+int main() {
+    int v, e;
+    int k;
+    scanf("%d %d", &v, &e);
+    scanf("%d", &k);
+
+
+    for(int i = 0; i < e; i++) {
+        scanf("%d %d %d", &arr[i][0], &arr[i][1], &arr[i][2]);
+    }
+
+
+
+    return 0;
+}
