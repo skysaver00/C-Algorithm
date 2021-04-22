0c0ba8bb671081b2ed4f34362d0a0a6a3099ad79
skysaver00 <skysaver00@naver.com>
2021-04-22 17:27:01 +0900

    not solved

--- 16234_Moving_Population/main.cpp
+++ 16234_Moving_Population/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int terr[51][51];
 int open[51][51];
-int n, l, r;
+int m, l, r;
 queue<pair<int, int>> bfs;
 
 int x[4] = {1, 0, -1 ,0};
@@ -24,8 +24,8 @@ int startBFS(int a, int b, int n) {
         int two = bfs.front().second;
         bfs.pop();
         for(int i = 0; i < 4; i++) {
-            if(one + x[i] > n || one + x[i] < 0 || two + y[i] > n || two + y[i] < 0) continue;
-            printf("%d %d %d %d\n", abs(terr[one + x[i]][two + y[i]] - terr[one][two]), one, two, i);
+            if(one + x[i] > m || one + x[i] < 0 || two + y[i] > m || two + y[i] < 0) continue;
+            printf("check: %d %d %d %d %d\n", abs(terr[one + x[i]][two + y[i]] - terr[one][two]), one, two, one + x[i], two + y[i]);
             printf("pp: %d %d %d\n", open[one + x[i]][two + y[i]], one + x[i], two + y[i]);
             if(open[one + x[i]][two + y[i]] == 0 && (abs(terr[one + x[i]][two + y[i]] - terr[one][two]) >= l) && (abs(terr[one + x[i]][two + y[i]] - terr[one][two]) <= r)) {
                 printf("yes: %d %d\n", one + x[i], two + y[i]);
@@ -39,22 +39,22 @@ int startBFS(int a, int b, int n) {
 }
 
 int main() {
-    scanf("%d %d %d", &n, &l, &r);
+    scanf("%d %d %d", &m, &l, &r);
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < m; j++) {
             scanf("%d", &terr[i][j]);
         }
     }
 
     int count = 1;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < m; j++) {
             int check = startBFS(i, j, count);
             count = ++check;
 
-            for(int a = 0; a < n; a++) {
-                for(int b = 0; b < n; b++) {
+            for(int a = 0; a < m; a++) {
+                for(int b = 0; b < m; b++) {
                     printf("%d ", open[a][b]);
                 }
                 printf("\n");
