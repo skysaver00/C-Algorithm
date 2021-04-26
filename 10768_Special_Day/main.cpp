c378c240d962ace6c4b98f440aff4619751a4b99
skysaver00 <skysaver00@naver.com>
2021-04-27 00:00:08 +0900

    not solved

--- 10768_Special_Day/main.cpp
+++ 10768_Special_Day/main.cpp
@@ -3,5 +3,13 @@
 int main() {
     int month, day;
     scanf("%d %d", &month, &day);
-    
+
+    if(month > 2) {
+        printf("After");
+        return 0;
+    } else if(month >= 2) {
+        if(day > 18)
+    }
+
+    return 0;
 }
