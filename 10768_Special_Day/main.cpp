4725faff686ae7b619a6a0ff7b384ac9124fb009
skysaver00 <skysaver00@naver.com>
2021-04-27 00:02:14 +0900

    solved Bronze IV

--- 10768_Special_Day/main.cpp
+++ 10768_Special_Day/main.cpp
@@ -5,10 +5,17 @@ int main() {
     scanf("%d %d", &month, &day);
 
     if(month > 2) {
-        printf("After");
-        return 0;
+        printf("After\n");
     } else if(month >= 2) {
-        if(day > 18)
+        if(day > 18) {
+            printf("After\n");
+        } else if(day == 18) {
+            printf("Special\n");
+        } else {
+            printf("Before\n");
+        }
+    } else {
+        printf("Before\n");
     }
 
     return 0;
