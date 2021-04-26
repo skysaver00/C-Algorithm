1b1ae9c040f3250fe4ee549f359f741aed338965
skysaver00 <skysaver00@naver.com>
2021-04-26 23:58:14 +0900

    not solved

---
+++ 10768_Special_Day/cmake-build-debug/CMakeFiles/10768_Special_Day.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "10768_Special_Day.exe"
+  "10768_Special_Day.exe.manifest"
+  "10768_Special_Day.pdb"
+  "CMakeFiles/10768_Special_Day.dir/main.cpp.obj"
+  "lib10768_Special_Day.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10768_Special_Day.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
