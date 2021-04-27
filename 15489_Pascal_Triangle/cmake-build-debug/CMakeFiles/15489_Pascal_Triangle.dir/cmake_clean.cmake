c4f9d1889dc4010eeb4edab35940ae9e0249939e
skysaver00 <skysaver00@naver.com>
2021-04-27 15:57:18 +0900

    not solved

---
+++ 15489_Pascal_Triangle/cmake-build-debug/CMakeFiles/15489_Pascal_Triangle.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15489_Pascal_Triangle.exe"
+  "15489_Pascal_Triangle.exe.manifest"
+  "15489_Pascal_Triangle.pdb"
+  "CMakeFiles/15489_Pascal_Triangle.dir/main.cpp.obj"
+  "lib15489_Pascal_Triangle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15489_Pascal_Triangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 15489_Pascal_Triangle/cmake-build-debug/CMakeFiles/15489_Pascal_Triangle.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 15489_Pascal_Triangle.
+# This may be replaced when dependencies are built.
