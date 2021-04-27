3b9146a759734d8ee5cedc28dacda9061b0e61cd
skysaver00 <skysaver00@naver.com>
2021-04-27 15:55:10 +0900

    solved Silver V

--- 14916_Change/main.cpp
+++ 14916_Change/main.cpp
@@ -12,20 +12,19 @@ int main() {
 
     for(int i = 0; i <= 100000; i++) arr[i] = 999999;
 
-    arr[1] = -1;
     arr[2] = 1;
-    arr[3] = -1;
     arr[4] = 2;
     arr[5] = 1;
 
     for(int i = 6; i <= n; i++) {
         arr[i] = lowest(arr[i], lowest(arr[i - 2] + 1, arr[i - 5] + 1));
-        if(arr[i] == 999999) arr[i] = -1;
     }
 
-    for(int i = 1; i <= n; i++) {
-        printf("%d ", arr[i]);
+    if(arr[n] == 999999) {
+        printf("-1\n");
+        return 0;
     }
 
     printf("%d\n", arr[n]);
+    return 0;
 }
