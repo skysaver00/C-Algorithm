dbc90e761ea57f3b44445622eba87a67fb068f2c
skysaver00 <skysaver00@naver.com>
2021-04-27 15:51:01 +0900

    not solved

---
+++ 14916_Change/cmake-build-debug/CMakeFiles/14916_Change.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14916_Change.exe"
+  "14916_Change.exe.manifest"
+  "14916_Change.pdb"
+  "CMakeFiles/14916_Change.dir/main.cpp.obj"
+  "lib14916_Change.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14916_Change.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14916_Change/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int lowest(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int arr[100001];
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i <= 100000; i++) arr[i] = 999999;
+
+    arr[1] = -1;
+    arr[2] = 1;
+    arr[3] = -1;
+    arr[4] = 2;
+    arr[5] = 1;
+
+    for(int i = 6; i <= n; i++) {
+        arr[i] = lowest(arr[i], lowest(arr[i - 2] + 1, arr[i - 5] + 1));
+        if(arr[i] == 999999) arr[i] = -1;
+    }
+
+    printf("%d\n", arr[n]);
+}
