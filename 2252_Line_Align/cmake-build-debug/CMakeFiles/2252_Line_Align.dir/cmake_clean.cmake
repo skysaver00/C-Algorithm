31fd9d681d24a5189f6e00839ca96778efb51831
skysaver00 <skysaver00@naver.com>
2021-04-29 21:07:44 +0900

    not solved

---
+++ 2252_Line_Align/cmake-build-debug/CMakeFiles/2252_Line_Align.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2252_Line_Align.exe"
+  "2252_Line_Align.exe.manifest"
+  "2252_Line_Align.pdb"
+  "CMakeFiles/2252_Line_Align.dir/main.cpp.obj"
+  "lib2252_Line_Align.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2252_Line_Align.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2252_Line_Align/main.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int degree[32001];
+vector<int> con[32001];
+
+queue<int> top;
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for(int i = 0; i < m; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        degree[b]++;
+        con[a].push_back(b);
+    }
+
+    for(int i = 1; i <= n; i++) {
+        if(degree[i] == 0) top.push(i);
+    }
+
+    while(!top.empty()) {
+        int ans = top.front();
+        printf("%dx ", ans);
+        top.pop();
+
+        int len = con[ans].size();
+        for(int i = 0; i < len; i++) {
+            printf("%d\n", degree[con[ans][i]]);
+            degree[con[ans][i]]--;
+            if(degree[con[ans][i]] == 0) top.push(con[ans][i]);
+        }
+    }
+
+    return 0;
+}
