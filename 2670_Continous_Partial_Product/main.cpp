4327b1eac63d5b128b9658bc42e1dd8a7d1fc5f1
skysaver00 <skysaver00@naver.com>
2021-04-29 16:21:15 +0900

    solved Silver IV

--- 2670_Continous_Partial_Product/main.cpp
+++ 2670_Continous_Partial_Product/main.cpp
@@ -1,7 +1,26 @@
 #include <stdio.h>
-
 double arr[10001];
+double real[10001];
+
+double max(double i, double j) {
+    return (i > j) ? i : j;
+}
 
 int main() {
-    
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%lf", &arr[i]);
+    }
+
+    real[0] = arr[0];
+    double val = -9999;
+    for(int i = 1; i < n; i++) {
+        real[i] = max(arr[i], arr[i] * real[i - 1]);
+        if (val < real[i]) val = real[i];
+    }
+    printf("%.3lf\n", val);
+
+    return 0;
 }
