7b58616295ba662583508599290f77f804726fb1
skysaver00 <skysaver00@naver.com>
2021-04-30 02:34:37 +0900

    not solved

--- 1005_ACM_Craft/main.cpp
+++ 1005_ACM_Craft/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int duration[1001];
+int total[1001];
 int level[1001];
 vector<int> path[1001];
 queue<int> topoQueue;
@@ -16,7 +17,7 @@ int main() {
         int n, k;
         scanf("%d %d", &n, &k);
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 1; i <= n; i++) {
             scanf("%d", &duration[i]);
         }
 
@@ -24,7 +25,33 @@ int main() {
             int x, y;
             scanf("%d %d", &x, &y);
 
-            
+            level[y]++;
+            path[x].push_back(y);
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(level[i] == 0) {
+                topoQueue.push(i);
+                total[i] += duration[i];
+            }
+        }
+
+        while(!topoQueue.empty()) {
+            int val = topoQueue.front();
+            topoQueue.pop();
+
+            int len = path[val].size();
+            for(int j = 0; j < len; j++) {
+                int next = path[val][j];
+                if(total[next] > total[val] + duration[next]) {
+                    total[next] = total[val] + duration[next];
+                }
+                topoQueue.push(next);
+            }
+        }
+
+        for(int i  = 1; i <= n; i++) {
+            printf("%d\n", total[i]);
         }
     }
     return 0;
