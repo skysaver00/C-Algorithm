866ada676471d97067b6aeb365cc046d0c843e78
skysaver00 <skysaver00@naver.com>
2021-04-30 02:16:16 +0900

    not solved

--- 1005_ACM_Craft/main.cpp
+++ 1005_ACM_Craft/main.cpp
@@ -3,6 +3,7 @@
 #include <queue>
 using namespace std;
 
+int duration[1001];
 int level[1001];
 vector<int> path[1001];
 queue<int> topoQueue;
@@ -15,7 +16,16 @@ int main() {
         int n, k;
         scanf("%d %d", &n, &k);
 
-        
+        for(int i = 0; i < n; i++) {
+            scanf("%d", &duration[i]);
+        }
+
+        for(int i = 0; i < k; i++) {
+            int x, y;
+            scanf("%d %d", &x, &y);
+
+            
+        }
     }
     return 0;
 }
