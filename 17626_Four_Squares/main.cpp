06657b4442cbdfd63df132edf1a58b41a54d4d2c
skysaver00 <skysaver00@naver.com>
2021-04-29 14:43:14 +0900

    not solved

--- 17626_Four_Squares/main.cpp
+++ 17626_Four_Squares/main.cpp
@@ -7,34 +7,21 @@ int main() {
     scanf("%d", &n);
 
     for(int i = sqrt(n); i >= 1; i--) {
-        
-    }
+        for(int j = 1; j <= n; j++) {
+            if(arr[j][1] == j) continue;
+            if(arr[j][1] + (i * i) * ((j - arr[j][1]) / (i * i)) > j) continue;
+            arr[j][0] += (j - arr[j][1]) / (i * i);
+            arr[j][1] += (i * i) * ((j - arr[j][1]) / (i * i));
 
-    for(int i = sqrt(n); i >= 1; i--) {
-        for(int j = 1; i * j <= n; j++) {
-            if(arr[i * i * j][1] + (i * i * j) > n) continue;
-            if(arr[i * i * j][1] == n) continue;
-            arr[i * i * j][0] += j;
-            arr[i * i * j][1] += i * i * j;
-
-            for(int l = 1; l <= n; l++) {
-                printf("%d ", arr[l][0]);
-            }printf("\n");
-
-            for(int l = 1; l <= n; l++) {
-                printf("%d ", arr[l][1]);
-            }printf("\n");
+            if(j == n)  {
+                printf("%d\n", arr[n][0]);
+                printf("%d\n", arr[n][1]);
+            }
         }
     }
 
-    for(int i = 1; i <= n; i++) {
-        printf("%d ", arr[i][0]);
-    }printf("\n");
-
-    for(int i = 1; i <= n; i++) {
-        printf("%d ", arr[i][1]);
-    }
-
+    printf("%d\n", arr[n][0]);
+    printf("%d\n", arr[n][1]);
 
 
     return 0;
