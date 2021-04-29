0773aa406b9013d7e464bfdda715e866a783b124
skysaver00 <skysaver00@naver.com>
2021-04-29 14:25:39 +0900

    not solved

--- 17626_Four_Squares/main.cpp
+++ 17626_Four_Squares/main.cpp
@@ -1,13 +1,38 @@
 #include <stdio.h>
-#include <
+#include <math.h>
 int arr[50001][2];
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    for(int i = 1; i <= 223; i++) {
-        arr[i * i] = 1;
+    for(int i = sqrt(n); i >= 1; i--) {
+        
+    }
+
+    for(int i = sqrt(n); i >= 1; i--) {
+        for(int j = 1; i * j <= n; j++) {
+            if(arr[i * i * j][1] + (i * i * j) > n) continue;
+            if(arr[i * i * j][1] == n) continue;
+            arr[i * i * j][0] += j;
+            arr[i * i * j][1] += i * i * j;
+
+            for(int l = 1; l <= n; l++) {
+                printf("%d ", arr[l][0]);
+            }printf("\n");
+
+            for(int l = 1; l <= n; l++) {
+                printf("%d ", arr[l][1]);
+            }printf("\n");
+        }
+    }
+
+    for(int i = 1; i <= n; i++) {
+        printf("%d ", arr[i][0]);
+    }printf("\n");
+
+    for(int i = 1; i <= n; i++) {
+        printf("%d ", arr[i][1]);
     }
 
 
