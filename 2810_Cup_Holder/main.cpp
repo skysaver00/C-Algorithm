7621494c35ad2a8fc5f4df120c066ba2f08bf27e
skysaver00 <skysaver00@naver.com>
2021-04-29 17:18:56 +0900

    not solved

--- 2810_Cup_Holder/main.cpp
+++ 2810_Cup_Holder/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 char arr[51];
 
 int main() {
@@ -6,7 +7,17 @@ int main() {
     scanf("%d", &n);
     scanf("%s", arr);
 
-    
+    int len = strlen(arr);
+
+    int ns = 0;
+    int nl = 0;
+    for(int i = 0; i < len; i++) {
+        if(arr[i] == 'S') ns++;
+        else if(arr[i] == 'L') nl++;
+    }
+    nl /= 2;
+
+    printf("%d\n", ns + nl + );
 
     return 0;
 }
