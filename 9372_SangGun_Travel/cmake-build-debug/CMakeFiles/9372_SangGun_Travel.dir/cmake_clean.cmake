d68fe23129294998bcb4215638361332d04ddfab
skysaver00 <skysaver00@naver.com>
2021-04-30 23:39:36 +0900

    not solved

---
+++ 9372_SangGun_Travel/cmake-build-debug/CMakeFiles/9372_SangGun_Travel.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "9372_SangGun_Travel.exe"
+  "9372_SangGun_Travel.exe.manifest"
+  "9372_SangGun_Travel.pdb"
+  "CMakeFiles/9372_SangGun_Travel.dir/main.cpp.obj"
+  "lib9372_SangGun_Travel.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9372_SangGun_Travel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
