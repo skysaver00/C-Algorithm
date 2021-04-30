9d0c45a81733417416150e2d5342f2065893411b
skysaver00 <skysaver00@naver.com>
2021-05-01 02:21:33 +0900

    not solved

---
+++ 2293_Coin_1/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int coin[101];
+int val[10001];
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+
+    for(int i = 0; i < n; i++) scanf("%d", &coin[i]);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 1; j <= k; j++) {
+            if()
+        }
+    }
+
+
+
+    return 0;
+}
