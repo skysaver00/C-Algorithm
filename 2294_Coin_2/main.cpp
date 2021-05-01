31d44c521832b3b43e886c55212ef08f68b6a60c
skysaver00 <skysaver00@naver.com>
2021-05-01 17:14:54 +0900

    not solved

--- 2294_Coin_2/main.cpp
+++ 2294_Coin_2/main.cpp
@@ -3,6 +3,10 @@ bool check[100001];
 int coin[101];
 int price[10001];
 
+int max(int i, int j) {
+    return (i > j) ?i:j;
+}
+
 int main() {
     int n, k;
     scanf("%d %d", &n, &k);
@@ -20,8 +24,20 @@ int main() {
         }
     }
 
-    for(int i = 0; i < t; i++) {
-        printf("%d\n", coin[i]);
+    for(int i = 1; i <= k; i++) price[i] = 99999999;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 1; j <= k; j++) {
+            if(j == coin[i]) price[j] = 1;
+            else if(j < coin[i]) continue;
+            else {
+                price[j] = max(price[j], price[j - coin[i]] + 1);
+            }
+        }
+
+        for(int j = 1; j <= k; j++) {
+            printf("%d\n", price[j]);
+        } printf("\n");
     }
 
     return 0;
