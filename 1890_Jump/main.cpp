f1c2186baf75dbf1cba15178cddd2119b4331e57
skysaver00 <skysaver00@naver.com>
2021-05-01 18:02:06 +0900

    not solved

--- 1890_Jump/main.cpp
+++ 1890_Jump/main.cpp
@@ -17,6 +17,7 @@ int main() {
         }
     }
 
+    turn[0][0] = 1;
     loc.push({0, 0});
 
     while(!loc.empty()) {
@@ -24,20 +25,30 @@ int main() {
         int x = loc.front().second;
 
         loc.pop();
+
         int jump = gameArea[y][x];
 
-        turn[y + jump][x] = turn[y][x] + 1;
-        turn[y][x + jump] = turn[y][x] + 1;
+        turn[y + jump][x] += turn[y][x];
+        turn[y][x + jump] += turn[y][x];
+        //printf("right now: %d %d\n", y, x);
 
-        loc.push({y + jump, x});
-        loc.push({y, x + jump});
+        if(y + jump < n && y + jump != y) {
+            //printf("%d %d\n", y + jump, x);
+            if(y + jump != n - 1 || x != n - 1) loc.push({y + jump, x});
+        }
+        if(x + jump < n && x + jump != x) {
+            //printf("%d %d\n", y, x + jump);
+            if(y != n - 1 || x + jump != n - 1) loc.push({y, x + jump});
+        }
 
-        for(int i = 0; i < n ; i++) {
+        /*for(int i = 0; i < n ; i++) {
             for(int j = 0; j < n; j++) {
                 printf("%d ", turn[i][j]);
             } printf("\n");
-        } printf("\n");
+        } printf("\n");*/
     }
+
+    printf("%lld\n", turn[n - 1][n - -1]);
 
     return 0;
 }
