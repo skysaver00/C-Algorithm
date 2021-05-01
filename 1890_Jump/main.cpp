19ebb4d272586cc0a7c229fe1bf97c8c98a82fd9
skysaver00 <skysaver00@naver.com>
2021-05-01 17:43:27 +0900

    not solved

--- 1890_Jump/main.cpp
+++ 1890_Jump/main.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <queue>
 using namespace std;
+
 int gameArea[101][101];
 long long turn[101][101];
 
-
+queue<pair<int, int>> loc;
 
 int main() {
     int n;
@@ -16,7 +17,27 @@ int main() {
         }
     }
 
+    loc.push({0, 0});
 
+    while(!loc.empty()) {
+        int y = loc.front().first;
+        int x = loc.front().second;
+
+        loc.pop();
+        int jump = gameArea[y][x];
+
+        turn[y + jump][x] = turn[y][x] + 1;
+        turn[y][x + jump] = turn[y][x] + 1;
+
+        loc.push({y + jump, x});
+        loc.push({y, x + jump});
+
+        for(int i = 0; i < n ; i++) {
+            for(int j = 0; j < n; j++) {
+                printf("%d ", turn[i][j]);
+            } printf("\n");
+        } printf("\n");
+    }
 
     return 0;
 }
