b34840a6782f13442a85fcb776629a02a2f71693
skysaver00 <skysaver00@naver.com>
2021-05-04 22:55:56 +0900

    solved Bronze V

---
+++ 5554_Errand/cmake-build-debug/CMakeFiles/5554_Errand.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5554_Errand.exe"
+  "5554_Errand.exe.manifest"
+  "5554_Errand.pdb"
+  "CMakeFiles/5554_Errand.dir/main.cpp.obj"
+  "lib5554_Errand.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5554_Errand.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 5554_Errand/main.cpp
+++ 5554_Errand/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int sec;
+    int time = 0;
+    for(int i = 0; i < 4; i++) {
+        cin >> sec;
+        time += sec;
+    }
+
+    printf("%d %d\n", time / 60, time % 60);
 }
