dbcd65efb3c1830d95b99de6cb5c6843910a0b14
skysaver00 <skysaver00@naver.com>
2021-05-05 00:17:23 +0900

    solved Silver I

--- 10844_Easy_Stair_Number/main.cpp
+++ 10844_Easy_Stair_Number/main.cpp
@@ -4,30 +4,26 @@ long long arr[10][101];
 int main() {
     for(int i = 1; i <= 9; i++) arr[i][0] = 1;
 
-
+    long long mod = 1000000000;
     int n;
     scanf("%d", &n);
 
     for(int tr = 1; tr < n; tr++) {
         for(int i = 0; i <= 9; i++) {
             if(i == 0) {
-                arr[i + 1][tr] += arr[i][tr - 1];
+                arr[i + 1][tr] = arr[i + 1][tr] + (arr[i][tr - 1] % mod);
             } else if(i == 9) {
-                arr[i - 1][tr] += arr[i][tr - 1];
+                arr[i - 1][tr] = arr[i - 1][tr] + (arr[i][tr - 1] % mod);
             } else {
-                arr[i + 1][tr] += arr[i][tr - 1];
-                arr[i - 1][tr] += arr[i][tr - 1];
+                arr[i + 1][tr] = arr[i + 1][tr] + (arr[i][tr - 1] % mod);
+                arr[i - 1][tr] = arr[i - 1][tr] + (arr[i][tr - 1] % mod);
             }
-            arr[i - 1][tr] = arr[i - 1][tr] % 1000000000;
-            arr[i + 1][tr] = arr[i + 1][tr] % 1000000000;
-
-            printf("%lld %lld\n", arr[i - 1][tr], arr[i + 1][tr]);
         }
     }
 
     long long sum = 0;
     for(int i = 0; i <= 9; i++) {
-        sum += arr[i][n - 1] % 1000000000;
+        sum = (sum + arr[i][n - 1]) % 1000000000;
     }
 
     printf("%lld\n", sum % 1000000000);
