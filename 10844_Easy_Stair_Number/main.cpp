b3601a22b11072701695d97a452a3dff3ca516c5
skysaver00 <skysaver00@naver.com>
2021-05-04 23:47:11 +0900

    not solved

--- 10844_Easy_Stair_Number/main.cpp
+++ 10844_Easy_Stair_Number/main.cpp
@@ -2,7 +2,7 @@
 int arr[10][101];
 
 int main() {
-    for(int i = 0; i <= 9; i++) arr[i][0] = 1;
+    for(int i = 1; i <= 9; i++) arr[i][0] = 1;
 
     int n;
     scanf("%d", &n);
@@ -20,7 +20,14 @@ int main() {
         }
 
         for(int i = 0; i <= 9; i++) {
-            printf("%d ", arr[i][tr]);
-        }printf("\n");
+            arr[i][tr] %= 1000000000;
+        }
     }
+
+    int sum = 0;
+    for(int i = 0; i <= 9; i++) {
+        sum += arr[i][n - 1];
+    }
+
+    printf("%d\n", sum % 1000000000);
 }
