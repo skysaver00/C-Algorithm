025836209d62a00aa0415b13af0d0e3b19fa865e
skysaver00 <skysaver00@naver.com>
2021-05-04 23:03:00 +0900

    not solved

---
+++ 14924_VonNeumann_Paris/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_8ba67.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_8ba67.dir/testCXXCompiler.cxx.obj"
+  "cmTC_8ba67.exe"
+  "cmTC_8ba67.exe.manifest"
+  "cmTC_8ba67.pdb"
+  "libcmTC_8ba67.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_8ba67.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14924_VonNeumann_Paris/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_8ba67.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_8ba67.
+# This may be replaced when dependencies are built.
