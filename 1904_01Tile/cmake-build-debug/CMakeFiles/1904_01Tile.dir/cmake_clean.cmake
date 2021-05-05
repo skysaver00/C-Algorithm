ee0ea14b65a080c6a20e8be82b8542483530b67c
skysaver00 <skysaver00@naver.com>
2021-05-05 22:22:52 +0900

    not solved

---
+++ 1904_01Tile/cmake-build-debug/CMakeFiles/1904_01Tile.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1904_01Tile.exe"
+  "1904_01Tile.exe.manifest"
+  "1904_01Tile.pdb"
+  "CMakeFiles/1904_01Tile.dir/main.cpp.obj"
+  "lib1904_01Tile.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1904_01Tile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
