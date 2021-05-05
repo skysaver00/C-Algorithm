c84b1e4bcabfe4e002a427a0e1d9495f99892839
skysaver00 <skysaver00@naver.com>
2021-05-05 23:01:00 +0900

    solved Silver III

--- 9461_Wind_Wave_Permutation/main.cpp
+++ 9461_Wind_Wave_Permutation/main.cpp
@@ -10,16 +10,17 @@ int main() {
     arr[2] = 1;
     arr[3] = 1;
     arr[4] = 2;
+    arr[5] = 2;
 
-    for(int i = 5; i <= 100; i++) {
-        arr[i] = arr[i - 1] + arr[i - 4];
-    }
-
-    for(int i = 1; i <= 100; i++) {
-        cout << arr[i] << endl;
+    for(int i = 6; i <= 100; i++) {
+        arr[i] = arr[i - 1] + arr[i - 5];
     }
 
     while(t--) {
-
+        int n;
+        cin >> n;
+        cout << arr[n] << endl;
     }
+
+    return 0;
 }
