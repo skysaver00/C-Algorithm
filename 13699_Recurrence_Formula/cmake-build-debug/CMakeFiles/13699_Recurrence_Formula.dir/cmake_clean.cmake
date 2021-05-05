8d3d8ce4240f49fe5f673f2595919e4e8c4a57c0
skysaver00 <skysaver00@naver.com>
2021-05-05 22:12:44 +0900

    not solved

---
+++ 13699_Recurrence_Formula/cmake-build-debug/CMakeFiles/13699_Recurrence_Formula.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "13699_Recurrence_Formula.exe"
+  "13699_Recurrence_Formula.exe.manifest"
+  "13699_Recurrence_Formula.pdb"
+  "CMakeFiles/13699_Recurrence_Formula.dir/main.cpp.obj"
+  "lib13699_Recurrence_Formula.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/13699_Recurrence_Formula.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 13699_Recurrence_Formula/main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+long long arr[40];
+
+int main() {
+    int n;
+    scanf("%d", &n);
+}
