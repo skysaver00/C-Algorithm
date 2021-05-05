e6d86bb1f5ef4bf8f542b196c5180bd05f0f3885
skysaver00 <skysaver00@naver.com>
2021-05-05 17:49:33 +0900

    not solved

---
+++ 9656_Hearthstone_2/cmake-build-debug/CMakeFiles/9656_Hearthstone_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "9656_Hearthstone_2.exe"
+  "9656_Hearthstone_2.exe.manifest"
+  "9656_Hearthstone_2.pdb"
+  "CMakeFiles/9656_Hearthstone_2.dir/main.cpp.obj"
+  "lib9656_Hearthstone_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9656_Hearthstone_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
