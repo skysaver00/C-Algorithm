26914a387d860a15b8e3a20e58e4f366e8a1b012
skysaver00 <skysaver00@naver.com>
2021-05-05 22:54:31 +0900

    not solved

---
+++ 9461_Wind_Wave_Permutation/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_b449c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_b449c.dir/testCXXCompiler.cxx.obj"
+  "cmTC_b449c.exe"
+  "cmTC_b449c.exe.manifest"
+  "cmTC_b449c.pdb"
+  "libcmTC_b449c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_b449c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9461_Wind_Wave_Permutation/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_b449c.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_b449c.
+# This may be replaced when dependencies are built.
