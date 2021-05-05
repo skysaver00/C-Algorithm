6c9a52c4cfacf14dbc2790433cb51b5eaf1142fb
skysaver00 <skysaver00@naver.com>
2021-05-05 22:29:10 +0900

    not solved

--- 1904_01Tile/main.cpp
+++ 1904_01Tile/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
+int arr[1000001];
 
 int main() {
     int n;
     cin >> n;
 
-    
+    arr[0] = 1, arr[1] = 2;
+
+    for(int i = 2; i <= n; i++) {
+        arr[i] = arr[i - 2] + arr[i - 1];
+        arr[i] %= 15746;
+    }
+
+    printf("%d\n", arr[n]);
+
+    return 0;
 }
