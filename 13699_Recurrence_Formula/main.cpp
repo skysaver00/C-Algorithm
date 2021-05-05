7ee11305a1b2c7b48493140c57464481e75a6929
skysaver00 <skysaver00@naver.com>
2021-05-05 22:15:05 +0900

    not solved

--- 13699_Recurrence_Formula/main.cpp
+++ 13699_Recurrence_Formula/main.cpp
@@ -4,4 +4,17 @@ long long arr[40];
 int main() {
     int n;
     scanf("%d", &n);
+
+    arr[0] = 1;
+
+    for(int i = 1; i <= n; i++) {
+        arr[i] = 1;
+        for(int j = 0; j <= i; j++) {
+            arr[i] *= arr[j] * arr[i - j];
+        }
+
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
 }
