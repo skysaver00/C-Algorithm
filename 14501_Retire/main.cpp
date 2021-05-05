8ffbf898cf0837136c5364ef07b54cd0fdad5c3a
skysaver00 <skysaver00@naver.com>
2021-05-05 18:28:36 +0900

    solved Silver IV

--- 14501_Retire/main.cpp
+++ 14501_Retire/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-int workworkwork[1001][2];
-int total[1001];
+int workworkwork[1010][2];
+int total[1010];
 
 int max(int i , int j) {
     return i > j ? i : j;
@@ -15,9 +15,25 @@ int main() {
         cin >> workworkwork[i][0] >> workworkwork[i][1];
     }
 
-    for(int i = 1; i < n; i++) {
-        total[i + workworkwork[i][0]] = max(total[i + workworkwork[i][0]], );
+    for(int i = 0; i < n; i++) {
+        total[i + workworkwork[i][0]] = max(total[i] + workworkwork[i][1], total[i + workworkwork[i][0]]);
+        for(int j = i + workworkwork[i][0]; j < n; j++) {
+            if(total[j] < total[i + workworkwork[i][0]]) total[j] = total[i + workworkwork[i][0]];
+        }
+
+        /*printf("%d %d\n", total[i] + workworkwork[i][1], total[i + workworkwork[i][0]]);
+
+        for(int j = 0; j <= n; j++) {
+            printf("%d\n", total[j]);
+        }*/
     }
+
+    int ma = 0;
+    for(int i = 0; i <= n; i++) {
+        ma = max(ma, total[i]);
+    }
+
+    printf("%d\n", ma);
 
 
 
