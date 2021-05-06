4d6746ac8ea9a6f5ae1fe343b6d97e5acb86b686
skysaver00 <skysaver00@naver.com>
2021-05-06 17:13:52 +0900

    not solved

---
+++ 1965_Box_Fitting/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_1aba3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_1aba3.dir/CMakeCXXCompilerABI.cpp.obj"
+  "cmTC_1aba3.exe"
+  "cmTC_1aba3.exe.manifest"
+  "cmTC_1aba3.pdb"
+  "libcmTC_1aba3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_1aba3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
