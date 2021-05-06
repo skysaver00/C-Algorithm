885835ec85bf86bcb3c3b7b2159f2d0a9dda933d
skysaver00 <skysaver00@naver.com>
2021-05-06 19:49:33 +0900

    solved Silver II

---
+++ 11722_Decreasing_Permutation/cmake-build-debug/CMakeFiles/11722_Decreasing_Permutation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11722_Decreasing_Permutation.exe"
+  "11722_Decreasing_Permutation.exe.manifest"
+  "11722_Decreasing_Permutation.pdb"
+  "CMakeFiles/11722_Decreasing_Permutation.dir/main.cpp.obj"
+  "lib11722_Decreasing_Permutation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11722_Decreasing_Permutation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 11722_Decreasing_Permutation/main.cpp
+++ 11722_Decreasing_Permutation/main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <stdio.h>
+int arr[1001];
+int total[1001];
+
+int max(int i, int j) {
+    return i > j ? i : j;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(arr[i] < arr[j]) {
+                total[i] = max(total[j] + 1, total[i]);
+            }
+        }
+    }
+
+    int big = 0;
+    for(int i = 0; i < n; i++) {
+        if(big < total[i]) big = total[i];
+    }
+
+    printf("%d\n", big + 1);
+
     return 0;
 }
