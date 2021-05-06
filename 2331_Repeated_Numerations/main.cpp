7fe18f5ca58780225a3dca13924c5f9107f715f6
skysaver00 <skysaver00@naver.com>
2021-05-06 23:08:15 +0900

    not solved

--- 2331_Repeated_Numerations/main.cpp
+++ 2331_Repeated_Numerations/main.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int arr[400000];
+int arr[1000000];
 
 int main() {
     int a, p;
     cin >> a >> p;
 
+    int out;
+    int count = 1;
+
+    if(a == 1) {
+        cout << 0 << endl;
+        return 0;
+    }
+
     while(1) {
         int sum = 0;
         while(a != 0) {
-            sum += pow(a % 10, p);
+            int val = a % 10;
+            sum += (int)pow((double)val, (double)p);
             a /= 10;
         }
 
-        cout << sum << endl;
+        if(arr[sum] != 0) {
+            out = arr[sum];
+            cout << sum << endl;
+            break;
+        } else {
+            arr[sum] = count;
+        }
         a = sum;
+        count++;
         //if() break;
 
-
+        cout << sum << endl;
     }
+
+    cout << out << endl;
+    return 0;
 }
