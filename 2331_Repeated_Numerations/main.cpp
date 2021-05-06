4d5a4310c8297b41fee04059d16812023eec6308
skysaver00 <skysaver00@naver.com>
2021-05-06 23:25:57 +0900

    not solveD

--- 2331_Repeated_Numerations/main.cpp
+++ 2331_Repeated_Numerations/main.cpp
@@ -10,31 +10,29 @@ int main() {
     int out;
     int count = 1;
 
-    if(a == 1) {
-        cout << 0 << endl;
-        return 0;
-    }
-
+    int mir_a = a;
     while(1) {
         int sum = 0;
         while(a != 0) {
             int val = a % 10;
-            sum += (int)pow((double)val, (double)p);
+            sum += floor(pow((double)val, (double)p) + 0.4);
             a /= 10;
         }
 
         if(arr[sum] != 0) {
             out = arr[sum];
-            cout << sum << endl;
+            if(mir_a == sum) out--;
+            //cout << sum << endl;
             break;
         } else {
             arr[sum] = count;
         }
+
         a = sum;
         count++;
         //if() break;
 
-        cout << sum << endl;
+        //cout << sum << endl;
     }
 
     cout << out << endl;
