7acfea17df918cd1ded64bb0ddc6cfab34ccddd7
skysaver00 <skysaver00@naver.com>
2021-05-06 23:59:00 +0900

    not solved

--- 2331_Repeated_Numerations/main.cpp
+++ 2331_Repeated_Numerations/main.cpp
@@ -3,38 +3,34 @@
 using namespace std;
 int arr[1000000];
 
+int multiply(int a, int p) {
+    int sum = 0;
+    while(a != 0) {
+        int val = a % 10;
+        sum += floor(pow((double)val, (double)p) + 0.4);
+        a /= 10;
+    }
+
+    return sum;
+}
+
+void check(int a, int p) {
+    arr[a]++;
+    if(arr[a] > 2) return;
+
+    check(multiply(a, p), p);
+}
+
 int main() {
     int a, p;
     cin >> a >> p;
 
-    int out;
-    int count = 1;
+    check(a, p);
+    int count = 0;
 
-    int mir_a = a;
-    while(1) {
-        int sum = 0;
-        while(a != 0) {
-            int val = a % 10;
-            sum += floor(pow((double)val, (double)p) + 0.4);
-            a /= 10;
-        }
-
-        if(arr[sum] != 0) {
-            out = arr[sum];
-            if(mir_a == sum) out--;
-            //cout << sum << endl;
-            break;
-        } else {
-            arr[sum] = count;
-        }
-
-        a = sum;
-        count++;
-        //if() break;
-
-        //cout << sum << endl;
+    for(int i = 0; i < 1000000; i++) {
+        if(arr[i] == 1) count++;
     }
-
-    cout << out << endl;
+    cout << count << endl;
     return 0;
 }
