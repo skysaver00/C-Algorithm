8fa303445ff95d1f6438fd05f9b7ef976493c142
skysaver00 <skysaver00@naver.com>
2021-05-06 17:50:08 +0900

    not solved

--- 2407_Combination/main.cpp
+++ 2407_Combination/main.cpp
@@ -6,10 +6,25 @@ int main() {
     int n, m;
     scanf("%d %d", &n, &m);
 
-    int up = 1;
-    int down = 1;
-    for(int i = n + 1; i <= 100; i++) {
+    long long up = 1;
+    long long down = 1;
+
+    int r = m;
+
+    if(m < (n / 2)) m = 100 - n;
+    printf("%d\n", m);
+    for(int i = m + 1; i <= n; i++) {
         up *= i;
-        for(int j = )
+        down *= r;
+
+        if(up % down == 0) {
+            up /= down;
+            down = 1;
+        }
+        r--;
+
+        printf("%d %d %lld %lld\n", i, r, up, down);
     }
+
+     return 0;
 }
