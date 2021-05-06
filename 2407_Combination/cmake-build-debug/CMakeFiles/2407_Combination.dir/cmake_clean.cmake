a2efd7a67bcd5ee5677ef2e708fdc7f1914e557b
skysaver00 <skysaver00@naver.com>
2021-05-06 17:43:37 +0900

    not solved

---
+++ 2407_Combination/cmake-build-debug/CMakeFiles/2407_Combination.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2407_Combination.exe"
+  "2407_Combination.exe.manifest"
+  "2407_Combination.pdb"
+  "CMakeFiles/2407_Combination.dir/main.cpp.obj"
+  "lib2407_Combination.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2407_Combination.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2407_Combination/main.cpp
@@ -0,0 +1,15 @@
+//100! is too big!
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int up = 1;
+    int down = 1;
+    for(int i = n + 1; i <= 100; i++) {
+        up *= i;
+        for(int j = )
+    }
+}
