14d4d0fd88cc2810b213e4e3685d9792b8957de0
skysaver00 <skysaver00@naver.com>
2021-05-06 21:55:38 +0900

    not solved

--- 2331_Repeated_Numerations/main.cpp
+++ 2331_Repeated_Numerations/main.cpp
@@ -1,30 +1,23 @@
-#include <stdio.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int arr[400000];
 
-int count[10000];
 int main() {
-    int A, P;
-    scanf("%d %d", &A, &P);
+    int a, p;
+    cin >> a >> p;
 
-    int cnt = 1;
     while(1) {
-        int mir_A = A;
-
         int sum = 0;
-        while(mir_A) {
-            int ans = 1;
-            for(int i = 0; i < P; i++){
-                ans *= mir_A % 10;
-            }
-            sum += ans;
-            mir_A /= 10;
+        while(a != 0) {
+            sum += pow(a % 10, p);
+            a /= 10;
         }
-        printf("%d %d\n", sum, cnt);
-        A = sum;
 
-        if(count[A] != 0) {
-            printf("%d\n", count[A] + 1);
-            return 0;
-        }
-        count[A] = cnt++;
+        cout << sum << endl;
+        a = sum;
+        //if() break;
+
+
     }
 }
