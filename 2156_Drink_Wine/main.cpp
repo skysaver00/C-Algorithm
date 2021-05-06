425641a6c543cb5f60ac70b62387c66bacdbee74
skysaver00 <skysaver00@naver.com>
2021-05-07 03:04:45 +0900

    solved Silver I

--- 2156_Drink_Wine/main.cpp
+++ 2156_Drink_Wine/main.cpp
@@ -18,7 +18,8 @@ int main() {
     sum[1] = arr[0] + arr[1];
 
     sum[2] = max(sum[0] + arr[2], 0 + arr[1] + arr[2]);
-    
+    sum[2] = max(sum[2], sum[1]);
+
     for(int i = 3; i < n; i++) {
         sum[i] = max(sum[i - 2] + arr[i], sum[i - 3] + arr[i - 1] + arr[i]);
         sum[i] = max(sum[i - 1], sum[i]);
@@ -26,7 +27,6 @@ int main() {
 
     int big = 0;
     for(int i = 0; i < n; i++) {
-        printf("%d\t", sum[i]);
         big = max(big, sum[i]);
     }
     printf("%d\n", big);
