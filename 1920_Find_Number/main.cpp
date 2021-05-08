5f62d8d26b9e1c7d6efe60ce1c9fe1c63674785d
skysaver00 <skysaver00@naver.com>
2021-05-08 22:45:21 +0900

    solved Silver IV

--- 1920_Find_Number/main.cpp
+++ 1920_Find_Number/main.cpp
@@ -4,12 +4,38 @@ using namespace std;
 
 int arr[100001];
 
+bool binSearch(int value, int left, int right) {
+    int mid;
+
+    while(left <= right) {
+        mid = (left + right) / 2;
+
+        if(arr[mid] == value) return true;
+        else if(arr[mid] < value) left = mid + 1;
+        else if(arr[mid] > value) right = mid - 1;
+    }
+    return false;
+}
+
 int main() {
     int n;
     scanf("%d", &n);
 
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
+    }
+    sort(arr, arr + n);
+
+    int m;
+    scanf("%d", &m);
+
+    for(int i = 0; i < m; i++) {
+        int val;
+        scanf("%d", &val);
+
+        bool so = binSearch(val, 0, n);
+        if(so) printf("1\n");
+        else printf("0\n");
     }
 
     return 0;
