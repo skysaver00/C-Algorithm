1c4d70721994cb46086a3969dc4dfa4e7157362b
skysaver00 <skysaver00@naver.com>
2021-05-09 19:58:35 +0900

    solved Gold V

--- 2075_Nth_Biggest/main.cpp
+++ 2075_Nth_Biggest/main.cpp
@@ -2,16 +2,26 @@
 #include <queue>
 using namespace std;
 
-priority_queue<int> val;
+priority_queue<int, vector<int>, greater<int>> pq;
 
 int main() {
     int n;
+    scanf("%d", &n);
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             int val;
             scanf("%d", &val);
+
+            pq.push(val);
+
+            if(pq.size() == n + 1) {
+                pq.pop();
+            }
         }
     }
+
+    printf("%d\n", pq.top());
 
     return 0;
 }
