4ac176ee3fee035f8d7e5ae80dddb533eb501230
skysaver00 <skysaver00@naver.com>
2021-05-09 16:37:44 +0900

    solved Silver IV 이문제 기수정렬로 풀림

--- 10815_Number_Card(Radix)_____/main.cpp
+++ 10815_Number_Card(Radix)_____/main.cpp
@@ -1,9 +1,22 @@
 #include <stdio.h>
+bool radix[20000001];
 
 int main() {
     int n;
     scanf("%d", &n);
     for(int i = 0; i < n; i++) {
-        
+        int val;
+        scanf("%d", &val);
+        radix[val + 10000000] = true;
     }
+
+    int m;
+    scanf("%d", &m);
+    for(int i = 0; i < m; i++) {
+        int val;
+        scanf("%d", &val);
+        if(radix[val + 10000000]) printf("1 ");
+        else printf("0 " );
+    }
+    return 0;
 }
