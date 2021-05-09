edccc208b292edfc5d1c01fc3c4b8a13e5a3c0ad
skysaver00 <skysaver00@naver.com>
2021-05-09 18:19:56 +0900

    not solved

---
+++ 2805_Cutting_Tree/cmake-build-debug/CMakeFiles/2805_Cutting_Tree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2805_Cutting_Tree.exe"
+  "2805_Cutting_Tree.exe.manifest"
+  "2805_Cutting_Tree.pdb"
+  "CMakeFiles/2805_Cutting_Tree.dir/main.cpp.obj"
+  "lib2805_Cutting_Tree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2805_Cutting_Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2805_Cutting_Tree/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int arr[1000001];
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+
+    return 0;
+}
