bdbf0debfd371ff79642c702b8381b7d3f3a38bd
skysaver00 <skysaver00@naver.com>
2021-05-09 16:33:48 +0900

    solved Silver IV

--- 10815_Number_Card_____/main.cpp
+++ 10815_Number_Card_____/main.cpp
@@ -1,17 +1,16 @@
 #include <stdio.h>
+#include <algorithm>
+using namespace std;
 int arr[500001];
 
 bool binary(int number, int left, int right) {
     int mid;
-
     while(left <= right) {
         mid = (left + right) / 2;
-
         if(arr[mid] == number) return true;
         else if(arr[mid] > number) right = mid - 1;
         else if(arr[mid] < number) left = mid + 1;
     }
-
     return false;
 }
 
@@ -19,6 +18,7 @@ int main() {
     int n;
     scanf("%d", &n);
     for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    sort(arr, arr + n);
 
     int m;
     scanf("%d", &m);
@@ -30,6 +30,5 @@ int main() {
         if(tf) printf("1 ");
         else printf("0 ");
     }
-    
     return 0;
 }
