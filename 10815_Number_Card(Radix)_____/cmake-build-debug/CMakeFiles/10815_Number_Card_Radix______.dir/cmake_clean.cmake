82d1609a3d9d821193bedf1a396331f122e58598
skysaver00 <skysaver00@naver.com>
2021-05-09 16:35:28 +0900

    not solved

---
+++ 10815_Number_Card(Radix)_____/cmake-build-debug/CMakeFiles/10815_Number_Card_Radix______.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "10815_Number_Card_Radix______.exe"
+  "10815_Number_Card_Radix______.exe.manifest"
+  "10815_Number_Card_Radix______.pdb"
+  "CMakeFiles/10815_Number_Card_Radix______.dir/main.cpp.obj"
+  "lib10815_Number_Card_Radix______.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10815_Number_Card_Radix______.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
