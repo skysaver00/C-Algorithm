e42e74324faedf10463d3b66b0676a1251975830
skysaver00 <skysaver00@naver.com>
2021-05-09 18:26:46 +0900

    not solved

--- 2805_Cutting_Tree/main.cpp
+++ 2805_Cutting_Tree/main.cpp
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <algorithm>
+using namespace std;
 int arr[1000001];
+
+long long binary(int n, int m, int left, int right) {
+    int mid;
+    long long ans = 0;
+
+    while(left <= right) {
+        mid = (left + right) / 2;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] < mid) continue;
+            sum += arr[i] - mid;
+        }
+
+        if(sum >= m) {
+            if(ans < mid) ans = mid;
+            left = mid + 1;
+        } else right = mid - 1;
+    }
+
+    return ans;
+}
 
 int main() {
     int n, m;
@@ -8,8 +32,10 @@ int main() {
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+    sort(arr, arr + n);
 
-    
+    long long ans = binary(n, m, 0, arr[n - 1]);
+    printf("%lld\n", ans);
 
     return 0;
 }
