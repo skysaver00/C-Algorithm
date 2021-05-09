4ab41fa563688589e1bd05e2b5ac9b59f1418a91
skysaver00 <skysaver00@naver.com>
2021-05-09 18:30:29 +0900

    not solved

---
+++ 2075_Nth_Biggest/cmake-build-debug/CMakeFiles/2075_Nth_Biggest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2075_Nth_Biggest.exe"
+  "2075_Nth_Biggest.exe.manifest"
+  "2075_Nth_Biggest.pdb"
+  "CMakeFiles/2075_Nth_Biggest.dir/main.cpp.obj"
+  "lib2075_Nth_Biggest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2075_Nth_Biggest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2075_Nth_Biggest/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <queue>
+using namespace std;
+
+priority_queue<int> val;
+
+int main() {
+    int n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            int val;
+            scanf("%d", &val);
+        }
+    }
+
+    return 0;
+}
