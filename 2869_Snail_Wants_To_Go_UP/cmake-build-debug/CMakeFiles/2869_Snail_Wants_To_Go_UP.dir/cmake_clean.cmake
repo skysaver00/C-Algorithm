5db6956d780dd5064abbc224f7633145fd1c4c9a
skysaver00 <skysaver00@naver.com>
2021-05-10 23:10:48 +0900

    not solved

---
+++ 2869_Snail_Wants_To_Go_UP/cmake-build-debug/CMakeFiles/2869_Snail_Wants_To_Go_UP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2869_Snail_Wants_To_Go_UP.exe"
+  "2869_Snail_Wants_To_Go_UP.exe.manifest"
+  "2869_Snail_Wants_To_Go_UP.pdb"
+  "CMakeFiles/2869_Snail_Wants_To_Go_UP.dir/main.cpp.obj"
+  "lib2869_Snail_Wants_To_Go_UP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2869_Snail_Wants_To_Go_UP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2869_Snail_Wants_To_Go_UP/cmake-build-debug/CMakeFiles/2869_Snail_Wants_To_Go_UP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 2869_Snail_Wants_To_Go_UP.
+# This may be replaced when dependencies are built.
