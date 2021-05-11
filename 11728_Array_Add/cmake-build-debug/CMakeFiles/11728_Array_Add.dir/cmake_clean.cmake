84eb46a9d90e9ac3bd42b75e15dbabeca85beea7
skysaver00 <skysaver00@naver.com>
2021-05-11 13:54:22 +0900

    not solved

---
+++ 11728_Array_Add/cmake-build-debug/CMakeFiles/11728_Array_Add.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11728_Array_Add.exe"
+  "11728_Array_Add.exe.manifest"
+  "11728_Array_Add.pdb"
+  "CMakeFiles/11728_Array_Add.dir/main.cpp.obj"
+  "lib11728_Array_Add.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11728_Array_Add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11728_Array_Add/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int nrr[1000001];
+int mrr[1000001];
+int sumarr[2000001];
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++) scanf("%d", &nrr[i]);
+    for(int i = 0; i < m; i++) scanf("%d", &mrr[i]);
+
+    sort(nrr, nrr + n);
+    sort(mrr, mrr + m);
+
+    int i, j, k;
+    i = j = k = 0;
+
+    while(i <= n || j <= m) {
+        if(nrr[i] < mrr[j]) {
+            sumarr[k] = nrr[i];
+            i++, k++;
+        } else if(nrr[i] > mrr[j]) {
+            sumarr[k] = mrr[j];
+            j++, k++;
+        } else {
+            sumarr[k] = nrr[i];
+            i++, k++;
+            sumarr[k] = mrr[j];
+            j++, k++;
+        }
+    }
+
+    for(int l = 0; l < k; l++) {
+        printf("%d ", sumarr[l]);
+    }printf("\n");
+
+    return 0;
+}
