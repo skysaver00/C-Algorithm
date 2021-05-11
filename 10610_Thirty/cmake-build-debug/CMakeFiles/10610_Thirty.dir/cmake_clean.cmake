108ed9232c43721a15d87ebfc2aadb189220ecbe
skysaver00 <skysaver00@naver.com>
2021-05-11 14:15:13 +0900

    not solved

---
+++ 10610_Thirty/cmake-build-debug/CMakeFiles/10610_Thirty.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "10610_Thirty.exe"
+  "10610_Thirty.exe.manifest"
+  "10610_Thirty.pdb"
+  "CMakeFiles/10610_Thirty.dir/main.cpp.obj"
+  "lib10610_Thirty.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10610_Thirty.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 10610_Thirty/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include 
+char number[100005];
+int one[10];
+
+int main() {
+    scanf("%s", number);
+
+
+}
