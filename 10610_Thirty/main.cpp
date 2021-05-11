f7f08665fc230161c2d4e2071dbe3eb9c42911bc
skysaver00 <skysaver00@naver.com>
2021-05-11 14:26:10 +0900

    solved Silver V

--- 10610_Thirty/main.cpp
+++ 10610_Thirty/main.cpp
@@ -1,10 +1,39 @@
 #include <cstdio>
-#include 
+#include <cstring>
 char number[100005];
 int one[10];
 
 int main() {
     scanf("%s", number);
+    int len = strlen(number);
 
+    bool ans = false;
+    for(int i = 0; i < len; i++) {
+        if(number[i] == '0') ans = true;
+        one[number[i] - 0x30]++;
+    }
 
+    if(!ans) {
+        printf("-1\n");
+        return 0;
+    }
+
+    int sum = 0;
+    for(int i = 9; i >= 0; i--) {
+        for(int j = one[i]; j > 0; j--) {
+            sum += i;
+        }
+    }
+
+    if(sum % 3 != 0) {
+        printf("-1\n");
+        return 0;
+    }
+
+    for(int i = 9; i >= 0; i--) {
+        for(int j = one[i]; j > 0; j--) {
+            printf("%d", i);
+        }
+    }printf("\n");
+    return 0;
 }
