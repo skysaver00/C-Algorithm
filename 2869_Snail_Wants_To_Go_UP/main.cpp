390b4f25f065cecfe3128a1d88c439c0022123fb
skysaver00 <skysaver00@naver.com>
2021-05-16 22:25:36 +0900

    solved Bronze I

--- 2869_Snail_Wants_To_Go_UP/main.cpp
+++ 2869_Snail_Wants_To_Go_UP/main.cpp
@@ -4,11 +4,11 @@ int main () {
     int a, b, v;
     scanf("%d %d %d", &a, &b, &v);
 
-    int vmin = v - a;
+    int vmin = v - b;
     int dvd = vmin / (a - b);
 
     if(vmin % (a - b) != 0) dvd++;
-    printf("%d\n", vmin);
+    printf("%d\n", dvd);
 
     return 0;
 }
