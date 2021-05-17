3769d56339a54055316b958ce375c8b4451bb9f0
skysaver00 <skysaver00@naver.com>
2021-05-17 18:55:17 +0900

    not solved

--- 2583_Finding_The_Area/main.cpp
+++ 2583_Finding_The_Area/main.cpp
@@ -1,7 +1,29 @@
 #include <cstdio>
 #include <queue>
 using namespace std;
+int axis[101][101];
+int cpaxis[101][101];
 
 int main() {
+    int m, n, k;
+    scanf("%d %d %d", &m, &n, &k);
 
+    for(int i = 0; i < k; i++) {
+        int lx, ly, rx, ry;
+        scanf("%d %d %d %d", &lx, &ly, &rx, &ry);
+
+        for(int j = lx; j < rx; j++) {
+            for(int k = ly; k < ry; k++) {
+                axis[k][j] = 1;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            printf("%d ", axis[i][j]);
+        }printf("\n");
+    }
+
+    return 0
 }
