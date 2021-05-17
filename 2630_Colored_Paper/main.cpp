c72df4e4891c00e535faa99ce835d9161efd9f60
skysaver00 <skysaver00@naver.com>
2021-05-17 22:25:30 +0900

    not solved

--- 2630_Colored_Paper/main.cpp
+++ 2630_Colored_Paper/main.cpp
@@ -1,6 +1,27 @@
 #include <cstdio>
 int paper[130][130];
 
+void solve(int y, int x, int n) {
+    int check = paper[y][x];
+    for(int i = y; i < y + n; i++) {
+        for(int j = x; j < x + n; j++) {
+            if(check == 0 && paper[i][j] == 1) check = 2;
+            else if(check == 1 && paper[i][j] == 0) check = 2;
+        }
+    }
+}
+
 int main () {
-    
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &paper[i][j]);
+        }
+    }
+
+
+
+    return 0;
 }
