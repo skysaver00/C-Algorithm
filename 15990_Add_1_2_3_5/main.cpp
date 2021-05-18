8d3907b2f2f195567bacfaf01bb5eacbf554ccd6
skysaver00 <skysaver00@naver.com>
2021-05-18 16:01:01 +0900

    solved Silver III

--- 15990_Add_1_2_3_5/main.cpp
+++ 15990_Add_1_2_3_5/main.cpp
@@ -3,6 +3,29 @@ using namespace std;
 long long arr[100001][3];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int t;
+    cin >> t;
+
+    arr[1][0] = 1;
+    arr[2][1] = 1;
+    arr[3][0] = arr[3][1] = arr[3][2] = 1;
+
+    for(int i = 4; i <= 100000; i++) {
+        arr[i][0] += arr[i - 1][1] + arr[i - 1][2];
+        arr[i][1] += arr[i - 2][0] + arr[i - 2][2];
+        arr[i][2] += arr[i - 3][0] + arr[i - 3][1];
+        arr[i][0] %= 1000000009;
+        arr[i][1] %= 1000000009;
+        arr[i][2] %= 1000000009;
+    }
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        long long sum = ((arr[n][0] + arr[n][1] + arr[n][2]) % 1000000009);
+        cout << sum << endl;
+    }
+
     return 0;
 }
