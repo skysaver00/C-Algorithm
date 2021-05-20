65e736b16b6e45571c28dd512b4859cdf993db92
skysaver00 <skysaver00@naver.com>
2021-05-20 16:37:48 +0900

    solved Silver III

--- 2630_Colored_Paper/main.cpp
+++ 2630_Colored_Paper/main.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 int paper[130][130];
+int blue = 0;
+int white = 0;
 
 void solve(int y, int x, int n) {
     int check = paper[y][x];
@@ -8,7 +10,18 @@ void solve(int y, int x, int n) {
             if(check == 0 && paper[i][j] == 1) check = 2;
             else if(check == 1 && paper[i][j] == 0) check = 2;
         }
+        if(check == 2) {
+            solve(y, x, n / 2);
+            solve(y, x + (n / 2), n / 2);
+            solve(y + (n / 2) , x, n / 2);
+            solve(y + (n / 2), x + (n / 2), n / 2);
+
+            return;
+        }
     }
+
+    if(check == 1) blue++;
+    else white++;
 }
 
 int main () {
@@ -21,6 +34,8 @@ int main () {
         }
     }
     solve(0, 0, n);
+
+    printf("%d\n%d\n", white, blue);
 
     return 0;
 }
