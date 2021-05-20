34be80f964811e3579b5aa4353da50d734cb8a2c
skysaver00 <skysaver00@naver.com>
2021-05-20 17:06:19 +0900

    not solved

--- 1074_Z_____/main.cpp
+++ 1074_Z_____/main.cpp
@@ -10,13 +10,13 @@ int solve(int n, int r, int c) {
         total += 0;
         solve(n - 1, r, c);
     } else if(r < pow(2, n - 1) && c >= pow(2, n - 1)) {
-        total += int(pow(2, (n - 1) * 2));
+        total += pow(2, (n - 1) * 2);
         solve(n - 1, r, c - int(pow(2, n - 1)));
     } else if(r >= pow(2, n - 1) && c < pow(2, n - 1)) {
-        total += (2 * int(pow(2, (n - 1) * 2)));
+        total += (2 * pow(2, (n - 1) * 2));
         solve(n - 1, r - int(pow(2, n - 1)), c);
     } else if(r >= pow(2, n - 1) && c >= pow(2, n - 1)) {
-        total += (3 * int(pow(2, (n - 1) * 2)));
+        total += (3 * pow(2, (n - 1) * 2));
         solve(n - 1, r - int(pow(2, n - 1)), c - int(pow(2, n - 1)));
     }
 }
@@ -24,6 +24,6 @@ int solve(int n, int r, int c) {
 int main() {
     scanf("%d %d %d", &n, &r, &c);
 
-    solve(n, r, c);
-    printf("%d\n", total);
+    int val = solve(n, r, c);
+    printf("%d\n", val);
 }
