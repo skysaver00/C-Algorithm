0dda8b17653740d68d2193e2785309e889f221e7
skysaver00 <skysaver00@naver.com>
2021-05-20 16:49:15 +0900

    not solved

---
+++ 1074_Z_____/cmake-build-debug/CMakeFiles/1074_Z_____.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1074_Z_____.exe"
+  "1074_Z_____.exe.manifest"
+  "1074_Z_____.pdb"
+  "CMakeFiles/1074_Z_____.dir/main.cpp.obj"
+  "lib1074_Z_____.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1074_Z_____.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
