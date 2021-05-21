323b2f2ee1e76c9a1fd2ffed4b3af1267f22df2a
skysaver00 <skysaver00@naver.com>
2021-05-21 23:38:03 +0900

    not solved

--- 1780_Paper_Count/main.cpp
+++ 1780_Paper_Count/main.cpp
@@ -1,56 +1,46 @@
 #include <stdio.h>
+
 int arr[2190][2190];
 int one, zero, minus;
 
 void solve(int y, int x, int n) {
     int check = arr[y][x];
-    int flag = 0;
-    for(int i = y; i < n; i++) {
-        for(int j = x; j < n; j++) {
-            if(check == 0 && (arr[i][j] == 1 || arr[i][j] == -1)) {
-                check = 2;
-                flag = 1;
-                break;
-            } else if(check == 1 && (arr[i][j] == 0 || arr[i][j] == -1)) {
-                check = 2;
-                flag = 1;
-                break;
-            } else if(check == -1 && (arr[i][j] == 0 || arr[i][j] == 1)) {
-                check = 2;
-                flag = 1;
-                break;
-            }
+    for (int i = y; i < y + n; i++) {
+        for (int j = x; j < x + n; j++) {
+            if (check == 0 && (arr[i][j] == 1 || arr[i][j] == -1)) check = 2;
+            else if (check == 1 && (arr[i][j] == 0 || arr[i][j] == -1)) check = 2;
+            else if (check == -1 && (arr[i][j] == 0 || arr[i][j] == 1)) check = 2;
         }
-        if(flag == 1) {
-            break;
+        if (check == 2) {
+            for (int a = 0; a < 3; a++) {
+                for (int b = 0; b < 3; b++) {
+                    solve(y + (a * (n / 3)), x + (b * (n / 3)), n / 3);
+                }
+            }
+            return;
         }
     }
 
-    if(check == 2) {
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                solve(y + (i * (n /3)), x + (i * (n / 3)), n / 3);
-            }
-        }
-    }
 
-    if(check == 1) one++;
-    else if(check == 0) zero++;
-    else if(check == -1) minus++;
+    printf("%d %d %d\n", y, x, n);
+
+    if (check == 1) one++;
+    else if (check == 0) zero++;
+    else if (check == -1) minus++;
 }
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             scanf("%d", &arr[i][j]);
         }
     }
 
     solve(0, 0, n);
-    
+    printf("%d %d %d\n", one, zero, minus);
 
     return 0;
 }
