e8f464da4d4110514978ce99cefbd408c9a8ec6a
skysaver00 <skysaver00@naver.com>
2021-05-22 17:11:58 +0900

    not solved

---
+++ 1992_Quad_Tree/cmake-build-debug/CMakeFiles/1992_Quad_Tree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1992_Quad_Tree.exe"
+  "1992_Quad_Tree.exe.manifest"
+  "1992_Quad_Tree.pdb"
+  "CMakeFiles/1992_Quad_Tree.dir/main.cpp.obj"
+  "lib1992_Quad_Tree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1992_Quad_Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1992_Quad_Tree/cmake-build-debug/CMakeFiles/1992_Quad_Tree.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1992_Quad_Tree.
+# This may be replaced when dependencies are built.
