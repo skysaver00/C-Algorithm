7310aa08fd6d57f17eee0d23ebbf6c18da7d0b58
skysaver00 <skysaver00@naver.com>
2021-05-22 17:13:57 +0900

    not solved

--- 1992_Quad_Tree/main.cpp
+++ 1992_Quad_Tree/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <cstdio>
+
+int arr[65][65];
+
+void solve(int y, int x, int n) {
+    int check = arr[y][x];
+    for(int i = y; i < y + n; i++) {
+        for(int j = x; j < x + n; j++) {
+            
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
