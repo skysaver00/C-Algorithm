35bb619ead3d334b18b1d49bb1b42c9654c69ff2
skysaver00 <skysaver00@naver.com>
2021-05-22 17:43:08 +0900

    not solved

--- 1992_Quad_Tree/main.cpp
+++ 1992_Quad_Tree/main.cpp
@@ -1,7 +1,9 @@
 #include <cstdio>
 #include <queue>
+#include <cstdlib>
 using namespace std;
 
+char var[65][65];
 int arr[65][65];
 queue<char> val;
 
@@ -9,20 +11,23 @@ void solve(int y, int x, int n) {
     int check = arr[y][x];
     for(int i = y; i < y + n; i++) {
         for(int j = x; j < x + n; j++) {
-            if(check == 0 && arr[y][x] == 1) check = 2;
-            else if(check == 1 && arr[y][x] == 0) check = 2;
+            if(check == 0 && arr[y][x] == '1') check = 2;
+            else if(check == 1 && arr[y][x] == '0') check = 2;
         }
 
         if(check == 2) {
+            printf("%d %d %d\n", y, x, n / 2);
             val.push('(');
             solve(y, x, n / 2);
             solve(y, x + (n / 2), n / 2);
             solve(y + (n / 2), x, n / 2);
             solve(y + (n / 2), x + (n / 2), n / 2);
             val.push(')');
+
+            return;
         }
     }
-
+    printf("%d %d %d\n", y, x, n / 2);
     if(check == 1) val.push('1');
     else if(check == 0) val.push('0');
 }
@@ -32,9 +37,8 @@ int main() {
     scanf("%d", &n);
 
     for(int i = 0; i < n ; i++) {
-        for(int j = 0; j < n; j++) {
-            scanf("%d", &arr[i][j]);
-        }
+        scanf("%s", var[i]);
+        printf("%s", var[i]);
     }
 
     val.push('(');
