22999a20ee1d38e3f486e07e9b7a9e83b241c0ce
skysaver00 <skysaver00@naver.com>
2021-05-23 22:06:01 +0900

    not solved

--- 2447_Stars_10/main.cpp
+++ 2447_Stars_10/main.cpp
@@ -5,14 +5,14 @@ char arr[2200][2200];
 
 void print(int n, int x, int y, int t) {
     if(t == 5) {
-        int k = pow(3, n);
+        int k = n;
         for(int i = x; i < x + k; i++) {
             for(int j = y; j < y + k; j++) {
                 arr[i][j] = ' ';
             }
         }
     }
-    if(n == 1) {
+    if(n == 3) {
         for(int i = x; i < x + 3; i++) {
             for(int j = y; j < y + 3; j++) {
                 if(i == x + 1 && j == y + 1) {
@@ -26,8 +26,8 @@ void print(int n, int x, int y, int t) {
     else {
         for(int i = 0; i < 3; i++) {
             for(int j = 1; j <= 3; j++) {
-                printf("%d %d %d %d\n", n, x + (int)(i * pow(3, n)), ((j - 1) * (int)pow(3, n)), t);
-                print(n - 1, x + (int)(i * pow(3, n)), ((j - 1) * (int)pow(3, n)), (i * 3) + j);
+                printf("%d %d %d %d\n", n, x + i * (n / 3), (j - 1) * (n / 3), t);
+                print(n / 3, x + i * (n / 3), (j - 1) * (n / 3), (i * 3) + j);
             }
         }
         return;
@@ -38,5 +38,5 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    print(n, 0, 0, 1);
+    print(n, 0, 0, 0);
 }
