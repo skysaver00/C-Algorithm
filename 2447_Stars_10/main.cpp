1ca1cc43e43162036498c3060adb22db09261348
skysaver00 <skysaver00@naver.com>
2021-05-23 22:02:45 +0900

    not solved

--- 2447_Stars_10/main.cpp
+++ 2447_Stars_10/main.cpp
@@ -6,22 +6,28 @@ char arr[2200][2200];
 void print(int n, int x, int y, int t) {
     if(t == 5) {
         int k = pow(3, n);
-        for(int i = 0; i < k; i++) {
-            for(int j = 0; j < k; j++) {
-                printf(" ");
-            }printf("\n");
+        for(int i = x; i < x + k; i++) {
+            for(int j = y; j < y + k; j++) {
+                arr[i][j] = ' ';
+            }
         }
     }
     if(n == 1) {
-        for(int i = 0; i < 3; i++) {
-
+        for(int i = x; i < x + 3; i++) {
+            for(int j = y; j < y + 3; j++) {
+                if(i == x + 1 && j == y + 1) {
+                    arr[i][j] = ' ';
+                    continue;
+                }
+                arr[i][j] = '*';
+            }
         }
     }
     else {
         for(int i = 0; i < 3; i++) {
             for(int j = 1; j <= 3; j++) {
-                printf("%d %d\n", n, t);
-                print(n - 1, (i * 3) + j);
+                printf("%d %d %d %d\n", n, x + (int)(i * pow(3, n)), ((j - 1) * (int)pow(3, n)), t);
+                print(n - 1, x + (int)(i * pow(3, n)), ((j - 1) * (int)pow(3, n)), (i * 3) + j);
             }
         }
         return;
