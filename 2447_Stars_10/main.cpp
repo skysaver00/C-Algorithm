af2e2d1a287b37dcd2da4302be14121efe94d6d9
skysaver00 <skysaver00@naver.com>
2021-05-23 21:57:46 +0900

    not solved

--- 2447_Stars_10/main.cpp
+++ 2447_Stars_10/main.cpp
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <math.h>
-void print(int n, int t) {
+
+char arr[2200][2200];
+
+void print(int n, int x, int y, int t) {
     if(t == 5) {
         int k = pow(3, n);
         for(int i = 0; i < k; i++) {
             for(int j = 0; j < k; j++) {
-                
-            }
+                printf(" ");
+            }printf("\n");
         }
     }
     if(n == 1) {
+        for(int i = 0; i < 3; i++) {
+
+        }
     }
     else {
         for(int i = 0; i < 3; i++) {
             for(int j = 1; j <= 3; j++) {
+                printf("%d %d\n", n, t);
                 print(n - 1, (i * 3) + j);
             }
         }
+        return;
     }
 }
 
@@ -24,5 +32,5 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    print(n, 1);
+    print(n, 0, 0, 1);
 }
