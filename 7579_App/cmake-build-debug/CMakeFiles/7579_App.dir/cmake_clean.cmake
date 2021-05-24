6bc8d84617b961d5ac182cb8f98292c892cb783a
skysaver00 <skysaver00@naver.com>
2021-05-24 22:44:30 +0900

    not solved

---
+++ 7579_App/cmake-build-debug/CMakeFiles/7579_App.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "7579_App.exe"
+  "7579_App.exe.manifest"
+  "7579_App.pdb"
+  "CMakeFiles/7579_App.dir/main.cpp.obj"
+  "lib7579_App.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/7579_App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
