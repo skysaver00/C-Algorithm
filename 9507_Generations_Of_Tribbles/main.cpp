809ee82736465f34381917138649b6a561ab1d3c
skysaver00 <skysaver00@naver.com>
2021-05-25 17:44:39 +0900

    solved Silver III

--- 9507_Generations_Of_Tribbles/main.cpp
+++ 9507_Generations_Of_Tribbles/main.cpp
@@ -1,1 +1,17 @@
-//큰수 연산 여기서 못한다.
+#include <stdio.h>
+long long arr[70];
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    arr[0] = 1, arr[1] = 1, arr[2] = 2, arr[3] = 4;
+    for(int i = 4; i <= 67; i++) arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3] + arr[i - 4];
+
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+        printf("%lld\n", arr[n]);
+    }
+    return 0;
+}
