dbab299c610f6126b354c8a52abaaaf6f9272c55
skysaver00 <skysaver00@naver.com>
2021-05-25 17:36:16 +0900

    not solved

---
+++ 9507_Generations_Of_Tribbles/cmake-build-debug/CMakeFiles/9507_Generations_Of_Tribbles.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "9507_Generations_Of_Tribbles.exe"
+  "9507_Generations_Of_Tribbles.exe.manifest"
+  "9507_Generations_Of_Tribbles.pdb"
+  "CMakeFiles/9507_Generations_Of_Tribbles.dir/main.cpp.obj"
+  "lib9507_Generations_Of_Tribbles.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9507_Generations_Of_Tribbles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9507_Generations_Of_Tribbles/cmake-build-debug/CMakeFiles/9507_Generations_Of_Tribbles.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 9507_Generations_Of_Tribbles.
+# This may be replaced when dependencies are built.
