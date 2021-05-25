723d5fe38c23958a329c77b38fee930c7f0fdfe7
skysaver00 <skysaver00@naver.com>
2021-05-26 03:07:27 +0900

    not solveD

--- 1753_Shortest_Path/main.cpp
+++ 1753_Shortest_Path/main.cpp
@@ -8,6 +8,25 @@ int INF = 2147483647;
 void dijkstra(int k) {
     val[k] = 0;
     priority_queue<pair<int, int>> pq;
+    pq.push(make_pair(k, 0));
+
+    while(!pq.empty()) {
+        int curr = pq.top().first;
+        int dist = -pq.top().second;
+
+        pq.pop();
+        if(val[curr] < dist) continue;
+        int size = arr[curr].size();
+        for(int i = 0; i < size; i++) {
+            int next = arr[curr][i].first;
+            int nextDist = dist + arr[curr][i].second;
+
+            if(nextDist < val[next]) {
+                val[next] = nextDist;
+                pq.push(make_pair(next, -nextDist));
+            }
+        }
+    }
 }
 
 int main() {
@@ -26,6 +45,9 @@ int main() {
     }
 
     dijkstra(k);
+    for(int i = 1; i <= v; i++) {
+        printf("%d\n", val[i]);
+    }
 
     return 0;
 }
