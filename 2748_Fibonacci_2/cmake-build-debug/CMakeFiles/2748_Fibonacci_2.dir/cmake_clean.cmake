603e7a5291fde4071bc8e5af4350b9cc240cadd4
skysaver00 <skysaver00@naver.com>
2021-05-25 16:21:32 +0900

    solved Bronze I

---
+++ 2748_Fibonacci_2/cmake-build-debug/CMakeFiles/2748_Fibonacci_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2748_Fibonacci_2.exe"
+  "2748_Fibonacci_2.exe.manifest"
+  "2748_Fibonacci_2.pdb"
+  "CMakeFiles/2748_Fibonacci_2.dir/main.cpp.obj"
+  "lib2748_Fibonacci_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2748_Fibonacci_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2748_Fibonacci_2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+long long arr[91];
+
+int main() {
+    int n;
+    cin >> n;
+
+    arr[1] = 1;
+    arr[2] = 1;
+
+    for(int i = 3; i <= n; i++) arr[i] = arr[i - 1] + arr[i - 2];
+    cout << arr[n] << endl;
+    return 0;
+}
