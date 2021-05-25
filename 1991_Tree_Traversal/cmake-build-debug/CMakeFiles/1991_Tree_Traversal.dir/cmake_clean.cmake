9febf7fe254cd942eb5b077952681ef7311c528c
skysaver00 <skysaver00@naver.com>
2021-05-25 23:53:59 +0900

    not solved

---
+++ 1991_Tree_Traversal/cmake-build-debug/CMakeFiles/1991_Tree_Traversal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1991_Tree_Traversal.exe"
+  "1991_Tree_Traversal.exe.manifest"
+  "1991_Tree_Traversal.pdb"
+  "CMakeFiles/1991_Tree_Traversal.dir/main.cpp.obj"
+  "lib1991_Tree_Traversal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1991_Tree_Traversal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
