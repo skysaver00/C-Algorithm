fe5ae94235ff5967ebabd36be0f0264a7f6066f2
skysaver00 <skysaver00@naver.com>
2021-05-25 15:54:09 +0900

    solved Silver III

--- 11726_2xN_Tiling/main.cpp
+++ 11726_2xN_Tiling/main.cpp
@@ -4,15 +4,11 @@ long long arr[1001];
 int main() {
     int n;
     scanf("%d", &n);
-
     arr[1] = 1;
     arr[2] = 2;
 
-    for(int i = 3; i <= n; i++) {
-        arr[i] = (arr[i - 2] + arr[i - 1]) % 10007;
-    }
+    for(int i = 3; i <= n; i++) arr[i] = (arr[i - 2] + arr[i - 1]) % 10007;
 
-    printf("%d\n" arr[n]);
-
+    printf("%lld\n", arr[n]);
     return 0;
 }
