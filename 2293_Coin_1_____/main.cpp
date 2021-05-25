2288964aa99d6fdd0fdec76601ee178239462391
skysaver00 <skysaver00@naver.com>
2021-05-25 16:46:47 +0900

    not solved

--- 2293_Coin_1_____/main.cpp
+++ 2293_Coin_1_____/main.cpp
@@ -10,6 +10,12 @@ int main () {
         dp[coin[i]] = 1;
     }
 
+    for(int i = 0; i < n; i++) {
+        for(int j = 1; j <= k; j++) {
+            
+        }
+    }
+
     for(int i = 1; i <= k; i++) {
         for(int j = 0; j < n; j++) {
             if(i - coin[j] < 0);
