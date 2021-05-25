a82915498e4d8d59020598600328dee7a2718cee
skysaver00 <skysaver00@naver.com>
2021-05-25 14:07:21 +0900

    solved Gold III

--- 7579_App/main.cpp
+++ 7579_App/main.cpp
@@ -1,8 +1,6 @@
 #include <cstdio>
-
 int mem[101];
 int off[101];
-
 int dp[10001];
 
 int max(int i, int j) {
@@ -23,21 +21,17 @@ int main() {
         sum += off[i];
     }
 
+    int min = 10001;
     for(int i = 0; i < n; i++) {
-        for(int j = sum; j >= off[i]; j--) {
-            dp[j] = max(dp[j], dp[j - off[i]] + mem[i]);
-        }
-
-        int flag = 0;
+        for(int j = sum; j >= off[i]; j--) dp[j] = max(dp[j], dp[j - off[i]] + mem[i]);
         for(int i = 0; i <= sum; i++) {
-            if(dp[i] >= m) {
-                printf("%d\n", i);
-                flag = 1;
-                break;
+            if(dp[i] >= m && i < min) {
+                min = i;
+                continue;
             }
         }
-        if(flag == 1) break;
     }
 
+    printf("%d\n", min);
     return 0;
 }
