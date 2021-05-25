abb2de94ece10bdd3868059fc50a7400a8612a02
skysaver00 <skysaver00@naver.com>
2021-05-25 15:52:30 +0900

    not solveD

---
+++ 11726_2xN_Tiling/cmake-build-debug/CMakeFiles/11726_2xN_Tiling.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11726_2xN_Tiling.exe"
+  "11726_2xN_Tiling.exe.manifest"
+  "11726_2xN_Tiling.pdb"
+  "CMakeFiles/11726_2xN_Tiling.dir/main.cpp.obj"
+  "lib11726_2xN_Tiling.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11726_2xN_Tiling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11726_2xN_Tiling/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+long long arr[1001];
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    arr[1] = 1;
+    arr[2] = 2;
+
+    for(int i = 3; i <= n; i++) {
+        arr[i] = (arr[i - 2] + arr[i - 1]) % 10007;
+    }
+
+    printf("%d\n" arr[n]);
+
+    return 0;
+}
