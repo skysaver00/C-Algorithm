e595206d0dec9cb1918ec9fd15db4b8dffced733
skysaver00 <skysaver00@naver.com>
2021-05-26 02:32:23 +0900

    not solved

--- 1238_Party/main.cpp
+++ 1238_Party/main.cpp
@@ -2,6 +2,14 @@
 #define INF (unsigned)!((int)0)
 int point[1001][1001];
 
+void dijk(int n) {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            printf("%d ", point[i][j]);
+        }printf("\n");
+    }
+}
+
 int main() {
     int n, m, x;
     scanf("%d %d %d", &n, &m, &x);
@@ -15,7 +23,10 @@ int main() {
     for(int i = 0; i < m; i++) {
         int from, to, len;
         scanf("%d %d %d", &from, &to, &len);
+        point[from][to] = len;
     }
+
+    dijk(n);
 
     return 0;
 }
