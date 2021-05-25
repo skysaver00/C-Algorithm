e67c91f1d6d9fb33ad912b8cba02845fafa8200d
skysaver00 <skysaver00@naver.com>
2021-05-26 00:50:41 +0900

    solved Silver II 문제의 난이도가 Gold가 아닌가 싶다.

--- 11725_Find_Tree_Parent/main.cpp
+++ 11725_Find_Tree_Parent/main.cpp
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <vector>
+using namespace std;
 int n;
-int arr[100001][3];
-int check[100001];
-int root[100001];
+vector<int> arr[100005];
+int check[100005];
+int root[100005];
 
 void dfs(int i) {
     if(check[i] == 1) return;
     check[i] = 1;
 
-    for(int j = 0; j < 3; j++) {
+    int t = arr[i].size();
+    for(int j = 0; j < t; j++) {
         if(arr[i][j] != 0 && check[arr[i][j]] == 0) {
             root[arr[i][j]] = i;
             dfs(arr[i][j]);
@@ -23,21 +26,10 @@ int main() {
     for(int i = 0; i < n - 1; i++) {
         int one, two;
         scanf("%d %d", &one, &two);
-
-        for(int j = 0; j < 3; j++) {
-            if(arr[one][j] == 0) {
-                arr[one][j] = two;
-                break;
-            }
-        }
-
-        for(int j = 0; j < 3; j++) {
-            if(arr[two][j] == 0) {
-                arr[two][j] = one;
-                break;
-            }
-        }
+        arr[one].push_back(two);
+        arr[two].push_back(one);
     }
+
     dfs(1);
     for(int i = 2; i <= n; i++) printf("%d\n", root[i]);
 
