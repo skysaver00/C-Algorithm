933d425768e66ec0fc8fc866f27a7b02a7653a17
skysaver00 <skysaver00@naver.com>
2021-05-25 16:17:45 +0900

    solved Silver I

--- 2156_Drink_Wine_____/main.cpp
+++ 2156_Drink_Wine_____/main.cpp
@@ -14,9 +14,15 @@ int main() {
     dp[1] = arr[1];
     dp[2] = dp[1] + arr[2];
 
+    dp[3] = max(dp[1] + arr[3], arr[2] + arr[3]);
+    dp[3] = max(dp[3], dp[2]);
+
     for(int i = 3; i <= n; i++) {
-        dp[i] = max(dp[i - 1] + arr[i])
+        dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
+        dp[i] = max(dp[i - 1], dp[i]);
     }
+
+    printf("%d\n", dp[n]);
 
     return 0;
 }
