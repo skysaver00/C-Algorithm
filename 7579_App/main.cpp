1108667eda9ee25ec6cd68185972f643433eb005
skysaver00 <skysaver00@naver.com>
2021-05-25 13:51:41 +0900

    not solved

--- 7579_App/main.cpp
+++ 7579_App/main.cpp
@@ -3,6 +3,12 @@
 int mem[101];
 int off[101];
 
+int dp[101][2];
+
+int max(int i, int j) {
+    return i > j ? i : j;
+}
+
 int main() {
     int n, m;
     scanf("%d %d", &n, &m);
@@ -14,4 +20,17 @@ int main() {
     for(int i = 0; i < n; i++) {
         scanf("%d", &off[i]);
     }
+
+    for(int i = 0; i <= 10000; i++) {
+        for(int j = 0; j < 100; j++) {
+            for(int k = 0; k <= j; k++) {
+                if(dp[k][1] + off[j] <= i) {
+                    dp[j][0] = max(dp[k][0] + mem[k], dp[j][0]);
+                    if(dp[j][0] == dp[k][0] + mem[k]) dp[k][1]
+                }
+            }
+        }
+    }
+
+    return 0;
 }
