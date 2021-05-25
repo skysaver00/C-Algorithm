a1ad3aa8cdd5ff0a0d1e4a39e6012eec0b6f97f1
skysaver00 <skysaver00@naver.com>
2021-05-26 02:13:23 +0900

    not solved

---
+++ 1238_Party/cmake-build-debug/CMakeFiles/1238_Party.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1238_Party.exe"
+  "1238_Party.exe.manifest"
+  "1238_Party.pdb"
+  "CMakeFiles/1238_Party.dir/main.cpp.obj"
+  "lib1238_Party.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1238_Party.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1238_Party/cmake-build-debug/CMakeFiles/1238_Party.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1238_Party.
+# This may be replaced when dependencies are built.
