b2af70c99f6807a0b9464158d711640d33fc6573
skysaver00 <skysaver00@naver.com>
2021-05-26 02:27:42 +0900

    not solved

--- 1238_Party/main.cpp
+++ 1238_Party/main.cpp
@@ -1,3 +1,21 @@
 #include <cstdio>
+#define INF (unsigned)!((int)0)
+int point[1001][1001];
 
-int 
+int main() {
+    int n, m, x;
+    scanf("%d %d %d", &n, &m, &x);
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            point[i][j] = INF;
+        }
+    }
+
+    for(int i = 0; i < m; i++) {
+        int from, to, len;
+        scanf("%d %d %d", &from, &to, &len);
+    }
+
+    return 0;
+}
