bde0c5cfec9a881d92194e88d51dd5cab4e62a13
skysaver00 <skysaver00@naver.com>
2021-05-26 18:30:22 +0900

    not solved

--- 1038_Two_Solutions/main.cpp
+++ 1038_Two_Solutions/main.cpp
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 #include <algorithm>
 using namespace std;
 
+int solutions[100001];
+
+int binSearch(int left, int right, int val, int val2) {
+    int mid = (left + right) / 2;
+    
+}
+
 int main() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) scanf("%d", &solutions[i]);
+    sort(solutions, solutions + n);
+
+    int one = -1100000000, two = -1100000000;
+    for(int i = 0; i < n - 1; i++) {
+        int ori = solutions[i];
+        int ori2 = binSearch(i + 1, n, ori, ori);
+    }
 
     return 0;
 }
