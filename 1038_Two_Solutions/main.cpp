d5ad7b78613a998b33b52319264f94646bee9707
skysaver00 <skysaver00@naver.com>
2021-05-26 19:13:17 +0900

    solved Silver V

--- 1038_Two_Solutions/main.cpp
+++ 1038_Two_Solutions/main.cpp
@@ -3,25 +3,50 @@
 #include <algorithm>
 using namespace std;
 
-int solutions[100001];
+int solu[100001];
 
-int binSearch(int left, int right, int val, int val2) {
-    int mid = (left + right) / 2;
-    
+int binSearch(int left, int right, int val, int point) {
+    int mid;
+    int now = point;
+    while(left <= right) {
+        //("%d %d %d %d\n", solu[left], solu[right], val, point);
+        mid = (left + right) / 2;
+        if (abs(solu[point] + solu[mid]) < abs(val)) {
+            val = solu[point] + solu[mid];
+            now = mid;
+        }
+
+        if(abs(solu[point] + solu[mid]) < abs(solu[point] + solu[mid + 1])) {
+            right = mid - 1;
+        } else left = mid + 1;
+    }
+    //printf("%d %d\n", solu[point], solu[now]);
+    return solu[now];
 }
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    for(int i = 0; i < n; i++) scanf("%d", &solutions[i]);
-    sort(solutions, solutions + n);
+    for(int i = 0; i < n; i++) scanf("%d", &solu[i]);
+    sort(solu, solu + n);
 
     int one = -1100000000, two = -1100000000;
     for(int i = 0; i < n - 1; i++) {
-        int ori = solutions[i];
-        int ori2 = binSearch(i + 1, n, ori, ori);
+        int ori = solu[i];
+        int ori2 = binSearch(i + 1, n - 1, 2100000000, i);
+
+        if(abs(ori + ori2) < abs(one + two)) {
+            one = ori;
+            two = ori2;
+        }
     }
+    if(one > two) {
+        int t = one;
+        one = two;
+        two = t;
+    }
+    printf("%d %d\n", one, two);
 
     return 0;
 }
