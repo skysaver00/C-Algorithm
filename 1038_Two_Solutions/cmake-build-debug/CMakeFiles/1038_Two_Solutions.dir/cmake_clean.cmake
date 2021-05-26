0c280c3abd6e5f5e0519726436d5fa31e50e47be
skysaver00 <skysaver00@naver.com>
2021-05-26 18:13:04 +0900

    not solved

---
+++ 1038_Two_Solutions/cmake-build-debug/CMakeFiles/1038_Two_Solutions.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1038_Two_Solutions.exe"
+  "1038_Two_Solutions.exe.manifest"
+  "1038_Two_Solutions.pdb"
+  "CMakeFiles/1038_Two_Solutions.dir/main.cpp.obj"
+  "lib1038_Two_Solutions.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1038_Two_Solutions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1038_Two_Solutions/cmake-build-debug/CMakeFiles/1038_Two_Solutions.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1038_Two_Solutions.
+# This may be replaced when dependencies are built.
