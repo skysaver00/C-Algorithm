54ea8619a55b1a172d72855caaec9e87f4bf2959
skysaver00 <skysaver00@naver.com>
2021-05-28 02:54:05 +0900

    not solved

---
+++ 14889_Start_And_Link/cmake-build-debug/CMakeFiles/14889_Start_And_Link.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14889_Start_And_Link.exe"
+  "14889_Start_And_Link.exe.manifest"
+  "14889_Start_And_Link.pdb"
+  "CMakeFiles/14889_Start_And_Link.dir/main.cpp.obj"
+  "lib14889_Start_And_Link.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14889_Start_And_Link.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14889_Start_And_Link/main.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <math.h>
+int arr[21][21];
+int n, r;
+int start[11];
+int link[11];
+int t;
+bool check[21];
+
+int max = 2100000000;
+
+void comp() {
+    int one = 0;
+    int two = 0;
+
+    for(int i = 1; i <= r - 1; i++) {
+        for(int j = i + 1; j <= r; j++) {
+            one += (arr[start[i]][start[j]] + arr[start[j]][start[i]]);
+            two += (arr[link[i]][link[j]] + arr[link[j]][link[i]]);
+        }
+        if(max > abs(one - two)) max = abs(one - two);
+    }
+}
+
+void comb(int num, int idx) {
+    if(idx > r) {
+        for(int i = 1; i <= r; i++) check[start[i]] = 1;
+        int v = 1;
+        for(int i = 1; i <= n; i++) {
+            if(check[i] == 0) {
+                link[v] = i;
+                v++;
+            }
+        }
+        /*for(int i = 1; i <= r; i++) {
+            printf("%d ", start[i]);
+        }printf("\n");
+
+        for(int i = 1; i <= r; i++) {
+            printf("%d ", link[i]);
+        }printf("\n");*/
+
+        comp();
+
+        for(int i = 1; i <= n; i++) check[i] = 0;
+        return;
+    }
+
+    if(num > n) return;
+    start[idx] = num;
+
+    comb(num + 1, idx + 1);
+    comb(num + 1, idx);
+}
+
+int main() {
+    scanf("%d", &n);
+    r = n / 2;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    comb(1, 1);
+    printf("%d\n", max);
+}
