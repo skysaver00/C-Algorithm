081fe504d984f4fad2068868f3d8a2977abb22be
skysaver00 <skysaver00@naver.com>
2021-05-27 14:19:49 +0900

    not solved

---
+++ 16947_Seoul_Metro_2/cmake-build-debug/CMakeFiles/16947_Seoul_Metro_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "16947_Seoul_Metro_2.exe"
+  "16947_Seoul_Metro_2.exe.manifest"
+  "16947_Seoul_Metro_2.pdb"
+  "CMakeFiles/16947_Seoul_Metro_2.dir/main.cpp.obj"
+  "lib16947_Seoul_Metro_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/16947_Seoul_Metro_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 16947_Seoul_Metro_2/cmake-build-debug/CMakeFiles/16947_Seoul_Metro_2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 16947_Seoul_Metro_2.
+# This may be replaced when dependencies are built.
