60c1149d1937a2a8d0d80785921323d2905fd3c0
skysaver00 <skysaver00@naver.com>
2021-05-27 15:03:41 +0900

    not solved

--- 16947_Seoul_Metro_2/main.cpp
+++ 16947_Seoul_Metro_2/main.cpp
@@ -1,6 +1,40 @@
-#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+vector<int> v[3001];
+int check[3001];
+int visit[3001];
+
+int cycle(int pre, int n) {
+    if(check[n] == 1) return n;
+    check[n] = 1;
+
+    int prev = n;
+    int s = v[n].size();
+    for(int i = 0; i < s; i++) {
+        if(prev == v[n][i]) continue;
+        printf("c: : %d\n", v[n][i]);
+        cycle(prev, v[n][i]);
+    }
+    return 0;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        int l1, l2;
+        scanf("%d %d", &l1, &l2);
+
+        v[l1].push_back(l2);
+        v[l2].push_back(l1);
+    }
+
+    for(int i = 1; i <= n; i++) {
+        int t = cycle(-1, i);
+        printf("%d\n", t);
+    }
+
 }
