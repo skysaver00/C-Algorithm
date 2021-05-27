36387f723438b8bda8fdf6f44c44044d267c1fdc
skysaver00 <skysaver00@naver.com>
2021-05-27 16:02:27 +0900

    not solved

---
+++ 12026_BOJ_Distance/cmake-build-debug/CMakeFiles/12026_BOJ_Distance.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "12026_BOJ_Distance.exe"
+  "12026_BOJ_Distance.exe.manifest"
+  "12026_BOJ_Distance.pdb"
+  "CMakeFiles/12026_BOJ_Distance.dir/main.cpp.obj"
+  "lib12026_BOJ_Distance.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12026_BOJ_Distance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 12026_BOJ_Distance/cmake-build-debug/CMakeFiles/12026_BOJ_Distance.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 12026_BOJ_Distance.
+# This may be replaced when dependencies are built.
