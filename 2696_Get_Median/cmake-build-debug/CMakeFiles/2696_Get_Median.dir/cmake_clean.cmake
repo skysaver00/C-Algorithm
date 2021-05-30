5a98eb6ee372b758683e95f40e08ca9be253cef3
skysaver00 <skysaver00@naver.com>
2021-05-30 21:01:26 +0900

    not solved

---
+++ 2696_Get_Median/cmake-build-debug/CMakeFiles/2696_Get_Median.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2696_Get_Median.exe"
+  "2696_Get_Median.exe.manifest"
+  "2696_Get_Median.pdb"
+  "CMakeFiles/2696_Get_Median.dir/main.cpp.obj"
+  "lib2696_Get_Median.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2696_Get_Median.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2696_Get_Median/cmake-build-debug/CMakeFiles/2696_Get_Median.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 2696_Get_Median.
+# This may be replaced when dependencies are built.
