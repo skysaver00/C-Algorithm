cd571800eba34eec0af926c40e3b1007348aac34
skysaver00 <skysaver00@naver.com>
2021-05-30 18:49:44 +0900

    solved Gold V

--- 2075_Nth_Biggest_____/main.cpp
+++ 2075_Nth_Biggest_____/main.cpp
@@ -6,7 +6,6 @@ priority_queue<int, vector<int>, greater<int>> pq;
 
 int main() {
     int n; scanf("%d", &n);
-
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             int val;
