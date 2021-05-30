e306edf33413df4cf37b0ee1a8d8a6d021f337ca
skysaver00 <skysaver00@naver.com>
2021-05-30 15:42:33 +0900

    solved Silver II

--- 11279_Maximum_Heap_____/main.cpp
+++ 11279_Maximum_Heap_____/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <queue>
+using namespace std;
+priority_queue<int> pq;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n; cin >> n;
+
+    while(n--) {
+        int x; cin >> x;
+        if(x == 0) {
+            if(pq.empty()) cout << 0 << "\n";
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else {
+            pq.push(x);
+        }
+    }
     return 0;
 }
