420e9b8e62290eb3349964502402107d7d4d2200
skysaver00 <skysaver00@naver.com>
2021-05-30 23:41:42 +0900

    not solved

---
+++ 17478_Whats_Recursive/cmake-build-debug/CMakeFiles/17478_Whats_Recursive.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "17478_Whats_Recursive.exe"
+  "17478_Whats_Recursive.exe.manifest"
+  "17478_Whats_Recursive.pdb"
+  "CMakeFiles/17478_Whats_Recursive.dir/main.cpp.obj"
+  "lib17478_Whats_Recursive.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/17478_Whats_Recursive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 17478_Whats_Recursive/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int n;
+void rec(int val) {
+    for(int i = 0; i < 4 * val; i++) printf("-");
+    printf("\"재귀함수가 뭔가요 ?\"\n");
+
+    if(val == n + 1)  {
+        for(int i = 0; i < 4 * val; i++)
+    }
+
+    for(int i = 0; i < 4 * val; i++) printf("-");
+    printf("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+    for(int i = 0; i < 4 * val; i++) printf("-");
+    printf("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+    for(int i = 0; i < 4 * val; i++) printf("-");
+    printf("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+
+    rec(++val);
+}
+
+int main() {
+    scanf("%d", &n);
+    printf("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+    rec(0);
+    return 0;
+}
