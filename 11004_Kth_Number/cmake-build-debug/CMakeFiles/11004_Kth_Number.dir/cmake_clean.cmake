e1695b2131fac6cf7771095d435c0fa3304d10c1
skysaver00 <skysaver00@naver.com>
2021-05-30 15:59:52 +0900

    solved Silver II

---
+++ 11004_Kth_Number/cmake-build-debug/CMakeFiles/11004_Kth_Number.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11004_Kth_Number.exe"
+  "11004_Kth_Number.exe.manifest"
+  "11004_Kth_Number.pdb"
+  "CMakeFiles/11004_Kth_Number.dir/main.cpp.obj"
+  "lib11004_Kth_Number.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11004_Kth_Number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 11286_Abs_Heap/main.cpp
+++ 11286_Abs_Heap/main.cpp
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <queue>
 using namespace std;
+typedef pair<int, int> pr;
 
+priority_queue<pr, vector<pr>, greater<pr>> pq;
 
 int main() {
+    int n; scanf("%d", &n);
 
+    while(n--) {
+        int x; scanf("%d", &x);
+
+        if(x == 0) {
+            if(pq.empty()) printf("0\n");
+            else {
+                if(pq.top().second == 0) printf("%d\n", pq.top().first * (-1));
+                else printf("%d\n", pq.top().first);
+                pq.pop();
+            }
+        } else {
+            int val = 1;
+            if(x < 0) {
+                x *= -1;
+                val = 0;
+            }
+            pq.push({x, val});
+        }
+    }
+    return 0;
 }
