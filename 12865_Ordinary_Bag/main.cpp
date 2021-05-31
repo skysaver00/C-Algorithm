6d19223881210479adce09e872851f619c2a000a
skysaver00 <skysaver00@naver.com>
2021-05-31 20:00:04 +0900

    not solved

--- 12865_Ordinary_Bag/main.cpp
+++ 12865_Ordinary_Bag/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+using namespace std;
+
+int arr[1001][2];
+int solve[1001][2];
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, k;
+    cin >> n >> k;
+
+    for(int i = 1; i <= n ; i++) {
+        cin >> arr[i][0] >> arr[i][1];
+    }
+
     return 0;
 }
