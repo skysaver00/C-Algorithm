a5964ced3ce7d0ecb6a2c059cbf61ad8b15c218d
skysaver00 <skysaver00@naver.com>
2021-05-31 19:27:15 +0900

    not solved

---
+++ 12865_Ordinary_Bag/cmake-build-debug/CMakeFiles/12865_Ordinary_Bag.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "12865_Ordinary_Bag.exe"
+  "12865_Ordinary_Bag.exe.manifest"
+  "12865_Ordinary_Bag.pdb"
+  "CMakeFiles/12865_Ordinary_Bag.dir/main.cpp.obj"
+  "lib12865_Ordinary_Bag.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12865_Ordinary_Bag.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 12865_Ordinary_Bag/cmake-build-debug/CMakeFiles/12865_Ordinary_Bag.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 12865_Ordinary_Bag.
+# This may be replaced when dependencies are built.
