5e9dd50653436d7796633bc6c7c782cc48328723
skysaver00 <skysaver00@naver.com>
2021-06-01 14:07:16 +0900

    not solved

--- 12865_Ordinary_Bag/main.cpp
+++ 12865_Ordinary_Bag/main.cpp
@@ -1,20 +1,50 @@
 #include <iostream>
 using namespace std;
-
-int arr[1001][2];
-int solve[1001][2];
+int arr[101][100001];
+int w[101];
+int v[101];
+int n, k;
 
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
+void sort() {
+    int key;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - 1 - i; j++) {
+            if(w[j] > w[j + 1]) {
+                int temp = w[j];
+                w[j] = w[j + 1];
+                w[j + 1] = temp;
+                temp = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = temp;
+            }
+        }
+    }
+}
+
 int main() {
-    int n, k;
     cin >> n >> k;
 
     for(int i = 1; i <= n ; i++) {
-        cin >> arr[i][0] >> arr[i][1];
+        cin >> w[i] >> v[i];
     }
+
+    sort();
+
+    for(int i = 1; i <= n; i ++) {
+        printf("%d %d\n", w[i], v[i]);
+    }
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= k; j++) {
+            if(k - w[i] )
+        }
+    }
+
+
 
     return 0;
 }
