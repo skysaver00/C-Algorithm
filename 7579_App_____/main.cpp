d924f358a5f412cc9fde0ad8604919258a4042ab
skysaver00 <skysaver00@naver.com>
2021-06-01 14:49:43 +0900

    not solved

--- 7579_App_____/main.cpp
+++ 7579_App_____/main.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 int a[101];
 int c[101];
+int dp[10001];
 int n, m;
+
+int max(int i, int j) {
+    return (i > j) ? i : j;
+}
 
 int main() {
     ios_base :: sync_with_stdio(false);
@@ -10,10 +15,18 @@ int main() {
     cout.tie(NULL);
 
     cin >> n >> m;
+    int sum = 0;
     for(int i = 1; i <= n; i++) cin >> a[i];
-    for(int i = 1; i <= n; i++) cin >> c[i];
+    for(int i = 1; i <= n; i++) {
+        cin >> c[i];
+        sum += c[i];
+    }
 
-    
+    for(int i = 1; i <= n; i++) {
+        for(int j = sum; j >= c[i]; j--) {
+            dp[j] = max(dp[j], dp[j])
+        }
+    }
 
     return 0;
 }
