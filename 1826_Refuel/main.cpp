908af7265c527d6268fdf7338f51ba74bc5314c1
skysaver00 <skysaver00@naver.com>
2021-06-03 15:44:46 +0900

    solved Gold III

--- 1826_Refuel/main.cpp
+++ 1826_Refuel/main.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
+
 using namespace std;
 pair<int, int> gas[10001];
-bool check[10001];
+int check[10001];
 priority_queue<int> pq;
 
 int main() {
     int n;
     cin >> n;
 
-    for(int i = 0; i < n; i++) cin >> gas[i].first >> gas[i].second;
+    for (int i = 0; i < n; i++) cin >> gas[i].first >> gas[i].second;
     int x, y;
     cin >> x >> y;
 
     sort(gas, gas + n);
     int num = 0;
-    while(y < x) {
+    while (y < x) {
         for (int i = 0; i < n; i++) {
-            if (gas[i].first <= y && !check[i]) {
+            if (gas[i].first <= y && check[i] == 0) {
                 pq.push(gas[i].second);
-                check[i] = true;
-            } else break;
+                check[i] = 1;
+            } else if(check[i] == 1) continue;
+            else break;
         }
-        cout << y << " " << pq.top() << endl;
-        y += pq.top();
+        if (pq.size() == 0) {
+            printf("-1\n");
+            return 0;
+        }
+        y = y + pq.top();
         pq.pop();
         num++;
     }
