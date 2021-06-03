3870553f7d17b8156642a912fcd53dea5038be0f
skysaver00 <skysaver00@naver.com>
2021-06-03 14:03:54 +0900

    not solved

--- 12865_Ordinary_Bag_____/main.cpp
+++ 12865_Ordinary_Bag_____/main.cpp
@@ -3,12 +3,27 @@ int dp[101][100001];
 int val[1001];
 int wei[1001];
 
+int max(int i, int j) {
+    return i > j ? i : j;
+}
+
 int main() {
     int n, k;
     scanf("%d %d", &n, &k);
 
-    for(int i = 1; i<= n; i++) scanf("%d %d", &wei[i], &val[i]);
-    
+    for(int i = 1; i <= n; i++) scanf("%d %d", &wei[i], &val[i]);
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= 100000; j++) {
+            if(j - wei[i] >= 0) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - wei[i]] + val[i]);
+            else dp[i][j] = dp[i - 1][j];
+        }
+    }
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= k; j++) {
+            printf("%d ", dp[i][j]);
+        }printf("\n");
+    }
 
     return 0;
 }
