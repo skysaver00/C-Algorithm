cc5a61ecda589f090ee12b7e1b74d9b54d868848
skysaver00 <skysaver00@naver.com>
2021-06-04 17:07:29 +0900

    not solved

--- 7662_Dual_Priority_Queue/main.cpp
+++ 7662_Dual_Priority_Queue/main.cpp
@@ -5,6 +5,19 @@ using namespace std;
 set<int> s;
 
 int main() {
+    int t; cin >> t;
+
+    while(t--) {
+        int k; cin >> k;
+        for(int i = 0; i < k; i++) {
+            char c; int n;
+            cin >> c >> n;
+
+            s.insert(n);
+            for(auto i : s) cout << i << " ";
+            cout << "\n";
+        }
+    }
 
     return 0;
 }
