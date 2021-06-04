d2367eb21d2aeb30d632eb49d8d06299f6258543
skysaver00 <skysaver00@naver.com>
2021-06-04 16:25:04 +0900

    not solved

---
+++ 14425_String_Set/cmake-build-debug/CMakeFiles/14425_String_Set.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14425_String_Set.exe"
+  "14425_String_Set.exe.manifest"
+  "14425_String_Set.pdb"
+  "CMakeFiles/14425_String_Set.dir/main.cpp.obj"
+  "lib14425_String_Set.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14425_String_Set.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
