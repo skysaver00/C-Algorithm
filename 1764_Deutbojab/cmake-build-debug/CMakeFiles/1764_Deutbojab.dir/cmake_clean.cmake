a2c46c1759b0f4efb656559aade279d00fdeae64
skysaver00 <skysaver00@naver.com>
2021-06-04 16:36:05 +0900

    not solved

---
+++ 1764_Deutbojab/cmake-build-debug/CMakeFiles/1764_Deutbojab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1764_Deutbojab.exe"
+  "1764_Deutbojab.exe.manifest"
+  "1764_Deutbojab.pdb"
+  "CMakeFiles/1764_Deutbojab.dir/main.cpp.obj"
+  "lib1764_Deutbojab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1764_Deutbojab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1764_Deutbojab/cmake-build-debug/CMakeFiles/1764_Deutbojab.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1764_Deutbojab.
+# This may be replaced when dependencies are built.
