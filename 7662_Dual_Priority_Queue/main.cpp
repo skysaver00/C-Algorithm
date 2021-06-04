a4e9ec6b06998497729bc7e4a8ad49eb0c4a7eae
skysaver00 <skysaver00@naver.com>
2021-06-04 23:45:12 +0900

    solved Gold V

--- 7662_Dual_Priority_Queue/main.cpp
+++ 7662_Dual_Priority_Queue/main.cpp
@@ -2,9 +2,13 @@
 #include <set>
 using namespace std;
 
-set<int> s;
+multiset<int> s;
+multiset<int> ne;
 
 int main() {
+    /*ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);*/
     int t; cin >> t;
 
     while(t--) {
@@ -14,14 +18,28 @@ int main() {
             cin >> c >> n;
 
             if(c == 'I') s.insert(n);
-            else if(c == 'D') {
-                if(s.size() == 0) continue;
-                if(n == -1) s.erase(s.begin());
-                else if(n == 1) s.erase(s.end());
-            }
-            cout << s.size() << endl;
-        }
-    }
 
+            else if(c == 'D') {
+                if(s.size() == 0) {
+                    s = ne;
+                    continue;
+                }
+                if(n == -1) {
+                    auto iter = s.begin();
+                    s.erase(iter);
+                }
+                else if(n == 1) {
+                    auto iter = s.end();
+                    iter--;
+                    s.erase(iter);
+                }
+            }
+        }
+        if(s.size() == 0) cout << "EMPTY\n";
+        else {
+            cout << *s.rbegin() << " " << *s.begin() << "\n";
+        }
+        s = ne;
+    }
     return 0;
 }
