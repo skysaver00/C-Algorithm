711804f967d0e6911b3d3390c57124dfe1233232
skysaver00 <skysaver00@naver.com>
2021-06-04 22:40:11 +0900

    not solved

--- 7662_Dual_Priority_Queue/main.cpp
+++ 7662_Dual_Priority_Queue/main.cpp
@@ -13,7 +13,11 @@ int main() {
             char c; int n;
             cin >> c >> n;
 
-            s.insert(n);
+            if(c == 'I') s.insert(n);
+            else if(c == 'D') {
+                if(n == -1) s.erase(s.begin());
+                else if(n == 1) s.erase(s.end());
+            }
             for(auto i : s) cout << i << " ";
             cout << "\n";
         }
