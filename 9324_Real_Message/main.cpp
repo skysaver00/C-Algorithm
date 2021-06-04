adc925c7da8b95456ac9d37070ccf5e9e622e46d
skysaver00 <skysaver00@naver.com>
2021-06-04 23:59:58 +0900

    not solved

--- 9324_Real_Message/main.cpp
+++ 9324_Real_Message/main.cpp
@@ -13,22 +13,28 @@ int main() {
         int flag = 0;
         for(int i = 0; i < len; i++) {
             char t = mes.at(i);
+            A[t - 0x41]++;
             if(A[t - 0x41] % 3 == 0 && A[t - 0x41] != 0) {
-                if(t == len - 1) {
+                cout << t << " " << len - 1 << "\n";
+                if (t == len - 1) {
                     cout << "FAKE\n";
                     flag = 1;
                     break;
-                } else if(t != mes.at(i + 1)) {
+                } else if (t != mes.at(i + 1)) {
                     cout << "FAKE\n";
                     flag = 1;
                     break;
                 }
             }
-            A[t - 0x41]++;
 
             for(int j = 0; j < 26; j++) {
                 cout << A[j] << " ";
             }cout << "\n";
         }
+
+        if(flag == 0) cout << "OK\n";
+        for(int i = 0; i < 26; i++) A[i] = 0;
     }
+
+    return 0;
 }
