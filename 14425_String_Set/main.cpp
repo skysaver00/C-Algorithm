918b838f2092a93a5c3957f5c52cf6159c5b0094
skysaver00 <skysaver00@naver.com>
2021-06-04 16:33:38 +0900

    solved Silver III

--- 14425_String_Set/main.cpp
+++ 14425_String_Set/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+unordered_map <string, bool> map;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m, cnt;
+    cnt = 0;
+    cin >> n >> m;
+
+    string word;
+    for(int i = 0; i < n; i++) {
+        cin >> word;
+        map[word] = true;
+    }
+
+    for(int i = 0; i < m; i++) {
+        cin >> word;
+        if(map[word]) cnt++;
+    }
+    cout << cnt << "\n";
     return 0;
 }
