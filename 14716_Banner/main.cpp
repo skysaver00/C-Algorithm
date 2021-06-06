08ffffd13f94acde7cb5f0fc9578e3136e999e4c
skysaver00 <skysaver00@naver.com>
2021-06-06 22:01:34 +0900

    not solved

--- 14716_Banner/main.cpp
+++ 14716_Banner/main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <stdio.h>
+#include <queue>
+using namespace std;
+bool banner[251][251];
+int check[251][251];
+
+int x[10] = {1, 1, 0, -1, -1, -1, 0, 1};
+int y[10] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+queue<pair<int, int>> que;
+
+void startBFS(int i, int j) {
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &banner[i][j]);
+        }
+    }
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            startBFS(i, j);
+        }
+    }
+
     return 0;
 }
