297f820550d06543d344c528eba0e1622bca36d0
skysaver00 <skysaver00@naver.com>
2021-06-06 21:57:46 +0900

    not solved

---
+++ 14716_Banner/cmake-build-debug/CMakeFiles/14716_Banner.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14716_Banner.exe"
+  "14716_Banner.exe.manifest"
+  "14716_Banner.pdb"
+  "CMakeFiles/14716_Banner.dir/main.cpp.obj"
+  "lib14716_Banner.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14716_Banner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14716_Banner/cmake-build-debug/CMakeFiles/14716_Banner.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 14716_Banner.
+# This may be replaced when dependencies are built.
