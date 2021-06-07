e3bafea43c69f48d52abe025d79d4c11b3e861d6
skysaver00 <skysaver00@naver.com>
2021-06-07 18:13:02 +0900

    not solved

---
+++ 1987_Alphabet/cmake-build-debug/CMakeFiles/1987_Alphabet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1987_Alphabet.exe"
+  "1987_Alphabet.exe.manifest"
+  "1987_Alphabet.pdb"
+  "CMakeFiles/1987_Alphabet.dir/main.cpp.obj"
+  "lib1987_Alphabet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1987_Alphabet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
