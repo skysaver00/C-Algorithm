0a14afb64b64627e5cc015a3b59e3f58e40af1bf
skysaver00 <skysaver00@naver.com>
2021-06-07 22:28:28 +0900

    not solved

--- 11054_Longest_Bitonic_Permutation/main.cpp
+++ 11054_Longest_Bitonic_Permutation/main.cpp
@@ -4,23 +4,19 @@ int up[1001];
 int down[1001];
 
 int max (int i, int j) {
-    return i > j ? i : j;
+    return i >= j ? i : j;
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-
-    int ma = 0;
     for(int i = 1; i <= n; i++) {
         scanf("%d", &arr[i]);
-        ma = max(ma, arr[i]);
     }
 
     int flag = 0;
     up[1] = 1;
-    for(int i = 2; i <= n; i++) {
-        if(arr[i] == ma) flag = 1;
+    for(int i = 1; i <= n; i++) {
         for(int j = 1; j < i; j++) {
             if(arr[i] > arr[j]) {
                 if(up[i] <= up[j]) {
@@ -28,10 +24,11 @@ int main() {
                 }
             }
         }
-        if(flag == 1) break;
     }
 
-    for(int i = 1; i < n; i++) {
-        printf("%d\n", up[i]);
-    }
+    int ma = 0;
+    int loc = 0;
+    for(int i = 1; i <= n; i++) {
+        printf("%d ", up[i]);
+    }printf("\n");
 }
