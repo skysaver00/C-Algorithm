f69b84a1672cfc211ba8f6ca571e46a8bfc56337
skysaver00 <skysaver00@naver.com>
2021-06-07 17:19:04 +0900

    not solved

--- 2206_Break_Through_Wall/main.cpp
+++ 2206_Break_Through_Wall/main.cpp
@@ -6,7 +6,6 @@ char mp[1001][1001];
 int check[1001][1001];
 bool used[1001][1001];
 queue<pair<int, int>> que;
-int val = 1;
 int n, m;
 
 int x[4] = {1, 0, -1, 0};
@@ -14,28 +13,35 @@ int y[4] = {0, 1, 0, -1};
 
 void startBFS(int a, int b) {
     que.push({a, b});
-    check[a][b] = val++;
+    check[a][b] = 1;
 
     while(!que.empty()) {
+        printf("right now: %d %d\n", que.front().first, que.front().second);
+        int xx, yy;
+        yy = que.front().first;
+        xx = que.front().second;
         que.pop();
         for(int i = 0; i < 4; i++) {
-            int _y = a + y[i];
-            int _x = b + x[i];
+            int _y = yy + y[i];
+            int _x = xx + x[i];
+
+            printf("%d %d %d\n", _y, _x, check[_y][_x]);
 
             if(check[_y][_x] != 0) continue;
+
             if(_y < n && _x < m && _y >= 0 && _x >= 0) {}
             else continue;
 
-            if(mp[_y][_x] == 1) {
-                if(used[a][b] == 1) continue;
+            if(mp[_y][_x] == '1') {
+                if(used[yy][xx] == 1) continue;
                 else {
                     used[_y][_x] = true;
-                    check[_y][_x] = val++;
+                    check[_y][_x] = check[yy][xx] + 1;
                     que.push({_y, _x});
                 }
-            } else if(mp[_y][_x] == 0) {
-                check[_y][_x] = val++;
-                used[_y][_x] = used[a][b];
+            } else if(mp[_y][_x] == '0') {
+                check[_y][_x] = check[yy][xx] + 1;
+                used[_y][_x] = used[yy][xx];
                 que.push({_y, _x});
             }
 
