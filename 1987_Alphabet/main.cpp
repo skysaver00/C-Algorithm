2666c3f22b0c9fe82d3867e595746af53b44ff07
skysaver00 <skysaver00@naver.com>
2021-06-07 19:10:25 +0900

    not solved

--- 1987_Alphabet/main.cpp
+++ 1987_Alphabet/main.cpp
@@ -1,25 +1,18 @@
 #include <stdio.h>
 char arr[21][21];
-bool check[21][21];
 int route[21][21];
 bool Acheck[27];
 int r, c;
+int t = 1;
+
+int max(int i, int j) {
+    return i > j ? i : j;
+}
 
 int x[4] = {1, 0, -1, 0};
 int y[4] = {0, 1, 0, -1};
 
-void startDFS(int a, int b) {
-    if(check[a][b] != 0) {
-        check[a][b] = false;
-        return;
-    }
-    if(Acheck[arr[a][b] - 0x41]) {
-        Acheck[arr[a][b] - 0x41] = false;
-        return;
-    }
-
-    Acheck[arr[a][b] - 0x41] = true;
-    check[a][b] = true;
+void startDFS(int a, int b, int t) {
     for(int i = 0; i < 4; i++) {
         int _x = a + x[i];
         int _y = b + y[i];
@@ -27,22 +20,12 @@ void startDFS(int a, int b) {
         if(_x < r && _y < c && _x >= 0 && _y >= 0) {}
         else continue;
 
-        if(check[_x][_y] != 0) continue;
+        if(Acheck[arr[_x][_y] - 0x41]) continue;
+        Acheck[arr[_x][_y] - 0x41] = true;
         route[_x][_y] = route[a][b] + 1;
 
-        for(int j = 0; j < r; j++) {
-            for(int k = 0; k < c; k++) {
-                printf("%d ", route[j][k]);
-            }printf("\n");
-        }printf("\n");
-
-        for(int j = 0; j < r; j++) {
-            for(int k = 0; k < c; k++) {
-                printf("%d ", check[j][k]);
-            }printf("\n");
-        }printf("\n");
-        printf("---------------------------------");
-        startDFS(_x, _y);
+        startDFS(_x, _y, ++t);
+        Acheck[arr[_x][_y] - 0x41] = false;
     }
 }
 
@@ -52,8 +35,18 @@ int main() {
     for(int i = 0; i < r; i++) {
         scanf("%s", arr[i]);
     }
+    Acheck[arr[0][0] - 0x41] = true;
     route[0][0] = 1;
-    startDFS(0, 0);
+    startDFS(0, 0, t);
+
+    int ma = -9999;
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            //printf("%d ", route[i][j]);
+            ma = max(ma, route[i][j]);
+        }//printf("\n");
+    }
+    printf("%d\n", ma);
 
     return 0;
 }
