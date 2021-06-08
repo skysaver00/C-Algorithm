c5d0c596cdffcf6090daaaa89c283a1e2410393c
skysaver00 <skysaver00@naver.com>
2021-06-08 13:58:06 +0900

    not solved

--- 14500_Tetromino/main.cpp
+++ 14500_Tetromino/main.cpp
@@ -3,11 +3,28 @@ using namespace std;
 int n, m;
 int arr[501][501];
 
-int one();
-int two();
-int three();
-int four();
-int five();
+int on1X[4][8] = {{0,0,0,0,0,0,0,0}, {0,0,1,1,0,0,-1,-1}, {1,-1,1,1,1,-1,-1,-1}};
+int on1Y[4][8] = {{0,0,0,0,0,0,0,0}, {1,1,0,0,-1,-1,0,0}, {1,1,1,-1,-1,-1,-1,1}};
+
+int one(int i, int j) {
+
+}
+
+int two(int i, int j) {
+
+}
+
+int three() {
+
+}
+
+int four() {
+
+}
+
+int five() {
+
+}
 
 int main() {
     cin >> n >> m;
@@ -20,7 +37,7 @@ int main() {
 
     for(int i = 0 ; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            
+
         }
     }
 
