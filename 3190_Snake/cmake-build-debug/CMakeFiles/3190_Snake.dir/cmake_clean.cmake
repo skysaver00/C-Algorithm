0d700bd82e53b73904676ffe1dc14f90ce4015ea
skysaver00 <skysaver00@naver.com>
2021-06-08 21:49:15 +0900

    not solved

---
+++ 3190_Snake/cmake-build-debug/CMakeFiles/3190_Snake.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3190_Snake.exe"
+  "3190_Snake.exe.manifest"
+  "3190_Snake.pdb"
+  "CMakeFiles/3190_Snake.dir/main.cpp.obj"
+  "lib3190_Snake.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3190_Snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
