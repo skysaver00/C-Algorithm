6d865c5163910f910128e7f84477d8827adbf62f
skysaver00 <skysaver00@naver.com>
2021-06-10 00:17:34 +0900

    not solved

--- 14002_Longest_Increasing_Partial_Sequence_4/main.cpp
+++ 14002_Longest_Increasing_Partial_Sequence_4/main.cpp
@@ -8,7 +8,18 @@ int main() {
     int n; cin >> n;
     for(int i = 1; i <= n; i++) cin >> arr[i];
 
-    
+    check[1] = 1;
+    for(int i = 1; i <= n; i++) {
+        for(int j = i; j < n; j++) {
+            if(arr[j] > arr[i]) {
+                if(check[j] < check[i]) check[j] = check[i] + 1;
+            }
+        }
+    }
+
+    for(int i = 1; i <= n; i++) {
+        cout << check[i] << " ";
+    }
 
     return 0;
 }
