b7028de4a818339768aa9ec14a679288cbe68a47
skysaver00 <skysaver00@naver.com>
2021-06-10 00:26:09 +0900

    not solved

--- 14002_Longest_Increasing_Partial_Sequence_4/main.cpp
+++ 14002_Longest_Increasing_Partial_Sequence_4/main.cpp
@@ -4,22 +4,32 @@ using namespace std;
 int arr[1001];
 int check[1001];
 
+bool ckPer[1001];
+int arr2[1001];
+
 int main() {
     int n; cin >> n;
     for(int i = 1; i <= n; i++) cin >> arr[i];
 
-    check[1] = 1;
-    for(int i = 1; i <= n; i++) {
-        for(int j = i; j < n; j++) {
-            if(arr[j] > arr[i]) {
-                if(check[j] < check[i]) check[j] = check[i] + 1;
+    check[1] = 0;
+    for(int i = 2; i <= n; i++) {
+        for(int j = 1; j < i; j++) {
+            if(arr[i] > arr[j]) {
+                if(check[i] <= check[j]) check[i] = check[j] + 1;
             }
         }
     }
 
     for(int i = 1; i <= n; i++) {
-        cout << check[i] << " ";
+        if(!ckPer[check[i]]) {
+            ckPer[check[i]] = true;
+            arr2[check[i]] = arr[i];
+        }
     }
 
+    for(int i = 0; i <= n; i++) {
+        if(arr2[i] == 0) break;
+        printf("%d ", arr2[i]);
+    }printf("\n");
     return 0;
 }
