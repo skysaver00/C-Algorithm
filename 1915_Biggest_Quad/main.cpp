c7e820d2354f20fc066e7c22356133906b81c1e9
skysaver00 <skysaver00@naver.com>
2021-06-09 22:50:10 +0900

    not solved

--- 1915_Biggest_Quad/main.cpp
+++ 1915_Biggest_Quad/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <stdio.h>
+char arr[1001][1001];
+int real[1001][2];
+
+void solve() {
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m; scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++) scanf("%s", arr[i]);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            real[i][j] = arr[i][j] - 0x30;
+            printf("%d ", real[i][j]);
+        }
+    }
+    solve();
+
     return 0;
 }
