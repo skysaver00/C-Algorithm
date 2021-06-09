5b7d13c60e30c2def4c1b85dca72d375fe9a58ec
skysaver00 <skysaver00@naver.com>
2021-06-09 17:31:10 +0900

    not solved

--- 15686_Chicken_Delivery/main.cpp
+++ 15686_Chicken_Delivery/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 int mp[51][51];
 int t[13][101];
 
@@ -12,15 +13,30 @@ int main() {
         }
     }
 
+    int now = -1;
+    int now2;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             if(mp[i][j] == 2) {
+                now++;
+                now2 = 0;
                 for(int k = 0; k < n; k++) {
                     for(int h = 0; h < n; h++) {
-                        
+                        if(mp[k][h] == 1) {
+                            t[now][now2] = abs(k - i) + abs(h - j);
+                            now2++;
+                        }
                     }
                 }
             }
         }
     }
+
+    for(int i = 0; i < now; i++) {
+        for(int j = 0; j < now2; j++) {
+            printf("%d ", t[i][j]);
+        }printf("\n");
+    }printf("\n");
+
+    return 0;
 }
