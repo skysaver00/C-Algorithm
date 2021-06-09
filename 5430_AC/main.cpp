a518f18dd2c6f37c66db7e4b1a49e93b18714efd
skysaver00 <skysaver00@naver.com>
2021-06-09 21:17:46 +0900

    not solveD

--- 5430_AC/main.cpp
+++ 5430_AC/main.cpp
@@ -24,16 +24,48 @@ int main(){
             if(str.at(i) == ',' || str.at(i) == ']') {
                 deq.push_back(val);
                 val = 0;
-            } else {
+            } else if(str.at(i) == '[') {} else {
                 val *= 10;
                 val += str.at(i) - 0x30;
             }
         }
 
-        int sz = deq.size();
-        for(int i = 0; i < sz; i++) {
-            cout << deq.front() << endl;
-            deq.pop_front();
+        len = P.length();
+        int fb = 0;
+        for(int i = 0; i < len; i++) {
+            if(P.at(i) == 'R') {
+                if(fb == 0) fb = 1;
+                else fb = 0;
+            } else {
+                int sz = deq.size();
+
+                if(sz > 0) {
+                    if (fb == 0) deq.pop_front();
+                    else deq.pop_back();
+                }
+            }
         }
+
+        int sz = deq.size();
+        if(sz == 0) {
+            cout << "error\n";
+            continue;
+        }
+
+        cout << "[";
+        for(int i = 0; i < sz; i++) {
+            if(fb == 0) {
+                cout << deq.front();
+                deq.pop_front();
+            } else {
+                cout << deq.back();
+                deq.pop_back();
+            }
+            if(i != sz - 1) cout << ",";
+        }
+        cout << "]\n";
+        P.clear();
+        str.clear();
     }
+    return 0;
 }
