c0b852b815cca2e527528860bb0e108aed628f72
skysaver00 <skysaver00@naver.com>
2021-06-10 00:10:07 +0900

    not solved

---
+++ 14002_Longest_Increasing_Partial_Sequence_4/cmake-build-debug/CMakeFiles/14002_Longest_Increasing_Partial_Sequence_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14002_Longest_Increasing_Partial_Sequence_4.exe"
+  "14002_Longest_Increasing_Partial_Sequence_4.exe.manifest"
+  "14002_Longest_Increasing_Partial_Sequence_4.pdb"
+  "CMakeFiles/14002_Longest_Increasing_Partial_Sequence_4.dir/main.cpp.obj"
+  "lib14002_Longest_Increasing_Partial_Sequence_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14002_Longest_Increasing_Partial_Sequence_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14002_Longest_Increasing_Partial_Sequence_4/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int arr[1001];
+int check[1001];
+
+int main() {
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++) cin >> arr[i];
+
+    
+
+    return 0;
+}
