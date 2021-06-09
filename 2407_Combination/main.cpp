12fcf1a95b76465d79a3a341cd589fe92a7d169a
skysaver00 <skysaver00@naver.com>
2021-06-10 01:51:03 +0900

    not solved

--- 2407_Combination/main.cpp
+++ 2407_Combination/main.cpp
@@ -1,29 +1,14 @@
-#include <cstdio>
-using namespace std;
+/*
+ * import sys
+n = int(input())
+m = int(input())
 
-long long arr[101][101];
+x = 1
+for i in range(n - m, n):
+	x *= i
 
-void makeIT() {
-    for(int i = 1; i <= 100; i++) {
-        arr[i][0] = 1;
-        arr[i][i] = 1;
-    }
+for i in range(m, n):
+	x /= i
 
-    for(int i = 2; i <= 100; i++) {
-        for(int j = 1; j < i; j++) {
-            arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
-        }
-    }
-}
-
-int main() {
-    makeIT();
-
-    for(int i = 0 ; i <= 100; i++) {
-        for(int j = 0; j <= i; j++) {
-            printf("%lld ", arr[i][j]);
-        }printf("\n");
-    }printf("\n");
-
-    return 0;
-}//cannot solve Because it needs big integer.
+print(x)
+ */
