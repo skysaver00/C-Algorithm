4d5fdbfa690e10a51a5640e42bb047ebffb5ef1c
skysaver00 <skysaver00@naver.com>
2021-06-10 16:19:55 +0900

    not solved

---
+++ 1918_Suffix/cmake-build-debug/CMakeFiles/1918_Suffix.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1918_Suffix.exe"
+  "1918_Suffix.exe.manifest"
+  "1918_Suffix.pdb"
+  "CMakeFiles/1918_Suffix.dir/main.cpp.obj"
+  "lib1918_Suffix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1918_Suffix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
