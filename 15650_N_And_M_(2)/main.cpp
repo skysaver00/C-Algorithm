e00202f6b3e930e129a67c6111b481112847aa32
skysaver00 <skysaver00@naver.com>
2021-06-11 20:16:36 +0900

    solved Silver III

--- 15650_N_And_M_(2)/main.cpp
+++ 15650_N_And_M_(2)/main.cpp
@@ -5,11 +5,19 @@ bool visit[10];
 
 void NnM(int idx, int cnt, int max, int size) {
     if(cnt == max) {
-
+        for(int i = 0; i < size; i++) {
+            if(visit[i]) cout << arr[i] << " ";
+        }cout << "\n";
         return;
     }
 
-    for(int i = idx, i < size; )
+    for(int i = idx; i < size; i++) {
+        if(visit[i]) continue;
+        visit[i] = true;
+
+        NnM(i, cnt + 1, max, size);
+        visit[i] = false;
+    }
 }
 
 int main() {
@@ -17,4 +25,5 @@ int main() {
     for(int i = 0; i < n; i++) arr[i] = i + 1;
 
     NnM(0, 0, m, n);
+    return 0;
 }
