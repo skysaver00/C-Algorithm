177014d68ed16d22d5e0d564f1097e8c8e907a8e
skysaver00 <skysaver00@naver.com>
2021-06-11 19:20:00 +0900

    노드의 레벨 완성

--- Practice_Permutation_Backtracking/main.cpp
+++ Practice_Permutation_Backtracking/main.cpp
@@ -4,20 +4,21 @@ int arr[50];
 int res[50];
 int visit[50];
 
-void dfs(int cnt, int max, int sz) {
+void dfs(int cnt, int max, int sz, int now) {
     if(cnt == max) {
         for(int i = 0; i < max; i++) { //찾으려는 개수 도달
             cout << res[i] << " ";
         }cout << "\n";
-
+        now--;
         return;
     }
-
     for(int i = 0; i < sz; i++) {
         if(visit[i] == true) continue; //이미 방문했다면 패스!
         visit[i] = true;
         res[cnt] = arr[i];
-        dfs(cnt + 1, max, sz);
+        cout << "now is: " << now << "\n";
+        dfs(cnt + 1, max, sz, now + 1);
+        cout << "now is: " << now << "\n";
         res[cnt] = 0;
         visit[i] = false;
     }
@@ -30,5 +31,5 @@ int main() {
         arr[i] = i + 1;
     }
 
-    dfs(0, m, n);
+    dfs(0, m, n, 1);
 }
