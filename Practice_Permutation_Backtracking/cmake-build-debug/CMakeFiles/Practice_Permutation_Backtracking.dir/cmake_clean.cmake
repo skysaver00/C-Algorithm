0a43fd3c5ca55bb73b9005ad8337e12f006e3736
skysaver00 <skysaver00@naver.com>
2021-06-11 17:30:21 +0900

    순열의 개수 == 총 개수 출력까지

---
+++ Practice_Permutation_Backtracking/cmake-build-debug/CMakeFiles/Practice_Permutation_Backtracking.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practice_Permutation_Backtracking.dir/main.cpp.obj"
+  "Practice_Permutation_Backtracking.exe"
+  "Practice_Permutation_Backtracking.exe.manifest"
+  "Practice_Permutation_Backtracking.pdb"
+  "libPractice_Permutation_Backtracking.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practice_Permutation_Backtracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Practice_Permutation_Backtracking/main.cpp
+++ Practice_Permutation_Backtracking/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
+int arr[50];
+int res[50];
+
+void dfs(int cnt, int max) {
+    if(cnt == max) {
+        for(int i = 0; i < max; i++) {
+            cout << res[i] << " ";
+        }cout << "\n";
+
+        return;
+    }
+
+    
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int n, m; cin >> n >> m;
+
+    for(int i = 0; i < n; i++) {
+        arr[i] = i + 1;
+    }
+
+    dfs(0, m) {
+
+    }
 }
