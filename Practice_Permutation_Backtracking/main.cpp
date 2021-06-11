409f009580128d8433ce09adda872a228bae336d
skysaver00 <skysaver00@naver.com>
2021-06-11 20:20:05 +0900

    not solved

--- Practice_Permutation_Backtracking/main.cpp
+++ Practice_Permutation_Backtracking/main.cpp
@@ -6,6 +6,12 @@ int visit[50];
 
 void dfs(int cnt, int max, int sz, int now) {
     if(cnt == max) {
+        /*int check = res[0];
+        for(int i = 1; i < max; i++) {
+            if(check > res[i]) return;
+            check = res[i];
+        }*/ //이걸 넣어주면 중복 순열 가능!
+
         for(int i = 0; i < max; i++) { //찾으려는 개수 도달
             cout << res[i] << " ";
         }cout << "\n";
@@ -13,10 +19,10 @@ void dfs(int cnt, int max, int sz, int now) {
         return;
     }
     for(int i = 0; i < sz; i++) {
-        if(visit[i] == true) continue; //이미 방문했다면 패스!
+        //if(visit[i] == true) continue; //이미 방문했다면 패스!
         visit[i] = true;
         res[cnt] = arr[i];
-        cout << "now is: " << now << "\n";
+        //cout << "now is: " << now << "\n";
         dfs(cnt + 1, max, sz, now + 1);
         visit[i] = false;
     }
