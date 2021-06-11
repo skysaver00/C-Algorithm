5632007b1b652e867766e36ac4b6b1e9d77ed3c9
skysaver00 <skysaver00@naver.com>
2021-06-12 01:15:54 +0900

    not sovled

--- 1620 Pokemon/main.cpp
+++ 1620 Pokemon/main.cpp
@@ -17,8 +17,23 @@ int main() {
         pokem2[i] = nme;
     }
 
-    for(int i = 0; i < N; i++) {
-        cout << pokem[i].name << " " << pokem[i].value << "\n";
+    string nme2;
+    for(int i = 0; i < M; i++) {
+        cin >> nme2;
+        int val;
+        int flag = 0;
+        if(nme2[0] >= 0x30 && nme2[0] <= 0x39) {
+            val = stoi(nme2);
+            flag = 1;;
+        }
+
+        if(flag == 1) cout << pokem2[val -1] << "\n";
+        else cout << pokem.find(nme2)->second << "\n";
+
+    }
+
+    for(auto i = pokem.begin(); i != pokem.end(); i++) {
+        //out << i->first << " " << i->second << "\n";
     }
 
     return 0;
