d4659ecd77efafa25843681104841420a8e8a737
skysaver00 <skysaver00@naver.com>
2021-06-12 12:41:36 +0900

    not solved

--- 2580_Sudoku/main.cpp
+++ 2580_Sudoku/main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 using namespace std;
 int sudoku[10][10];
-bool check[10];
+int check[10];
 
-void solve(int i, int j) {
+bool solve(int i, int j) {
     for(int a = 0; a < 9; a++) check[a] = false;
-    for(int a = 0; a < 9; a++) {
-        check[sudoku[i][a]] = true;
-        check[sudoku[a][j]] = true;
-    }
+    for(int a = 0; a < 9; a++) check[sudoku[i][a]] += 1;
+    for(int a = 0; a < 9; a++) if(check[a] >= 2) return false;
+
+
+    for(int a = 0; a < 9; a++) check[a] = false;
+    for(int a = 0; a < 9; a++) check[sudoku[a][j]] += 1;
+    for(int a = 0; a < 9; a++) if(check[a] >= 2) return false;
+
     int x = i / 3;
     int y = j / 3;
+    for(int a = 0; a < 9; a++) check[a] = false;
     for(int a = x * 3; a < x * 3 + 3; a++) {
         for(int b = y * 3; b < y * 3 + 3; b++) {
             check[sudoku[a][b]] = true;
         }
     }
+    for(int a = 0; a < 9; a++) if(check[a] >= 2) return false;
+
+    return true;
 }
 
 int main() {
