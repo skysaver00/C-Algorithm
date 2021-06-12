65c7688e063fb438db2a607fd6b6dc3daa7c16b0
skysaver00 <skysaver00@naver.com>
2021-06-12 20:52:47 +0900

    다시 해보는 중!

--- Practice_Combination_Backtracking/main.cpp
+++ Practice_Combination_Backtracking/main.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 using namespace std;
-int arr[50];
+int number[50];
 bool visit[50];
 
-void dfs(int idx, int cnt, int max, int size, int now) {
+void dfs(int cnt, int max, int size, int level) {
     if(cnt == max) {
         for(int i = 0; i < size; i++) {
-            if(visit[i]) cout << arr[i] << " ";
+            if(visit[i]) cout << number[i] << " ";
         }cout << "\n";
         return;
     }
-    for(int i = idx; i < size; i++) {
-        if(visit[i]) continue;
-        visit[i] = true;
-        dfs(i,cnt + 1, max, size, now + 1);
-        visit[i] = false;
-    }
+
+    
 }
 
 int main() {
     int n, m; cin >> n >> m;
-    for(int i = 0; i < n; i++) arr[i] = i + 1;
+    for(int i = 0; i < n; i++) number[i] = i + 1;
+    dfs(0, m, n, 0);
 
-    dfs(0, 0, m, n, 1);
     return 0;
 }
