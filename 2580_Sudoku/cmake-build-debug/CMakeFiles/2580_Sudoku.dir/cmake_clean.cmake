b8f76e03cf25ba531feb13534576c8c55a198ddc
skysaver00 <skysaver00@naver.com>
2021-06-12 12:25:39 +0900

    not solved

---
+++ 2580_Sudoku/cmake-build-debug/CMakeFiles/2580_Sudoku.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2580_Sudoku.exe"
+  "2580_Sudoku.exe.manifest"
+  "2580_Sudoku.pdb"
+  "CMakeFiles/2580_Sudoku.dir/main.cpp.obj"
+  "lib2580_Sudoku.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2580_Sudoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
