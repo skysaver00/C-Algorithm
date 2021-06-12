ef4639ecf09ea13359ff46a8fa1dbb5e7abc9f1c
skysaver00 <skysaver00@naver.com>
2021-06-12 21:11:28 +0900

    완성! 2차 완료

--- Practice_Combination_Backtracking/main.cpp
+++ Practice_Combination_Backtracking/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int number[50];
 bool visit[50];
 
-void dfs(int cnt, int max, int size, int level) {
+void dfs(int idx, int cnt, int max, int size, int level) {
     if(cnt == max) {
         for(int i = 0; i < size; i++) {
             if(visit[i]) cout << number[i] << " ";
@@ -11,13 +11,18 @@ void dfs(int cnt, int max, int size, int level) {
         return;
     }
 
-    
+    for(int i = idx; i < size; i++) {
+        if(visit[i]) continue;
+        visit[i] = true;
+        dfs(i, cnt + 1, max, size, level);
+        visit[i] = false;
+    }
 }
 
 int main() {
     int n, m; cin >> n >> m;
     for(int i = 0; i < n; i++) number[i] = i + 1;
-    dfs(0, m, n, 0);
+    dfs(0, 0, m, n, 0);
 
     return 0;
 }
