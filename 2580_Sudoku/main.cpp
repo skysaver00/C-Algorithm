eae2e21e2530af97d751be486f5939c52f17f9ae
skysaver00 <skysaver00@naver.com>
2021-06-12 13:36:16 +0900

    solved Gold IV 다시 한번 볼것

--- 2580_Sudoku/main.cpp
+++ 2580_Sudoku/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 int sudoku[10][10];
 vector<pair<int, int>> vec;
 
 bool check(int y, int x, int num) {
-    for(int i = 0; i < 9; i++) {
-        if(num == sudoku[i][x]) return false;
-        if(num == sudoku[y][i]) return false;
+    for (int i = 0; i < 9; i++) {
+        if (num == sudoku[i][x]) return false;
+        if (num == sudoku[y][i]) return false;
     }
 
-    for(int i = (y / 3) * 3; i < (y / 3) * 3 + 3; i++) {
-        for(int j = (x / 3) * 3; j < (x / 3) * 3 + 3; j++) {
-            if(num == sudoku[i][j]) return false;
+    for (int i = (y / 3) * 3; i < (y / 3) * 3 + 3; i++) {
+        for (int j = (x / 3) * 3; j < (x / 3) * 3 + 3; j++) {
+            if (num == sudoku[i][j]) return false;
         }
     }
 
@@ -20,27 +21,29 @@ bool check(int y, int x, int num) {
 }
 
 void print() {
-    for(int i = 0; i < 9; i++) {
-        for(int j = 0; j < 9; j++) {
-            
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << sudoku[i][j] << " ";
         }
+        cout << "\n";
     }
+    exit(0);
 }
 
-bool solve(int p) {
-    if(p == vec.size()) print();
+void solve(int p) {
+    if (p == vec.size()) print();
 
     int y = vec[p].first;
     int x = vec[p].second;
 
-    for(int i = 1; i <= 9; i++) {
-        if(check(y, x, i)) {
+    for (int i = 1; i <= 9; i++) {
+        if (check(y, x, i)) {
             sudoku[y][x] = i;
             solve(p + 1);
         }
     }
-
-    return true;
+    sudoku[y][x] = 0;
+    return;
 }
 
 int main() {
