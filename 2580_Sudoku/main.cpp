060a708dd2c074b9635b28639ccb28ce77cb3daf
skysaver00 <skysaver00@naver.com>
2021-06-12 13:04:54 +0900

    not solved

--- 2580_Sudoku/main.cpp
+++ 2580_Sudoku/main.cpp
@@ -4,14 +4,41 @@ using namespace std;
 int sudoku[10][10];
 vector<pair<int, int>> vec;
 
-int check(int i, int j, int num) {
+bool check(int y, int x, int num) {
+    for(int i = 0; i < 9; i++) {
+        if(num == sudoku[i][x]) return false;
+        if(num == sudoku[y][i]) return false;
+    }
+
+    for(int i = (y / 3) * 3; i < (y / 3) * 3 + 3; i++) {
+        for(int j = (x / 3) * 3; j < (x / 3) * 3 + 3; j++) {
+            if(num == sudoku[i][j]) return false;
+        }
+    }
+
+    return true;
+}
+
+void print() {
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            
+        }
+    }
 }
 
 bool solve(int p) {
+    if(p == vec.size()) print();
+
     int y = vec[p].first;
     int x = vec[p].second;
 
-
+    for(int i = 1; i <= 9; i++) {
+        if(check(y, x, i)) {
+            sudoku[y][x] = i;
+            solve(p + 1);
+        }
+    }
 
     return true;
 }
