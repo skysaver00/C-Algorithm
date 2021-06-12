4c4460274e6567f34463482bbab42f1d489e6b15
skysaver00 <skysaver00@naver.com>
2021-06-12 12:33:01 +0900

    not solved

--- 2580_Sudoku/main.cpp
+++ 2580_Sudoku/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 using namespace std;
+int sudoku[10][10];
+bool check[10];
 
-void solve() {
-
+void solve(int i, int j) {
+    for(int a = 0; a < 9; a++) check[a] = false;
+    for(int a = 0; a < 9; a++) {
+        check[sudoku[i][a]] = true;
+        check[sudoku[a][j]] = true;
+    }
+    int x = i / 3;
+    int y = j / 3;
+    for(int a = x * 3; a < x * 3 + 3; a++) {
+        for(int b = y * 3; b < y * 3 + 3; b++) {
+            check[sudoku[a][b]] = true;
+        }
+    }
 }
 
 int main() {
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            cin >> sudoku[i][j];
+        }
+    }
+    solve(0, 0);
 
+    return 0;
 }
