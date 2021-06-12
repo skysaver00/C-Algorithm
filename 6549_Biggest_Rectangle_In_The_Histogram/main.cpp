597fd38eab0d5cbe195abe7fcb1d67c7584bbb24
skysaver00 <skysaver00@naver.com>
2021-06-12 17:58:32 +0900

    not solved

--- 6549_Biggest_Rectangle_In_The_Histogram/main.cpp
+++ 6549_Biggest_Rectangle_In_The_Histogram/main.cpp
@@ -8,19 +8,23 @@ int solve(int left, int right) {
     if(left == right) return height[left];
     int mid = (left + right) / 2;
     int ret = max(solve(left, mid), solve(mid + 1, right));
-
+    cout << "RET..... " << ret << endl;
     int lo = mid, hi = mid + 1;
     int hei = min(height[lo], height[hi]);
     ret = max(ret, hei * 2);
+    cout << "RET HEI..... " << ret << " <> " << hei << endl;
 
     while(left < lo || hi < right) {
         if(hi < right && (lo == left || height[lo - 1] < height[hi + 1])) {
             ++hi;
             hei = min(hei, height[hi]);
+            cout << "Up" << hi << " <> " << hei << endl;
         } else {
             --lo;
             hei = min(hei, height[lo]);
+            cout << "Low" << hi << " <> " << hei << endl;
         }
+        cout << "HI, LO is..... " << hi << " <> " << lo << endl;
         ret = max(ret, hei * (hi - lo + 1));
     }
     return ret;
