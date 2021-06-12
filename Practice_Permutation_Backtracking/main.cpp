cff175b67c8d0926a70d7892177aafd78805e76d
skysaver00 <skysaver00@naver.com>
2021-06-12 13:43:08 +0900

    2차 복습중...

--- Practice_Permutation_Backtracking/main.cpp
+++ Practice_Permutation_Backtracking/main.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 using namespace std;
-int arr[50];
+int number[50];
 int res[50];
-int visit[50];
+bool visit[50];
 
-void dfs(int cnt, int max, int sz, int now) {
+void dfs(int cnt, int max, int size, int level) {
     if(cnt == max) {
-        /*int check = res[0];
-        for(int i = 1; i < max; i++) {
-            if(check > res[i]) return;
-            check = res[i];
-        }*/ //이걸 넣어주면 중복 순열 가능!
-
-        for(int i = 0; i < max; i++) { //찾으려는 개수 도달
-            cout << res[i] << " ";
-        }cout << "\n";
-        now--;
+        for(int i = 0; i < max; i++) cout << res[i] << " ";
+        cout << "\n";
+        level--;
         return;
     }
-    for(int i = 0; i < sz; i++) {
-        //if(visit[i] == true) continue; //이미 방문했다면 패스! 안넣어주면 중복순열
+
+    for(int i = 0; i < size; i++) {
+        if(visit[i]) continue;
+        res[cnt] = number[i];
         visit[i] = true;
-        res[cnt] = arr[i];
-        //cout << "now is: " << now << "\n";
-        dfs(cnt + 1, max, sz, now + 1);
+        dfs(cnt + 1, max, size, level + 1);
         visit[i] = false;
     }
 }
@@ -31,9 +24,8 @@ void dfs(int cnt, int max, int sz, int now) {
 int main() {
     int n, m; cin >> n >> m;
 
-    for(int i = 0; i < n; i++) {
-        arr[i] = i + 1;
-    }
+    for(int i = 0; i < n; i++) number[i] = i + 1;
+    dfs(0, m, n, 0);
 
-    dfs(0, m, n, 1);
+    return 0;
 }
