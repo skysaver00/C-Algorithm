e6cc6dbcc1c4f52e500d098c0c63fbea41e4a82f
skysaver00 <skysaver00@naver.com>
2021-06-14 01:44:44 +0900

    solved Silver V 1.이미 지난 지점이 아님, 2.마지막칸에서 첫칸으로 돌아오기 가능, 3.서로 뺀 칸이 3이지만, 2, 1로 차이가 나야함, 4.체스판을 벗어나면 안됨

--- 1331 Knight_Tour/main.cpp
+++ 1331 Knight_Tour/main.cpp
@@ -3,28 +3,37 @@
 bool chess[7][7];
 
 int main() {
+    char mc;
+    int mn;
     char c1, c2;
     int n1, n2;
     bool check = true;
 
     scanf(" %c%d", &c1, &n1);
+    chess[c1 - 0x41][n1] = true;
     if(c1 <= 'F' && c1 >= 'A' && n1 <= 6 && n1 >= 1) {}
     else check = false;
-
+    mc = c1, mn = n1;
     for(int i = 0; i < 35; i++) {
         scanf(" %c%d", &c2, &n2);
-        printf("%c %d %c %d\n", c1, n1, c2, n2);
+        //printf("%c %d %c %d\n", c1, n1, c2, n2);
+        if(chess[c2 - 0x41][n2]) check = false;
+        if(abs(c1 - c2) == 3 || abs(n1 - n2) == 3) check = false;
 
         if(c2 <= 'F' && c2 >= 'A' && n2 <= 6 && n2 >= 1) {}
         else check = false;
 
         int cnt = abs(c1 - c2) + abs(n1 - n2);
-        printf("%d\n", cnt);
+        //printf("%d\n", cnt);
         if(cnt != 3) check = false;
-        printf("%d\n", check);
+        //printf("%d\n", check);
 
         c1 = c2; n1 = n2;
+        chess[c2 - 0x41][n2] = true;
     }
+    if(abs(mc - c2) == 3 || abs(mn - n2) == 3) check = false;
+    int cnt = abs(mc - c2) + abs(mn - n2);
+    if(cnt != 3) check = false;
 
     if(check) printf("Valid\n");
     else printf("Invalid\n");
