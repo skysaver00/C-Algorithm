1bffed7d60f122e783be269e247fbd7e37879899
skysaver00 <skysaver00@naver.com>
2021-06-13 22:22:49 +0900

    not solved

---
+++ 2239_Sudoku/cmake-build-debug/CMakeFiles/2239_Sudoku.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2239_Sudoku.exe"
+  "2239_Sudoku.exe.manifest"
+  "2239_Sudoku.pdb"
+  "CMakeFiles/2239_Sudoku.dir/main.cpp.obj"
+  "lib2239_Sudoku.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2239_Sudoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 2239_Sudoku/main.cpp
+++ 2239_Sudoku/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string mir[10];
+int sudoku[10][10];
+vector<int> arr;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    for(int i = 0; i < 9; i++) cin >> mir[i];
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            sudoku[i][j] = stoi(mir[i].at(j));
+        }
+    }
+
     return 0;
 }
