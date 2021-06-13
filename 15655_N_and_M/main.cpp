b3c6c6e8c906d010f3f594888c956449a6bebbfa
skysaver00 <skysaver00@naver.com>
2021-06-13 16:38:30 +0900

    solved Silver III

--- 15655_N_and_M/main.cpp
+++ 15655_N_and_M/main.cpp
@@ -4,14 +4,19 @@ using namespace std;
 int number[50];
 int visit[50];
 
-void dfs(int cnt, int max, int size) {
+void dfs(int idx, int cnt, int max, int size) {
     if(cnt == max) {
-        for(int i = 0; i < size; i++) if(visit[i]) printf("%d", number[i]);
+        for(int i = 0; i < size; i++) if(visit[i]) printf("%d ", number[i]);
         printf("\n");
         return;
     }
 
-    
+    for(int i = idx; i < size; i++) {
+        if(visit[i]) continue;
+        visit[i] = true;
+        dfs(i, cnt + 1, max, size);
+        visit[i] = false;
+    }
 }
 
 int main() {
@@ -19,6 +24,6 @@ int main() {
     for(int i = 0; i < n; i++) scanf("%d", &number[i]);
     sort(number, number + n);
 
-    dfs(0, m, n);
+    dfs(0,0, m, n);
     return 0;
 }
