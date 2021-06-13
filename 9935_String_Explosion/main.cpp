6c53b57f13f6746993148662e2ebb4b6f6f40caa
skysaver00 <skysaver00@naver.com>
2021-06-13 19:02:46 +0900

    not solved

--- 9935_String_Explosion/main.cpp
+++ 9935_String_Explosion/main.cpp
@@ -17,18 +17,32 @@ int main() {
     int now = 0;
     for(int i = 0; i < len; i++) {
         if(str.at(i) == bomb.at(now)) {
+            cout << now << " " << bLen << "\n";
             arr[now]++;
             now++;
         } else {
             for(int j = 0; j < bLen; j++) {
                 arr[j] = 0;
-                now = 0;
             }
+            now = 0;
         }
 
         for(int j = 0; j < bLen; j++) {
             printf("%d ", arr[j]);
         }printf("\n");
+
+        if(now == bLen) {
+            for(int j = 0; j < bLen; j++) {
+                arr[j]--;
+            }
+
+            for(int j = 0; j < bLen; j++) {
+                if(arr[j] == 0) {
+                    now = j - 1;
+                    break;
+                }
+            }
+        }
     }
     return 0;
 }
