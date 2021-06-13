f514f491107901a3b45f567cdc7c9fb89b9d7cbb
skysaver00 <skysaver00@naver.com>
2021-06-14 01:05:37 +0900

    좋은수열도 이걸로 가능!

--- Practice_Permutation_Backtracking/main.cpp
+++ Practice_Permutation_Backtracking/main.cpp
@@ -1,38 +1,51 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 int number[50];
 int res[50];
 bool visit[50];
+
+bool is_goodPermutation(int cnt){
+    int len = cnt;
+
+    for(int i=1; i<=len/2; i++){
+        string s1 = "";
+        string s2 = "";
+
+        for(int j=0; j<i; j++){
+            s1 += to_string(res[len - (2*i) + j]);
+            s2 += to_string(res[len - i + j]);
+            //cout << s1 << " " << s2 << "\n";
+        }
+        if(s1 == s2) return false;
+    }
+    return true;
+}
 
 void dfs(int cnt, int max, int size, int level) {
     if(cnt == max) {
         for(int i = 0; i < max - 1; i++) {
             if(res[i] == res[i + 1]) return;
         }
-        /*for(int i = 0; i < max; i++) cout << res[i] << " ";
-        cout << "\n";*/
+        for(int i = 0; i < max; i++) cout << res[i];
+        cout << "\n";
         level--;
-        return;
+        exit(0);
     }
 
     for(int i = 0; i < size; i++) {
         //if(visit[i]) continue;
         res[cnt] = number[i];
         //visit[i] = true;
-        dfs(cnt + 1, max, size, level + 1);
+        bool ck = is_goodPermutation(cnt);
+        if(ck) dfs(cnt + 1, max, size, level + 1);
         //visit[i] = false;
     }
 }
 
 int main() {
-    clock_t start, end;
-    start = time(NULL);
     int n, m; cin >> n >> m;
 
     for(int i = 0; i < n; i++) number[i] = i + 1;
     dfs(0, m, n, 0);
-    end = time(NULL);
-    cout << (double)(end - start) << "\n";
     return 0;
 }
