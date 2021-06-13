45b8ee5028273fd8a82b9b69c2db9482b9a1213b
skysaver00 <skysaver00@naver.com>
2021-06-13 17:00:57 +0900

    not solved

---
+++ 1202_Mushroom_Thief/cmake-build-debug/CMakeFiles/1202_Mushroom_Thief.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1202_Mushroom_Thief.exe"
+  "1202_Mushroom_Thief.exe.manifest"
+  "1202_Mushroom_Thief.pdb"
+  "CMakeFiles/1202_Mushroom_Thief.dir/main.cpp.obj"
+  "lib1202_Mushroom_Thief.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1202_Mushroom_Thief.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1202_Mushroom_Thief/cmake-build-debug/CMakeFiles/1202_Mushroom_Thief.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1202_Mushroom_Thief.
+# This may be replaced when dependencies are built.
