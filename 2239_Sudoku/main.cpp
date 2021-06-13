cf2ad9de07736b4c896cc583ca08359c8590b134
skysaver00 <skysaver00@naver.com>
2021-06-13 22:31:52 +0900

    not solved

--- 2239_Sudoku/main.cpp
+++ 2239_Sudoku/main.cpp
@@ -3,17 +3,60 @@
 #include <vector>
 using namespace std;
 
-string mir[10];
+char mir[10][10];
 int sudoku[10][10];
-vector<int> arr;
+vector<pair<int, int>> arr;
+
+bool check(int y, int x, int num) {
+    for(int i = 0; i < 9; i++) {
+        if(num == sudoku[y][i]) return false;
+        if(num == sudoku[i][x]) return false;
+    }
+
+    for(int i = (y / 3) * 3; i < (y / 3) * 3 + 3; i++) {
+        for(int j = (x / 3) * 3; j < (x / 3) * 3 + 3; j++) {
+            if(num == sudoku[i][j]) return false;
+        }
+    }
+
+    return true;
+}
+
+void print() {
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            cout << 
+        }
+    }
+    exit(0);
+}
+
+int solve(int index) {
+    if(index == arr.size()) print();
+
+    int y = arr[index].first;
+    int x = arr[index].second;
+
+    for(int i = 1; i <= 9; i++) {
+        int ck = check(y, x, i);
+        if(ck) {
+            sudoku[y][x] = i;
+            solve(index + 1);
+        }
+    }
+    sudoku[y][x] = 0;
+}
 
 int main() {
     for(int i = 0; i < 9; i++) cin >> mir[i];
     for(int i = 0; i < 9; i++) {
         for(int j = 0; j < 9; j++) {
-            sudoku[i][j] = stoi(mir[i].at(j));
+            sudoku[i][j] = mir[i][j] - 0x30;
+            if(sudoku[i][j] == 0) arr.push_back({i, j});
         }
     }
+
+    solve(0);
 
     return 0;
 }
