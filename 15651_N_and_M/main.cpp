48c53b0d0a6bbd7af3471fdd8324dc45d1eb3225
skysaver00 <skysaver00@naver.com>
2021-06-13 16:23:41 +0900

    solved Silver III

--- 15651_N_and_M/main.cpp
+++ 15651_N_and_M/main.cpp
@@ -2,9 +2,23 @@
 int number[50];
 int res[50];
 bool visit[50];
+void dfs(int cnt, int max, int size) {
+    if(cnt == max) {
+        for(int i = 0; i < max; i++) printf("%d ", res[i]);
+        printf("\n");
+        return;
+    }
+
+    for(int i = 0; i < size; i++) {
+        res[cnt] = number[i];
+        dfs(cnt + 1, max, size);
+    }
+}
 
 int main() {
-    
+    int n, m; scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++) number[i] = i + 1;
+    dfs(0, m, n);
 
     return 0;
 }
