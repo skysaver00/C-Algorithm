c7ff5395a1f2423190915a67636aada759cf1a57
skysaver00 <skysaver00@naver.com>
2021-06-14 01:06:03 +0900

    이후를 위해 삭제함!

--- Practice_Permutation_Backtracking/main.cpp
+++ Practice_Permutation_Backtracking/main.cpp
@@ -4,23 +4,6 @@ int number[50];
 int res[50];
 bool visit[50];
 
-bool is_goodPermutation(int cnt){
-    int len = cnt;
-
-    for(int i=1; i<=len/2; i++){
-        string s1 = "";
-        string s2 = "";
-
-        for(int j=0; j<i; j++){
-            s1 += to_string(res[len - (2*i) + j]);
-            s2 += to_string(res[len - i + j]);
-            //cout << s1 << " " << s2 << "\n";
-        }
-        if(s1 == s2) return false;
-    }
-    return true;
-}
-
 void dfs(int cnt, int max, int size, int level) {
     if(cnt == max) {
         for(int i = 0; i < max - 1; i++) {
@@ -36,7 +19,6 @@ void dfs(int cnt, int max, int size, int level) {
         //if(visit[i]) continue;
         res[cnt] = number[i];
         //visit[i] = true;
-        bool ck = is_goodPermutation(cnt);
         if(ck) dfs(cnt + 1, max, size, level + 1);
         //visit[i] = false;
     }
