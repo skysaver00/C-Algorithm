ad7fc5a042cecda03d9da751734b2e63691100d4
skysaver00 <skysaver00@naver.com>
2021-06-13 18:52:05 +0900

    not solved

---
+++ 9935_String_Explosion/cmake-build-debug/CMakeFiles/9935_String_Explosion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "9935_String_Explosion.exe"
+  "9935_String_Explosion.exe.manifest"
+  "9935_String_Explosion.pdb"
+  "CMakeFiles/9935_String_Explosion.dir/main.cpp.obj"
+  "lib9935_String_Explosion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9935_String_Explosion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9935_String_Explosion/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+stack<int> st;
+string str;
+string bomb;
+int arr[40];
+
+int main() {
+    cin >> str;
+    cin >> bomb;
+
+    int len = str.length();
+    int bLen = bomb.length();
+    for(int i = 0; i < len; i++) {
+        for(int j = 0; j < bLen; j++) {
+
+        }
+    }
+
+}
