6556a7a0959bc0a7997c869b3aa72db44cda9511
skysaver00 <skysaver00@naver.com>
2021-06-14 00:55:51 +0900

    solved Gold IV 다시 한번 풀어볼것!

--- Practice_Permutation_Backtracking/main.cpp
+++ Practice_Permutation_Backtracking/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 int number[50];
 int res[50];
@@ -6,26 +7,32 @@ bool visit[50];
 
 void dfs(int cnt, int max, int size, int level) {
     if(cnt == max) {
-        for(int i = 0; i < max; i++) cout << res[i] << " ";
-        cout << "\n";
+        for(int i = 0; i < max - 1; i++) {
+            if(res[i] == res[i + 1]) return;
+        }
+        /*for(int i = 0; i < max; i++) cout << res[i] << " ";
+        cout << "\n";*/
         level--;
         return;
     }
 
     for(int i = 0; i < size; i++) {
-        if(visit[i]) continue;
+        //if(visit[i]) continue;
         res[cnt] = number[i];
-        visit[i] = true;
+        //visit[i] = true;
         dfs(cnt + 1, max, size, level + 1);
-        visit[i] = false;
+        //visit[i] = false;
     }
 }
 
 int main() {
+    clock_t start, end;
+    start = time(NULL);
     int n, m; cin >> n >> m;
 
     for(int i = 0; i < n; i++) number[i] = i + 1;
     dfs(0, m, n, 0);
-
+    end = time(NULL);
+    cout << (double)(end - start) << "\n";
     return 0;
 }
