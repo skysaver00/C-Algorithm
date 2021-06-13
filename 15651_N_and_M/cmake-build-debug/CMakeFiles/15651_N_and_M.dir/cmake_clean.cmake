95b21c6827475fe7280ab812c33d0c7911ab47e7
skysaver00 <skysaver00@naver.com>
2021-06-13 16:18:55 +0900

    not solved

---
+++ 15651_N_and_M/cmake-build-debug/CMakeFiles/15651_N_and_M.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15651_N_and_M.exe"
+  "15651_N_and_M.exe.manifest"
+  "15651_N_and_M.pdb"
+  "CMakeFiles/15651_N_and_M.dir/main.cpp.obj"
+  "lib15651_N_and_M.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15651_N_and_M.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 15651_N_and_M/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+int number[50];
+int res[50];
+bool visit[50];
+
+int main() {
+    
+
+    return 0;
+}
