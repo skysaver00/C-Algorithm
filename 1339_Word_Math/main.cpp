f21b4decba265b55b75673bb6348ce0a6c030f78
skysaver00 <skysaver00@naver.com>
2021-06-15 02:22:22 +0900

    not solved

--- 1339_Word_Math/main.cpp
+++ 1339_Word_Math/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 string str[11];
 int arr[11];
@@ -12,30 +13,38 @@ void insertion(int n) {
             if(key > arr[j]) {
                 arr[j + 1] = arr[j];
             } else break;
-
-            for(int k = 0; k < 10; k++) {
-                cout << arr[k] << " ";
-            }cout << "\n";
         }
         arr[j + 1] = key;
     }
+}
+
+bool compare(int i, int j) {
+    return i > j;
 }
 
 int main() {
     int n; cin >> n;
     for(int i = 0; i < n; i++) cin >> str[i];
 
-
     for(int i = 0; i < n; i++) {
         int len = str[i].length();
         int sum = 1;
         for(int j = len - 1; j >= 0; j--) {
-            arr[str[i].at(j) - 0x41] += sum;
+            int val = str[i].at(j) - 0x41;
+            arr[val] += sum;
             sum *= 10;
         }
     }
-    insertion(10);
-    
+    //insertion(10);
+    sort(arr, arr + 10, compare);
+
+    int sum = 0;
+    int num = 9;
+    for(int i = 0; i < 10; i++) {
+        sum += arr[i] * num;
+        num--;
+    }
+    cout << sum << "\n";
 
     return 0;
 }
