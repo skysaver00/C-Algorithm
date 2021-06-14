b32b007b481efa04939f441944574d6dde5e273e
skysaver00 <skysaver00@naver.com>
2021-06-15 01:40:28 +0900

    not solved

--- 1339_Word_Math/main.cpp
+++ 1339_Word_Math/main.cpp
@@ -6,7 +6,19 @@ int arr[11];
 
 int main() {
     int n; cin >> n;
+    for(int i = 0; i < n; i++) cin >> str[i];
+
+
     for(int i = 0; i < n; i++) {
-        
+        int len = str[i].length();
+        int sum = 1;
+        for(int j = len - 1; j >= 0; j--) {
+            arr[str[i].at(j) - 0x41] += sum;
+            sum *= 10;
+        }
+
+        for(int j = 0; j < 10; j++) {
+            cout << arr[j] << " ";
+        }cout << "\n";
     }
 }
