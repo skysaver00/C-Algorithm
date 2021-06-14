c3c91d6c7e9779797c695f8146d12c1cf05f143a
skysaver00 <skysaver00@naver.com>
2021-06-14 13:08:08 +0900

    not solved 방법을 찾은듯?

--- 1715_Card_Sort/main.cpp
+++ 1715_Card_Sort/main.cpp
@@ -16,27 +16,7 @@ int main() {
 
     long long sum = 0;
 
-    long long preCard = cards.top();
-    if(cards.size() == 1) {
-        cout << preCard << "\n";
-        return 0;
-    }
-    cards.pop();
-    preCard += cards.top();
-    cards.pop();
-
-    sum = preCard;
-    cout << sum << "\n";
-
-    int len = cards.size();
-    for(int i = 0; i < len; i++) {
-        int nextCard = preCard + cards.top();
-        cards.pop();
-        sum += nextCard;
-        preCard = nextCard;
-        cout << sum << "\n";
-    }
-    cout << sum << "\n";
+    while(!cards.empty())
 
     return 0;
 }
