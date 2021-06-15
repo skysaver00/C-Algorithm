39def556459276d2add920429d46082931152f03
skysaver00 <skysaver00@naver.com>
2021-06-15 21:04:15 +0900

    not solved

--- 9663_N_Queen/main.cpp
+++ 9663_N_Queen/main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int x[16], y[16];
+int chess[16];
+int ans = 0;
+bool check(int cnt) {
+    for(int i = 0; i < cnt; i++) if(chess[cnt] == chess[i] || cnt - i == abs(chess[cnt] - chess[i])) return false;
+    return true;
+}
 
-int check(int a, int b) {
+void nqueen(int cnt, int n) {
+    if(cnt == n) {
+        ans++;
+        return;
+    }
 
+    for(int i = 0; i < n; i++) {
+        chess[cnt] = i;
+        if(check(cnt)) nqueen(cnt + 1, n);
+    }
 }
 
 int main() {
-    
+    int n; cin >> n;
+    nqueen(0, n);
+
+    cout << ans << "\n";
+
     return 0;
 }
