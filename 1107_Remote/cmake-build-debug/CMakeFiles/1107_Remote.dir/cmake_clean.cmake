40a001823271d5fe370a39387dd13424f78b2820
skysaver00 <skysaver00@naver.com>
2021-06-17 01:34:09 +0900

    not solved

---
+++ 1107_Remote/cmake-build-debug/CMakeFiles/1107_Remote.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1107_Remote.exe"
+  "1107_Remote.exe.manifest"
+  "1107_Remote.pdb"
+  "CMakeFiles/1107_Remote.dir/main.cpp.obj"
+  "lib1107_Remote.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1107_Remote.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 1107_Remote/main.cpp
+++ 1107_Remote/main.cpp
@@ -1,6 +1,53 @@
-#include <iostream>
+#include <stdio.h>
+int dead[11];
+bool chaCh[500001];
+int num = 0;
+int cha;
+int minLimit, maxLimit;
+
+void permutation(int cnt, int max, int size) {
+    if(max <= 0) return;
+    if(cnt == max) {
+        if(chaCh[num]) return;
+        printf("cnt: %d, max: %d", cnt, max);
+        printf(" %d\n", num);
+        chaCh[num] = true;
+        return;
+    }
+
+    for(int i = 0; i < size; i++) {
+        num *= 10;
+        if(dead[i] == 0) num += i;
+        permutation(cnt + 1, max, size);
+        num /= 10;
+    }
+}
+
+int solve(int cha) {
+    int mir = cha;
+    int check = 0;
+    if(mir == 0) check++;
+    while(mir != 0) {
+        mir /= 10;
+        check++;
+    }
+    printf("%d\n", check);
+    permutation(0, check - 1, 10);
+    permutation(0, check, 10);
+    permutation(0, check + 1, 10);
+
+
+    return 0;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    scanf("%d", &cha);
+    int m; scanf("%d", &m);
+
+    for(int i = 0; i < m; i++) {
+        int but; scanf("%d", &but);
+        dead[but] = 1;
+    }
+    solve(cha);
     return 0;
 }
