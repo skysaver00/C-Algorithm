d512c0c633232877745a239b1070b6ed88938fd0
skysaver00 <skysaver00@naver.com>
2021-06-17 01:55:23 +0900

    not solved

--- 1107_Remote/main.cpp
+++ 1107_Remote/main.cpp
@@ -1,23 +1,31 @@
-#include <stdio.h>
-int dead[11];
-bool chaCh[500001];
+#include <cstdio>
+#include <cmath>
+int button[11];
+int live[11];
+bool chaCh[1000001];
 int num = 0;
 int cha;
-int minLimit, maxLimit;
+int ans;
+int c = 0;
+//int minLimit, maxLimit;
+
+int min(int i, int j) {
+    return i < j ? i : j;
+}
 
 void permutation(int cnt, int max, int size) {
-    if(max <= 0) return;
+    if(max <= 0 || max >= 7) return;
     if(cnt == max) {
         if(chaCh[num]) return;
-        printf("cnt: %d, max: %d", cnt, max);
-        printf(" %d\n", num);
+        ans = min(ans, cnt + abs(num - cha));
         chaCh[num] = true;
         return;
     }
 
     for(int i = 0; i < size; i++) {
         num *= 10;
-        if(dead[i] == 0) num += i;
+        num += live[i];
+        //printf("%d, %d, %d\n", num, i, live[i]);
         permutation(cnt + 1, max, size);
         num /= 10;
     }
@@ -31,23 +39,29 @@ int solve(int cha) {
         mir /= 10;
         check++;
     }
-    printf("%d\n", check);
-    permutation(0, check - 1, 10);
-    permutation(0, check, 10);
-    permutation(0, check + 1, 10);
-
-
+    permutation(0, check - 1, c);
+    permutation(0, check, c);
+    permutation(0, check + 1, c);
     return 0;
 }
 
 int main() {
     scanf("%d", &cha);
+    ans = abs(cha - 100);
     int m; scanf("%d", &m);
 
     for(int i = 0; i < m; i++) {
         int but; scanf("%d", &but);
-        dead[but] = 1;
+        button[but] = 1;
+    }
+
+    for(int i = 0; i < 10; i++) {
+        if(button[i] == 0) {
+            live[c] = i;
+            c++;
+        }
     }
     solve(cha);
+    printf("%d\n", ans);
     return 0;
 }
