bf9d533db1df77792a01d517364299b7921bf0c7
skysaver00 <skysaver00@naver.com>
2021-06-17 15:40:12 +0900

    solved Silver III

--- 15654_N_and_M/main.cpp
+++ 15654_N_and_M/main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int num[50];
+int res[50];
+bool visit[50];
+
+void solve(int cnt, int max, int size) {
+    if(cnt == max) {
+        for(int i = 0; i < max; i++) {
+            printf("%d ", res[i]);
+        }printf("\n");
+        return;
+    }
+
+    for(int i = 0; i < size; i++) {
+        if(visit[i]) continue;
+        res[cnt] = num[i];
+        visit[i] = true;
+        solve(cnt + 1, max, size);
+        visit[i] = false;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m; scanf("%d %d", &n, &m);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &num[i]);
+    }
+    sort(num, num + n);
+    solve(0, m, n);
+
     return 0;
 }
