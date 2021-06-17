6251d14edb6f91802f8d74ba546b744bff9eb147
skysaver00 <skysaver00@naver.com>
2021-06-17 14:29:19 +0900

    solved Silver III

--- 15652_N_and_M/main.cpp
+++ 15652_N_and_M/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#include <cstdio>
+int num[50];
+int res[50];
+
+void solve(int cnt, int max, int size) {
+    if(cnt == max) {
+        for(int i = 0; i < cnt - 1; i++) {
+            if(res[i + 1] < res[i]) return;
+        }
+        for(int i = 0; i < cnt; i++) {
+            printf("%d ", res[i]);
+        }printf("\n");
+        return;
+    }
+
+    for(int i = 0; i < size; i++) {
+        res[cnt] = num[i];
+        solve(cnt + 1, max, size);
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int m, n; scanf("%d %d", &m, &n);
+
+    for(int i = 0; i < m; i++) num[i] = i + 1;
+    solve(0, n, m);
     return 0;
 }
