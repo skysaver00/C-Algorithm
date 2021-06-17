6daf5050bc5fb28509de537ec96950ca29dd5ce2
skysaver00 <skysaver00@naver.com>
2021-06-17 15:28:25 +0900

    not solved

---
+++ 1976_Lets_Go_Vacation/cmake-build-debug/CMakeFiles/1976_Lets_Go_Vacation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1976_Lets_Go_Vacation.exe"
+  "1976_Lets_Go_Vacation.exe.manifest"
+  "1976_Lets_Go_Vacation.pdb"
+  "CMakeFiles/1976_Lets_Go_Vacation.dir/main.cpp.obj"
+  "lib1976_Lets_Go_Vacation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1976_Lets_Go_Vacation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1976_Lets_Go_Vacation/cmake-build-debug/CMakeFiles/1976_Lets_Go_Vacation.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1976_Lets_Go_Vacation.
+# This may be replaced when dependencies are built.
