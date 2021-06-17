96e0d12c1551643ec1a1ee80c1b5a3c0cfdff74b
skysaver00 <skysaver00@naver.com>
2021-06-17 15:28:59 +0900

    not solved

---
+++ 15654_N_and_M/cmake-build-debug/CMakeFiles/15654_N_and_M.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15654_N_and_M.exe"
+  "15654_N_and_M.exe.manifest"
+  "15654_N_and_M.pdb"
+  "CMakeFiles/15654_N_and_M.dir/main.cpp.obj"
+  "lib15654_N_and_M.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15654_N_and_M.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 15654_N_and_M/cmake-build-debug/CMakeFiles/15654_N_and_M.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 15654_N_and_M.
+# This may be replaced when dependencies are built.
