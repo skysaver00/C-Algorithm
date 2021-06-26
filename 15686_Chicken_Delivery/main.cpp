08bc47da8bbeacfe74c7f3ad60c4a113541caac9
skysaver00 <skysaver00@naver.com>
2021-06-26 15:51:22 +0900

    not solved

--- 15686_Chicken_Delivery/main.cpp
+++ 15686_Chicken_Delivery/main.cpp
@@ -2,3 +2,41 @@
 #include <vector>
 using namespace std;
 
+int n, m;
+int min;
+int dis[20];
+int visit[20];
+int axis[51][51];
+vector<pair<int, int>> vec;
+
+void combination(int idx, int cnt, int max, int size) {
+    if(cnt == max) {
+        for(int i = 0; i < size; i++) {
+            if(visit[i] == true) {
+                printf("%d %d: ", vec[i].first, vec[i].second);
+            }printf("\n");
+        }
+    }
+
+    for(int i = idx; i < size; i++) {
+        if(visit[i]) continue;
+        visit[i] = true;
+        combination(i, cnt + 1, max, size);
+        visit[i] = false;
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> axis[i][j];
+            if(axis[i][j] == 2) vec.push_back({i, j});
+        }
+    }
+
+    int sz = vec.size();
+    combination(0, 0, m, sz);
+
+    return 0;
+}
