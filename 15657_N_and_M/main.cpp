33d4d724166e5d92df8fa0955040c2cf5b0c1211
skysaver00 <skysaver00@naver.com>
2021-06-26 15:30:45 +0900

    solved Silver III

--- 15657_N_and_M/main.cpp
+++ 15657_N_and_M/main.cpp
@@ -5,9 +5,27 @@ int res[50];
 
 void sort(int n) {
     for(int i = 0; i < n - 1; i++) {
-        for(int j = i + 1; i < n; i++) {
-            if(num[i] > num[j]) 
+        for(int j = i + 1; j < n; j++) {
+            if(num[i] > num[j]) {
+                int temp = num[i];
+                num[i] = num[j];
+                num[j] = temp;
+            }
         }
+    }
+}
+
+void permutation(int cnt, int max, int size) {
+    if(cnt == max) {
+        for(int i = 0; i < cnt - 1; i++) if(res[i] > res[i + 1]) return;
+        for(int i = 0; i < cnt; i++) printf("%d ", res[i]);
+        printf("\n");
+        return;
+    }
+
+    for(int i = 0; i < size; i++) {
+        res[cnt] = num[i];
+        permutation(cnt + 1, max, size);
     }
 }
 
@@ -15,4 +33,7 @@ int main() {
     int n, m; scanf("%d %d", &n, &m);
     for(int i = 0; i < n; i++) scanf("%d", &num[i]);
     sort(n);
+    permutation(0, m, n);
+
+    return 0;
 }
