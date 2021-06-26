5b29a932d53c352da919276886de2463f0f34aff
skysaver00 <skysaver00@naver.com>
2021-06-26 16:16:01 +0900

    not solved

--- 15686_Chicken_Delivery/main.cpp
+++ 15686_Chicken_Delivery/main.cpp
@@ -2,20 +2,35 @@
 #include <vector>
 using namespace std;
 
+int min(int i, int j) {
+    return (i < j) ? i : j;
+}
+
 int n, m;
-int min;
-int dis[20];
+int minimum;
+int dis[110];
 int visit[20];
 int axis[51][51];
-vector<pair<int, int>> vec;
+vector<pair<int, int>> vec_1;
+vector<pair<int, int>> vec_2;
 
 void combination(int idx, int cnt, int max, int size) {
     if(cnt == max) {
+        int sz = vec_1.size();
         for(int i = 0; i < size; i++) {
-            if(visit[i] == true) {
-                printf("%d %d: ", vec[i].first, vec[i].second);
-            }printf("\n");
+            if(visit[i]) {
+                for(int j = 0; j < sz; j++)
+                    dis[j] = min(abs(vec_1[j].first - vec_2[i].first) + abs(vec_1[j].second - vec_2[i].second), dis[j]);
+            }
         }
+        int disCount = 0;
+        for(int i = 0; i < sz; i++) {
+            cout << dis[i] << " ";
+            disCount += dis[i];
+        }cout << "\n";
+        minimum = min(minimum, disCount);
+        for(int i = 0; i < sz; i++) dis[i] = 99999999;
+        return;
     }
 
     for(int i = idx; i < size; i++) {
@@ -31,12 +46,16 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             cin >> axis[i][j];
-            if(axis[i][j] == 2) vec.push_back({i, j});
+            if(axis[i][j] == 2) vec_2.push_back({i, j});
+            else if(axis[i][j] == 1) vec_1.push_back({i, j});
         }
     }
 
-    int sz = vec.size();
-    combination(0, 0, m, sz);
-
+    minimum = 999999999;
+    int sz = vec_1.size();
+    for(int i = 0; i < sz; i++) dis[i] = 99999999;
+    int sz2 = vec_2.size();
+    combination(0, 0, m, sz2);
+    cout << minimum << "\n";
     return 0;
 }
