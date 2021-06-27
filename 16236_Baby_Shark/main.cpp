4b0accd251cfd50f22507bb97c7bfd9e6bb5f600
skysaver00 <skysaver00@naver.com>
2021-06-27 15:30:50 +0900

    not solved

--- 16236_Baby_Shark/main.cpp
+++ 16236_Baby_Shark/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <queue>
+using namespace std;
+
+int n;
+int level = 2;
+int map[21][21];
+int ckMap[21][21];
+queue<pair<int, int>> que;
+pair<int, int> point;
+
+int x[4] = {0, -1, 1, 0};
+int y[4] = {1, 0, 0, -1};
+
+void solve() {
+    
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> map[i][j];
+            if(map[i][j] == 9) {
+                point.first = i;
+                point.second = j;
+            }
+        }
+    }
+
+    solve();
+
     return 0;
 }
