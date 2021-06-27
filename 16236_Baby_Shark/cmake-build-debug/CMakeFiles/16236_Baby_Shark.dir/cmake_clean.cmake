d2f51a57071599da82037a55527a0e839637be84
skysaver00 <skysaver00@naver.com>
2021-06-27 15:16:27 +0900

    not solved

---
+++ 16236_Baby_Shark/cmake-build-debug/CMakeFiles/16236_Baby_Shark.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "16236_Baby_Shark.exe"
+  "16236_Baby_Shark.exe.manifest"
+  "16236_Baby_Shark.pdb"
+  "CMakeFiles/16236_Baby_Shark.dir/main.cpp.obj"
+  "lib16236_Baby_Shark.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/16236_Baby_Shark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
