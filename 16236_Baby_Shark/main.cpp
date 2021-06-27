eae3c4b8cd5cddf18f4fb3800be50f1b4405cae4
skysaver00 <skysaver00@naver.com>
2021-06-27 15:58:18 +0900

    not solved

--- 16236_Baby_Shark/main.cpp
+++ 16236_Baby_Shark/main.cpp
@@ -9,10 +9,64 @@ queue<pair<int, int>> que;
 pair<int, int> point;
 
 int x[4] = {0, -1, 1, 0};
-int y[4] = {1, 0, 0, -1};
+int y[4] = {-1, 0, 0, 1};
+
+void bfs() {
+    que.push({point.first, point.second});
+    ckMap[point.first][point.second] = 1;
+
+    while(!que.empty()) {
+        int i = que.front().first;
+        int j = que.front().second;
+
+        que.pop();
+        bool flag = false;
+        for (int a = 0; a < 4; a++) {
+            int ii = i + x[a];
+            int jj = j + y[a];
+
+            if (ii > n && ii < 0 && jj > n && jj < 0) continue;
+            else {
+                if (map[ii][jj] < level) {
+                    ckMap[ii][jj] = ckMap[i][j] + 1;
+                    que.push({ii, jj});
+                } else {
+                    ckMap[ii][jj] = ckMap[i][j] + 1;
+
+                    for(int b = 0; b < n; b++){
+                        for(int c = 0; c < n; c++) {
+                            printf("%d ", ckMap[b][c]);
+                        }printf("\n");
+                    }printf("\n");
+
+                    cnt += ckMap[ii][jj];
+                    map[ii][jj] = 0;
+
+                    point.first = ii, point.second = jj;
+                    flag = true;
+                }
+            }
+            if (flag) break;
+        }
+        if (flag) while(!que.empty()) que.pop();
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            ckMap[i][j] = 0;
+        }
+    }
+    return;
+}
 
 void solve(int ate) {
     bool possible = false;
+    int food = ate;
+    if(food == level) {
+        level++;
+        food = 0;
+    }
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             if(map[i][j] < level) possible = true;
@@ -20,7 +74,8 @@ void solve(int ate) {
     }
 
     if(possible) {
-        
+        bfs();
+        solve(food + 1);
     } else return;
 }
 
