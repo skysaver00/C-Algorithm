2e84f2450e24464afc18c3480cfb996764c315d5
skysaver00 <skysaver00@naver.com>
2021-06-28 21:54:38 +0900

    solved Silver IV

--- 9471_Pisano_Period/main.cpp
+++ 9471_Pisano_Period/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int pisano(int n, int m) {
+    int per = 0;
+    int o1 = 0, o2 = 1;
+    do {
+        int temp = o1;
+        o1 = o2;
+        o2 = (temp + o1) % m;
+        per++;
+    } while(o1 != 0 || o2 != 1);
+    return per;
+}
+
 int main() {
     int p;
     cin >> p;
@@ -8,6 +20,8 @@ int main() {
     int n, m;
     for(int i = 0; i < p; i++) {
         cin >> n >> m;
+        int val = pisano(n, m);
+        cout << n << " " << val << "\n";
     }
     return 0;
 }
