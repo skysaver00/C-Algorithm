e9f211bf1ff9705080e789e99a583d8400e1817c
skysaver00 <skysaver00@naver.com>
2021-06-28 17:18:36 +0900

    solved Gold IV

--- 14002_Longest_Increasing_Partial_Sequence_4/main.cpp
+++ 14002_Longest_Increasing_Partial_Sequence_4/main.cpp
@@ -13,7 +13,6 @@ int main() {
         check[i] = 1;
     }
 
-    check[1] = 1;
     for(int i = 2; i <= n; i++) {
         for(int j = 1; j < i; j++) {
             if(arr[i] > arr[j]) {
@@ -24,18 +23,6 @@ int main() {
             }
         }
     }
-
-    /*for(int i = 1; i <= n; i++) {
-        cout << arr[i] << "\t";
-    }cout << "\n";
-
-    for(int i = 1; i <= n; i++) {
-        cout << check[i] << "\t";
-    }cout << "\n";
-
-    for(int i = 1; i <= n; i++) {
-        cout << v[i] << "\t";
-    }cout << "\n";*/
 
     int max = 0;
     int maxLoc = 0;
