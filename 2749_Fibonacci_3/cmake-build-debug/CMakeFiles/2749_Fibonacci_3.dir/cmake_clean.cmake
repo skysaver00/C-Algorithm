3d7063c8348b8f08b0157f3f558175da47365c3b
skysaver00 <skysaver00@naver.com>
2021-06-28 20:10:51 +0900

    not solved

---
+++ 2749_Fibonacci_3/cmake-build-debug/CMakeFiles/2749_Fibonacci_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2749_Fibonacci_3.exe"
+  "2749_Fibonacci_3.exe.manifest"
+  "2749_Fibonacci_3.pdb"
+  "CMakeFiles/2749_Fibonacci_3.dir/main.cpp.obj"
+  "lib2749_Fibonacci_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2749_Fibonacci_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
