dfbb54cbd55eed9cc7a36870d6bebd39822ca043
skysaver00 <skysaver00@naver.com>
2021-06-30 21:30:15 +0900

    not solved

--- 11444_Fibonacci_6/main.cpp
+++ 11444_Fibonacci_6/main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int fibo[65][2][2];
+long long fibo[65][2][2];
 int bin[65];
-int val[2][2][2];
+long long val[2][2][2];
 
+int makeFibo(int cnt) {
 
+}
+
+int makeVal(int cnt) {
+    
+}
 
 int main() {
+    long long n; cin >> n;
+    long long m = n;
+    int cnt = 0;
 
+    while(m != 0) {
+        if(m % 2 == 0);
+        else bin[cnt] = 1;
+        cnt++;
+        m /= 2;
+    }
+
+    cout << cnt << "\n";
+    for(int i = 0; i < cnt; i++) {
+        cout << bin[i] << " ";
+    }cout << "\n";
+
+    makeFibo(cnt);
+    makeVal(cnt);
+
+    return 0;
 }
