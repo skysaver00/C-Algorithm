88176e9ae8bcb6627cb299cc6fae9015aedc7ab1
skysaver00 <skysaver00@naver.com>
2021-07-01 19:33:02 +0900

    not solved

--- 11778_Fibonacci_And_GCD/main.cpp
+++ 11778_Fibonacci_And_GCD/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <cstdio>
+#include <numeric>
+using namespace std;
+
+long long fibo[65][2][2];
+int cnt[65];
+long long val[2][2][2];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long long n, m; scanf("%lld %lld", &n, &m);
+    //gcd(f(n), f(m)) == f(gcd(n, m))
+    long long nm = gcd(n, m);
+    printf("%lld\n",  nm);
+
     return 0;
 }
