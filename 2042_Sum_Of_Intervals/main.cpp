94759dd7da4ea4df2033439d0b36306162071a74
skysaver00 <skysaver00@naver.com>
2021-07-01 17:56:31 +0900

    not solved

--- 2042_Sum_Of_Intervals/main.cpp
+++ 2042_Sum_Of_Intervals/main.cpp
@@ -9,11 +9,29 @@ long long init(int start, int end, int node) {
     return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
 }
 
+long long sum(int start, int end, int node, int left, int right) {
+    if(left > end || right < start) return 0;
+    if(left <= start && end <= right) return tree[node];
+    int mid = (start + end) / 2;
+    return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+}
+
 int main() {
     int n, m, k; scanf("%d %d %d", &n, &m, &k);
     for(int i = 0; i < n; i++) scanf("%lld", &num[i]);
 
     init(0, n - 1, 1);
+
+    int a, b, c;
+    for(int i = 0; i < m + k; i++) {
+        scanf("%d %d %d", &a, &b, &c);
+        if(a == 1) {
+
+        } else if(a == 2) {
+            long long val = sum(0, n - 1, 1, b, c);
+            printf("%lld\n", val);
+        }
+    }
 
     for(int i = 0; i < 4 * n; i++) printf("%lld ", tree[i]);
 
