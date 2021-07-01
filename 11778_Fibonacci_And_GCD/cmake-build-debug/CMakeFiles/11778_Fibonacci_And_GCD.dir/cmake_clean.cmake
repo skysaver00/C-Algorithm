52608198433b6916366d39fe205331b52cca2fa6
skysaver00 <skysaver00@naver.com>
2021-07-01 18:58:50 +0900

    not solved

---
+++ 11778_Fibonacci_And_GCD/cmake-build-debug/CMakeFiles/11778_Fibonacci_And_GCD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11778_Fibonacci_And_GCD.exe"
+  "11778_Fibonacci_And_GCD.exe.manifest"
+  "11778_Fibonacci_And_GCD.pdb"
+  "CMakeFiles/11778_Fibonacci_And_GCD.dir/main.cpp.obj"
+  "lib11778_Fibonacci_And_GCD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11778_Fibonacci_And_GCD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11778_Fibonacci_And_GCD/cmake-build-debug/CMakeFiles/11778_Fibonacci_And_GCD.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 11778_Fibonacci_And_GCD.
+# This may be replaced when dependencies are built.
