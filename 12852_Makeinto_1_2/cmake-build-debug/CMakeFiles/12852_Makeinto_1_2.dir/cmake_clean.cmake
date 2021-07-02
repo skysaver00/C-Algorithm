1bf303a70e4878520a85d35ad7d666ce4f146e41
skysaver00 <skysaver00@naver.com>
2021-07-02 17:19:40 +0900

    not solved

---
+++ 12852_Makeinto_1_2/cmake-build-debug/CMakeFiles/12852_Makeinto_1_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "12852_Makeinto_1_2.exe"
+  "12852_Makeinto_1_2.exe.manifest"
+  "12852_Makeinto_1_2.pdb"
+  "CMakeFiles/12852_Makeinto_1_2.dir/main.cpp.obj"
+  "lib12852_Makeinto_1_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12852_Makeinto_1_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 12852_Makeinto_1_2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int val[1000001];
+int track[1000001];
+
+int main() {
+    int n; cin >> n;
+
+
+}
