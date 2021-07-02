39b7d8a4016b9d1cb501b15514d54f48b6e370de
skysaver00 <skysaver00@naver.com>
2021-07-02 17:17:27 +0900

    solved Gold V

--- 9252_LCS_2/main.cpp
+++ 9252_LCS_2/main.cpp
@@ -21,18 +21,27 @@ int main() {
         for(int j = 1; j <= len1; j++) {
             if(str1.at(j - 1) == str2.at(i - 1)) {
                 lcs[i][j] = lcs[i - 1][j - 1] + 1;
-                if(!str1Ck[i] && !str2Ck[j]) {
-                    overall += str1.at(j - 1);
-                    str1Ck[i] = true, str2Ck[j] = true;
-                }
             } else {
                 lcs[i][j] = MAX(lcs[i - 1][j], lcs[i][j - 1]);
             }
         }
     }
 
+    int i = len2, j = len1;
+    while(i != 0 && j != 0) {
+        if(lcs[i][j] == lcs[i][j - 1]) {
+            j -= 1;
+        } else if(lcs[i][j] == lcs[i - 1][j]) {
+            i -= 1;
+        } else {
+            overall += str2.at(i - 1);
+            i -= 1, j -= 1;
+        }
+    }
+
     cout << lcs[len2][len1] << "\n";
-    cout << overall << "\n";
+    i = overall.length();
+    for(j = i - 1; j >= 0 ; j--) cout << overall.at(j);
 
     return 0;
 }
