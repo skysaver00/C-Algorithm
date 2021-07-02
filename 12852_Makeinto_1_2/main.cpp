be6f8e8b4a725fa7ea29148cb703fa9c432744c6
skysaver00 <skysaver00@naver.com>
2021-07-02 17:50:50 +0900

    not solved

--- 12852_Makeinto_1_2/main.cpp
+++ 12852_Makeinto_1_2/main.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int val[1000001];
 int track[1000001];
+stack<int> st;
 
 int MIN(int i, int j) {
     return i < j ? i : j;
 }
 
 void solve(int n) {
-    for(int i = n; i >= 1; i--) {
+    for(int i = n - 1; i >= 1; i--) {
         if(i * 3 <= n) {
-            val[i] = MIN(val[i * 3], val[i]) + 1;
-            if(val[i * 3] + 1 == val[i]) track[i] = i * 3;
+            val[i] = MIN(val[i * 3], val[i]);
+            if(val[i * 3] == val[i]) track[i] = i * 3;
         }
 
         if(i * 2 <= n) {
-            val[i] = MIN(val[i * 2], val[i]) + 1;
-            if(val[i * 2] + 1 == val[i]) track[i] = i * 2;
+            val[i] = MIN(val[i * 2], val[i]);
+            if(val[i * 2] == val[i]) track[i] = i * 2;
         }
 
-        val[i] = MIN(val[i + 1], val[i]) + 1;
-        if(val[i + 1] + 1 == val[i]) track[i] = i + 1;
+        val[i] = MIN(val[i + 1], val[i]);
+        if(val[i + 1] == val[i]) track[i] = i + 1;
+        val[i] += 1;
     }
 }
 
 int main() {
     int n; cin >> n;
     for(int i = 0; i <= n; i++) val[i] = 999999999;
+    val[n] = 0, track[n] = -1;
     solve(n);
 
-    for(int i = 1; i <= 10; i++) {
-        cout << i << val[i] << track[i] << "\n";
+    cout << val[1] << "\n";
+    st.push(1);
+    int find = track[1];
+    while(find != -1) {
+        st.push(find);
+        find = track[find];
     }
+    while(!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }cout << "\n";
 }
