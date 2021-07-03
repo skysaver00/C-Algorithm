195d70d2bcf04d28b778208176e15bb7b52e00e4
skysaver00 <skysaver00@naver.com>
2021-07-03 16:40:02 +0900

    not solveD

--- 10868_Minimum_Value/main.cpp
+++ 10868_Minimum_Value/main.cpp
@@ -4,13 +4,25 @@ int arr[100001];
 int tree[400001];
 
 int min(int i, int j) {
-    return i < j ? i : j;
+    return (i < j) ? i : j;
 }
 
 int init(int start, int end, int node) {
     if(start == end) return tree[node] = arr[start];
     int mid = (start + end) / 2;
-    return tree[node] = min(init(start, mid, node * 2), init(mid + 1, end, node * 2 + 1));
+
+    int leftMinValue = init(start, mid, node * 2);
+    int rightMinValue = init(mid + 1, end, node * 2 + 1);
+
+    return tree[node] = min(leftMinValue, rightMinValue);
+}
+
+int ans(int start, int end, int node, int left, int right) {
+    if(end < left || right < start) return 0;
+    if(left <= start && end <= right) return tree[node];
+
+    int mid = (start + end) / 2;
+    return min(ans(start, mid, node * 2, left, right), ans(mid + 1, end, node * 2 + 1, left, right));
 }
 
 int main() {
@@ -24,6 +36,13 @@ int main() {
     init(0, n - 1, 1);
     for(int i = 0; i < 4 * n; i++) {
         cout << tree[i] << " ";
+    }
+
+    int left, right;
+    for(int i = 0; i < m; i++) {
+        cin >> left >> right;
+        int val = ans(0, n - 1, 1, left - 1, right - 1);
+        cout << val << "\n";
     }
 
     return 0;
