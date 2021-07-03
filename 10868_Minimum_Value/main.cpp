61baf56f1eb6a48e477c04c8903e83daf3c2fc35
skysaver00 <skysaver00@naver.com>
2021-07-03 17:24:43 +0900

    solved Gold I

--- 10868_Minimum_Value/main.cpp
+++ 10868_Minimum_Value/main.cpp
@@ -1,48 +1,41 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 using namespace std;
+int arr[100001];
+int tree[400001];
 
 int min(int i, int j) {
     return (i < j) ? i : j;
 }
 
-int init(vector<int> &arr, vector<int> &tree, int start, int end, int node) {
+int init(int start, int end, int node) {
     if(start == end) return tree[node] = arr[start];
     int mid = (start + end) / 2;
 
-    return tree[node] = min(init(arr, tree, start, mid, node * 2),
-                            init(arr, tree, mid + 1, end, node * 2 + 1));
+    return tree[node] = min(init(start, mid, node * 2), init(mid + 1, end, node * 2 + 1));
 }
 
-int ans(vector<int> &arr, vector<int> &tree, int start, int end, int node, int left, int right) {
-    if(start > right || end < left) return 1500000000;
-    if(start >= left && end <= right) return tree[node];
+int ans(int start, int end, int node, int left, int right) {
+    if(end < left || right < start) return 2000000000;
+    if(left <= start && end <= right) return tree[node];
 
     int mid = (start + end) / 2;
-    return min(ans(arr, tree, start, mid, node * 2, left, right),
-               ans(arr, tree, mid + 1, end, node * 2 + 1, left, right));
+    return min(ans(start, mid, node * 2, left, right), ans(mid + 1, end, node * 2 + 1, left, right));
 }
 
 int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int n, m; cin >> n >> m;
-    int h = (int)ceil(log2(n));
-    int size = (1 << (1 + h));
-
-    vector<int> arr(n);
-    vector<int> tree(size);
-
     for(int i = 0; i < n; i++) cin >> arr[i];
 
-    init(arr, tree, 0, n - 1, 1);
-    for(int i = 0; i < 4 * n; i++) {
-        cout << tree[i] << " ";
-    }
+    init(0, n - 1, 1);
 
     int left, right;
     for(int i = 0; i < m; i++) {
         cin >> left >> right;
-        int val = ans(arr, tree, 0, n - 1, 1, left - 1, right - 1);
+        int val = ans(0, n - 1, 1, left - 1, right - 1);
         cout << val << "\n";
     }
 
