09d26bcbac75f2b462fa755ab9440791e3515d35
skysaver00 <skysaver00@naver.com>
2021-07-14 22:22:41 +0900

    not solved

--- 1931_Conference_Room_Distribution/main.cpp
+++ 1931_Conference_Room_Distribution/main.cpp
@@ -14,9 +14,16 @@ int main() {
     }
     sort(vec.begin(), vec.end());
 
-    for(int i = 0; i < n; i++) {
-        cout << vec[i].first << " " << vec[i].second << "\n";
+    int ans = 1;
+    int time = vec[0].first;
+    for(int i = 1; i < n; i++) {
+        if(time <= vec[i].second) {
+            time = vec[i].first;
+            ans++;
+            cout << vec[i].first << " " << vec[i].second << "\n";
+        }
     }
 
+    cout << ans << "\n";
     return 0;
 }
