7cb61f99c703c8b2ad0228ca16a8f4d9533b9103
skysaver00 <skysaver00@naver.com>
2021-07-14 22:49:36 +0900

    not solved

---
+++ 15656_N_and_M/cmake-build-debug/CMakeFiles/15656_N_and_M.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 15656_N_and_M.
+# This may be replaced when dependencies are built.
---
+++ 15656_N_and_M/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int arr[10];
+int res[10];
+
+void Multiset(int cnt, int n, int m) {
+
+}
+
+int main() {
+    int n, m; cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    Multiset(0, n, m);
+    return 0;
+}
