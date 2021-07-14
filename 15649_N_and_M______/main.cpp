a7e771e7b64c30a3c006457f5036ef114b01ba97
skysaver00 <skysaver00@naver.com>
2021-07-14 22:47:10 +0900

    solved Silver III

--- 15649_N_and_M______/main.cpp
+++ 15649_N_and_M______/main.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
 using namespace std;
+int arr[10];
+int res[10];
+bool visit[10];
 
-int num[10];
-bool t_f[10];
-int perArr[10];
+void Permutation(int cnt, int m, int n) {
+    if(cnt == m) {
+        for(int i = 0; i < m; i++)
+            cout << res[i] << " ";
+        cout << "\n";
 
-void print(int max) {
-    for(int i = 0; i < max; i++) {
-        cout << perArr[i] << " ";
-    }cout << "\n";
-}
+        return;
+    }
 
-void Permutation(int max, int size, int at) {
-    if(max == at) print(max);
-
-    for(int i = 0; i < size; i++) {
-        if(t_f[i]) continue;
-        perArr[at] = num[i];
-        t_f[i] = true;
-        Permutation(max, size, at);
-        t_f[i] = false;
+    for(int i = 0; i < n; i++) {
+        if(visit[i]) continue;
+        res[cnt] = arr[i];
+        visit[i] = true;
+        Permutation(cnt + 1, m, n);
+        visit[i] = false;
     }
 }
 
 int main() {
-    int m, n;
-    cin >> m >> n;
+    int n, m; cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        arr[i] = i + 1;
 
-    for(int i = 0; i < m; i++)
-        num[i] = i + 1;
-
-    Permutation(0, n, 0);
-
-    return 0l;
+    Permutation(0, m, n);
+    return 0;
 }
