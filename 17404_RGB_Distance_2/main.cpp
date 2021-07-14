269a68564678c9260b4f1a2ec622d33ead7c4b98
skysaver00 <skysaver00@naver.com>
2021-07-15 01:55:21 +0900

    not solved

--- 17404_RGB_Distance_2/main.cpp
+++ 17404_RGB_Distance_2/main.cpp
@@ -3,12 +3,38 @@ int choice[1001][3];
 int val[1001];
 int col[1001];
 
+int min(int i, int j) {
+    return i < j ? i : j;
+}
+
 int main() {
     int n; scanf("%d", &n);
     for(int i = 0; i < n; i++)
         scanf("%d %d %d", &choice[i][0], &choice[i][1], &choice[i][2]);
 
-    
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < n; j++) col[j] = val[j] = 0;
+
+        col[0] = i;
+        val[0] = choice[0][i];
+        for(int j = 1; j < n - 1; j++) {
+            if(col[j - 1] == 0) {
+                val[j] = min(choice[j][1], choice[j][2]);
+            } else if(col[j - 1] == 1) {
+                val[j] = min(choice[j][0], choice[j][2]);
+            } else if(col[i - 2] == 2) {
+                val[j] = min(choice[j][0], choice[j][1]);
+            }
+
+            if(val[j] == choice[j][0]) col[j] = 0;
+            else if(val[j] == choice[j][1]) col[j] = 1;
+            else col[j] = 2;
+        }
+
+        int sum = 0;
+        for(int j = 0; j < n; j++) sum += val[j];
+        printf("%d\n", sum);
+    }
 
     return 0;
 }
