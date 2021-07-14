f68f55f268b76841a9c90df4a51322ee7ceab80f
skysaver00 <skysaver00@naver.com>
2021-07-14 22:13:24 +0900

    not solved

---
+++ 1931_Conference_Room_Distribution/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_5b88f.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_5b88f.dir/CMakeCXXCompilerABI.cpp.obj"
+  "cmTC_5b88f.exe"
+  "cmTC_5b88f.exe.manifest"
+  "cmTC_5b88f.pdb"
+  "libcmTC_5b88f.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_5b88f.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1931_Conference_Room_Distribution/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_5b88f.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_5b88f.
+# This may be replaced when dependencies are built.
