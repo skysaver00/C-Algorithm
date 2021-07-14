e0798af043cf53eb8e6081ce8ec9653cb66eed3d
skysaver00 <skysaver00@naver.com>
2021-07-14 22:57:27 +0900

    solved Silver III

--- 15656_N_and_M/main.cpp
+++ 15656_N_and_M/main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int arr[10];
 int res[10];
 
 void Multiset(int cnt, int n, int m) {
+    if(cnt == m) {
+        for(int i = 0; i < m; i++)
+            cout << res[i] << " ";
+        cout << "\n";
+        return;
+    }
 
+    for(int i = 0; i < n; i++) {
+        res[cnt] = arr[i];
+        Multiset(cnt + 1, n, m);
+    }
 }
 
 int main() {
     int n, m; cin >> n >> m;
     for(int i = 0; i < n; i++)
         cin >> arr[i];
+    sort(arr, arr + n);
 
     Multiset(0, n, m);
     return 0;
