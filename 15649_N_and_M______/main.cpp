52398fcb9f064dbe73f600d597fde54321e08d39
skysaver00 <skysaver00@naver.com>
2021-07-14 22:42:43 +0900

    not solved

--- 15649_N_and_M______/main.cpp
+++ 15649_N_and_M______/main.cpp
@@ -2,17 +2,24 @@
 using namespace std;
 
 int num[10];
+bool t_f[10];
 int perArr[10];
 
-void print() {
-
+void print(int max) {
+    for(int i = 0; i < max; i++) {
+        cout << perArr[i] << " ";
+    }cout << "\n";
 }
 
-void Permutation(int now, int size, int at) {
-    if(now == size) print();
+void Permutation(int max, int size, int at) {
+    if(max == at) print(max);
 
-    for(int i = now; i < size; i++) {
-        
+    for(int i = 0; i < size; i++) {
+        if(t_f[i]) continue;
+        perArr[at] = num[i];
+        t_f[i] = true;
+        Permutation(max, size, at);
+        t_f[i] = false;
     }
 }
 
