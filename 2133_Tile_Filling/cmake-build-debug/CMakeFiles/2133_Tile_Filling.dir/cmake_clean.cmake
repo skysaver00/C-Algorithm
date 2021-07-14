fe3fbe789d9b7bd8625d59162b57a774625d42fc
skysaver00 <skysaver00@naver.com>
2021-07-15 01:21:59 +0900

    not solved

---
+++ 2133_Tile_Filling/cmake-build-debug/CMakeFiles/2133_Tile_Filling.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2133_Tile_Filling.exe"
+  "2133_Tile_Filling.exe.manifest"
+  "2133_Tile_Filling.pdb"
+  "CMakeFiles/2133_Tile_Filling.dir/main.cpp.obj"
+  "lib2133_Tile_Filling.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2133_Tile_Filling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 2133_Tile_Filling/main.cpp
+++ 2133_Tile_Filling/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+using namespace std;
+
+int arr[31];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n; cin >> n;
+    if(n % 2 == 1) {
+        cout << 0 << "\n";
+        return 0;
+    }
+    arr[0] = 1;
+    arr[2] = 3;
+    for(int i = 4; i <= n; i += 2) {
+        arr[i] = (arr[i - 2] * arr[2]);
+        for(int j = 0; j <= i - 2; j += 2) {
+            arr[i] += arr[j] * 2;
+        }
+
+        cout << arr[i] << "\n";
+    }
+
     return 0;
 }
