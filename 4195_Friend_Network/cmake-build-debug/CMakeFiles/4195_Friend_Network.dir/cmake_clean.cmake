9d309d879c8a53b3a4008f14acf50bd2ece8399c
skysaver00 <skysaver00@naver.com>
2021-07-15 16:07:44 +0900

    not solved

---
+++ 4195_Friend_Network/cmake-build-debug/CMakeFiles/4195_Friend_Network.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4195_Friend_Network.exe"
+  "4195_Friend_Network.exe.manifest"
+  "4195_Friend_Network.pdb"
+  "CMakeFiles/4195_Friend_Network.dir/main.cpp.obj"
+  "lib4195_Friend_Network.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4195_Friend_Network.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 4195_Friend_Network/cmake-build-debug/CMakeFiles/4195_Friend_Network.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 4195_Friend_Network.
+# This may be replaced when dependencies are built.
