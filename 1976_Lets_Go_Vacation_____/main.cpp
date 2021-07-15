3a8fdf9a02ff2b5d1ea5e395abe54ee00312a351
skysaver00 <skysaver00@naver.com>
2021-07-15 15:33:52 +0900

    solved Gold IV

--- 1976_Lets_Go_Vacation_____/main.cpp
+++ 1976_Lets_Go_Vacation_____/main.cpp
@@ -6,27 +6,26 @@ bool visit[201];
 int travel[1001];
 queue<int> que;
 
-bool bfs(int i, int j, int node) {
+void bfs(int i, int node) {
     que.push(i);
 
     while(!que.empty()) {
         int now = que.front();
         visit[now] = true;
-
         que.pop();
+
         for(int a = 1; a <= node; a++) {
-            if(map[now][a] == 1 && a == j) {
-                return true;
-            }
             if(map[now][a] == 1 && !visit[a]) {
                 que.push(a);
             }
         }
     }
-    return false;
 }
 
 int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n, m; cin >> n >> m;
 
     for(int i = 1; i <= n; i++) {
@@ -36,9 +35,10 @@ int main() {
     }
 
     for(int i = 0; i < m; i++) cin >> travel[i];
-    for(int i = 0; i < m - 1; i++) {
-        bool check = bfs(travel[i], travel[i + 1], n);
-        if(!check) {
+    bfs(travel[0], n);
+    for(int i = 0; i < m; i++) {
+        if(visit[travel[i]]) continue;
+        else {
             cout << "NO\n";
             return 0;
         }
