5f609021ea79fee4b25df2eb61c74e7e775fb44c
skysaver00 <skysaver00@naver.com>
2021-07-16 12:20:34 +0900

    not solved

--- 9527_Count_Cnt_1/main.cpp
+++ 9527_Count_Cnt_1/main.cpp
@@ -5,19 +5,17 @@ int binA[66];
 int binB[66];
 long long val[66];
 
-int makeBin(long long i, int ck) {
-    int cnt = 0;
-    while(i != 0 && ck == 1) {
-        if(i % 2 == 1) binA[cnt] = 1;
-        cnt++;
-        i /= 2;
+long long makeBin(long long k) {
+    long long ans = k & 1;
+    long long bin = 1;
+    for(int i = 63; i > 0; i--) {
+        if(k & (bin << i)) {
+            ans += val[i - 1] + (k - (bin << i) + 1);
+            k -= (bin << i);
+        }
     }
-    while(i != 0 && ck == 2) {
-        if(i % 2 == 1) binB[cnt] = 1;
-        cnt++;
-        i /= 2;
-    }
-    return cnt;
+
+    return ans;
 }
 
 void makeVal() {
@@ -32,28 +30,14 @@ void makeVal() {
 int main() {
     makeVal();
     long long A, B; cin >> A >> B;
-    int lenA = makeBin(A, 1);
-    int lenB = makeBin(B, 2);
-
-    cout << lenA << " " << lenB << "\n";
-    for(int i = 0; i < lenA; i++)
-        cout << binA[i] << " ";
-    cout<<"\n";
-
-    for(int i = 0; i < lenB; i++)
-        cout << binB[i] << " ";
-    cout<<"\n";
 
     for(int i = 0; i < 64; i++) {
         cout << val[i] << "\n";
     }
 
-    long long solve1 = ;
-    long long solve2;
+    long long solve1 = makeBin(A - 1);
+    long long solve2 = makeBin(B);
 
-    for(int i = 63; i > 0; i--) {
-
-    }
-
+    cout << solve2 - solve1 << "\n";
     return 0;
 }
