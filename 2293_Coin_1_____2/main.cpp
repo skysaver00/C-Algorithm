ac2fbcb0a74c0b806bb4dae84f583318516ee114
skysaver00 <skysaver00@naver.com>
2021-07-16 12:49:58 +0900

    다시 한번 풀어볼 것

--- 2293_Coin_1_____2/main.cpp
+++ 2293_Coin_1_____2/main.cpp
@@ -8,22 +8,14 @@ int min(int i, int j) {
 
 int main() {
     int n, k; scanf("%d %d", &n, &k);
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-        dp[arr[i]] = 1;
-    }
+
 
     for(int i = 0; i < n; i++) {
-        for(int j = 1; j < k; j++) {
-            if(j == arr[i]) dp[j] += 1;
-            else if(j - arr[i] < 0) continue;
-            else dp[j] += dp[j - arr[i]];
+        for(int j = 1; j <= k; j++) {
         }
     }
-
-    for(int i = 0; i <= k; i++) {
-        printf("%d ", dp[i]);
-    }
-
+    printf("%d\n", dp[k]);
     return 0;
 }
