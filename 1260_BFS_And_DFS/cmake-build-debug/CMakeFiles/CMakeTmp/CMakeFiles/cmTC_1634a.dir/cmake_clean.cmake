e4db5fc8719291af02edcfef3adcc424d5924bbc
skysaver00 <skysaver00@naver.com>
2021-07-18 02:17:16 +0900

    not solved

---
+++ 1260_BFS_And_DFS/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_1634a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_1634a.dir/CMakeCXXCompilerABI.cpp.obj"
+  "cmTC_1634a.exe"
+  "cmTC_1634a.exe.manifest"
+  "cmTC_1634a.pdb"
+  "libcmTC_1634a.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_1634a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
