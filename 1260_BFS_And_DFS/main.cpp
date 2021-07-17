a118b0b15095ccea0a5caa0cae6a8a2c652454e6
skysaver00 <skysaver00@naver.com>
2021-07-18 02:50:13 +0900

    not solved

--- 1260_BFS_And_DFS/main.cpp
+++ 1260_BFS_And_DFS/main.cpp
@@ -5,13 +5,31 @@
 using namespace std;
 
 vector<int> graph[1001];
-bool check[1001][1001];
+queue<int> que;
+bool check[1001];
 
 void startDFS(int v) {
+
 }
 
-void startBFS(int v) {
+void startBFS(int n, int v) {
+    que.push(v);
+    check[v] = 1;
 
+    while(!que.empty()) {
+        int now = que.front();
+        int len = graph[now].size();
+        cout << now << " ";
+        que.pop();
+
+        for(int i = 0; i < len; i++) {
+            if(check[graph[now][i]]) continue;
+            else {
+                que.push(graph[now][i]);
+                check[graph[now][i]] = true;
+            }
+        }
+    }
 }
 
 int main() {
@@ -24,11 +42,17 @@ int main() {
         graph[to].push_back(from);
     }
 
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i <= n; i++)
         sort(graph[i].begin(), graph[i].end());
 
+    /*for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < graph[i].size(); j++) {
+            cout << graph[i][j] << " ";
+        }cout << "\n";
+    }*/
+
     startDFS(v);
-    startBFS(v);
+    startBFS(n, v);
 
     return 0;
 }
