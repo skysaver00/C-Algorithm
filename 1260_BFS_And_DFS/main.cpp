5a50f31cd4ebd8c8775b777c01b4a0806a2907c2
skysaver00 <skysaver00@naver.com>
2021-07-18 02:30:00 +0900

    not solved

--- 1260_BFS_And_DFS/main.cpp
+++ 1260_BFS_And_DFS/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> graph[1001];
+bool check[1001][1001];
+
+void startDFS(int v) {
+}
+
+void startBFS(int v) {
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m, v; cin >> n >> m >> v;
+
+    for(int i = 0; i < m; i++) {
+        int from, to;
+        cin >> from >> to;
+        graph[from].push_back(to);
+        graph[to].push_back(from);
+    }
+
+    for(int i = 0; i < n; i++)
+        sort(graph[i].begin(), graph[i].end());
+
+    startDFS(v);
+    startBFS(v);
+
     return 0;
 }
