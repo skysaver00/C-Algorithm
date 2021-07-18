97d39abb0aa6cb987e1d656f3007c3c032f497f1
skysaver00 <skysaver00@naver.com>
2021-07-18 16:48:20 +0900

    not solved

--- 3055_Escape/main.cpp
+++ 3055_Escape/main.cpp
@@ -11,6 +11,7 @@ int water[51][51];
 queue<pair<int, int>> que;
 queue<pair<int, int>> flood;
 int startX, startY;
+int pointX, pointY;
 
 void startWater(int n, int m) {
     for(int i = 0; i < n; i++) {
@@ -23,9 +24,8 @@ void startWater(int n, int m) {
     }
 
     while(!flood.empty()) {
-        int _x, _y;
-        _x = flood.front().first;
-        _y = flood.front().second;
+        int _x = flood.front().first;
+        int _y = flood.front().second;
         flood.pop();
 
         for(int i = 0; i < 4; i++) {
@@ -45,8 +45,30 @@ void startWater(int n, int m) {
 
 }
 
-void startBFS(int i, int j) {
+void startBFS(int i, int j, int n, int m) {
+    que.push({i, j});
+    able[i][j] = 0;
 
+    while(!que.empty()) {
+        int _x = que.front().first;
+        int _y = que.front().second;
+        que.pop();
+
+        if(able[_x][_y] < water[_x][_y]) {
+            for(int k = 0; k < 4; k++) {
+                int __x = _x + x[i];
+                int __y = _y + y[i];
+
+                if(__x < 0 || __x >= n || __y < 0 || __y >= m) continue;
+                if(map[__x][__y] == '*' || map[__x][__y] == 'X') continue;
+
+                if(able[__x][__y] > able[_x][_y] + 1) {
+                    able[__x][__y] = able[_x][_y] + 1;
+                    que.push({__x, __y});
+                }
+            }
+        }
+    }
 }
 
 int main() {
@@ -58,8 +80,13 @@ int main() {
                 startX = i;
                 startY = j;
             }
+            else if(map[i][j] == 'D') {
+                pointX = i;
+                pointY = j;
+            }
 
             water[i][j] = 9999;
+            able[i][j] = 9999;
         }
     }
     startWater(n, m);
@@ -74,7 +101,13 @@ int main() {
             cout << water[i][j] << "\t";
         }cout << "\n";
     }
-    startBFS(startX, startY);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << able[i][j] << "\t";
+        }cout << "\n";
+    }
+    startBFS(startX, startY, n, m);
 
     return 0;
 }
