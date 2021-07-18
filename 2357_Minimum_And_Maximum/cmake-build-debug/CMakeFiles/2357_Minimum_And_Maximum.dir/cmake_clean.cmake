c9eaf4580984e706efa51bd89777a039980d1afa
skysaver00 <skysaver00@naver.com>
2021-07-18 15:56:05 +0900

    not solved

---
+++ 2357_Minimum_And_Maximum/cmake-build-debug/CMakeFiles/2357_Minimum_And_Maximum.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2357_Minimum_And_Maximum.exe"
+  "2357_Minimum_And_Maximum.exe.manifest"
+  "2357_Minimum_And_Maximum.pdb"
+  "CMakeFiles/2357_Minimum_And_Maximum.dir/main.cpp.obj"
+  "lib2357_Minimum_And_Maximum.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2357_Minimum_And_Maximum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 2357_Minimum_And_Maximum/main.cpp
+++ 2357_Minimum_And_Maximum/main.cpp
@@ -1,6 +1,42 @@
-#include <iostream>
+#include <stdio.h>
+
+int arr[100001];
+int minTree[400001];
+int maxTree[400001];
+
+int min(int i, int j) {
+    return i < j ? i : j;
+}
+
+int max(int i, int j) {
+    return i > j ? i : j;
+}
+
+int init(int start, int end, int node, int flag) {
+    if(flag == 1) {
+        if(start == end) return minTree[node] = arr[start];
+        int mid = (start + end) / 2;
+        return minTree[node] = min(init(start, mid, node * 2, 1), init(mid + 1, end, node * 2 + 1, 1));
+    } else {
+        if(start == end) return maxTree[node] = arr[start];
+        int mid = (start + end) / 2;
+        return maxTree[node] = max(init(start, mid, node * 2, 2), init(mid + 1, end, node * 2 + 1, 2));
+    }
+}
+
+int ans(int start, int end, int node, int left, int right) {
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m; scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    init(0, n - 1, 1, 1);
+    init(0, n - 1, 1, 2);
+
+    
+
     return 0;
 }
