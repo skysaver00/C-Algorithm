a41e7228b6ba362a2925f06fc546345b7d6a9543
skysaver00 <skysaver00@naver.com>
2021-07-18 16:27:47 +0900

    not solved

--- 3055_Escape/main.cpp
+++ 3055_Escape/main.cpp
@@ -2,13 +2,52 @@
 #include <queue>
 using namespace std;
 
+int x[4] = {1, 0, -1, 0};
+int y[4] = {0, 1, 0, -1};
+
 char map[51][51];
 int able[51][51];
+int water[51][51];
 queue<pair<int, int>> que;
+queue<pair<int, int>> flood;
 int startX, startY;
 
+void startWater(int n, int m) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(map[i][j] == '*') {
+                flood.push({i, j});
+                water[i][j] = 0;
+            }
+        }
+    }
+
+    while(!flood.empty()) {
+        int _x, _y;
+        _x = flood.front().first;
+        _y = flood.front().second;
+        flood.pop();
+
+        for(int i = 0; i < 4; i++) {
+            int __x = _x + x[i];
+            int __y = _y + y[i];
+
+            if(__x < 0 || __x >= n || __y < 0 || __y >= m) continue;
+            if(water[__x][__y] == 'S' || water[__x][__y] == 'X' || water[__x][__y] == 'D') continue;
+
+
+            if(water[__x][__y] > water[_x][_y] + 1) {
+                water[__x][__y] = water[_x][_y] + 1;
+                flood.push({__x, __y});
+            }
+        }
+    }
+
+
+}
+
 void startBFS(int i, int j) {
-    
+
 }
 
 int main() {
@@ -20,9 +59,17 @@ int main() {
                 startX = i;
                 startY = j;
             }
+
+            water[i][j] = 9999;
         }
     }
+    startWater(n, m);
 
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << water[i][j] << " ";
+        }cout << "\n";
+    }
     startBFS(startX, startY);
 
     return 0;
