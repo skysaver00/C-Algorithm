87131cbd53e43fe1c57bd34e6d98577d33659de0
skysaver00 <skysaver00@naver.com>
2021-07-18 16:01:07 +0900

    solved Gold I

--- 2357_Minimum_And_Maximum/main.cpp
+++ 2357_Minimum_And_Maximum/main.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int arr[100001];
 int minTree[400001];
 int maxTree[400001];
@@ -24,9 +23,22 @@ int init(int start, int end, int node, int flag) {
     }
 }
 
-int ans(int start, int end, int node, int left, int right) {
+int minAns(int start, int end, int node, int left, int right) {
+    if(end < left || right < start) return 1000000001;
+    if(left <= start && end <= right) return minTree[node];
 
+    int mid = (start + end) / 2;
+    return min(minAns(start, mid, node * 2, left, right), minAns(mid + 1, end, node * 2 + 1, left, right));
 }
+
+int maxAns(int start, int end, int node, int left, int right) {
+    if(end < left || right < start) return 0;
+    if(left <= start && end <= right) return maxTree[node];
+
+    int mid = (start + end) / 2;
+    return max(maxAns(start, mid, node * 2, left, right), maxAns(mid + 1, end, node * 2 + 1, left, right));
+}
+
 
 int main() {
     int n, m; scanf("%d %d", &n, &m);
@@ -36,7 +48,12 @@ int main() {
     init(0, n - 1, 1, 1);
     init(0, n - 1, 1, 2);
 
-    
-
+    int left, right;
+    for(int i = 0; i < m; i++) {
+        scanf("%d %d", &left, &right);
+        int val = minAns(0, n - 1, 1, left - 1, right - 1);
+        int val2 = maxAns(0, n - 1, 1, left - 1, right - 1);
+        printf("%d %d\n", val, val2);
+    }
     return 0;
 }
