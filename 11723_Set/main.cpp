087c78945840fbb71717588c018795ae9281844c
skysaver00 <skysaver00@naver.com>
2021-07-20 02:37:41 +0900

    not solved

--- 11723_Set/main.cpp
+++ 11723_Set/main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <cstdio>
+#include <cstring>
+bool num[21];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int t;
+    scanf("%d", &t);
+
+    while(t--) {
+        int val;
+        char str[8];
+        scanf("%s", str);
+
+        if(strcmp(str, "add")) {
+            scanf("%d", &val);
+            num[val] = true;
+        } else if(strcmp(str, "check")) {
+            scanf("%d", &val);
+            if(num[val]) printf("1\n");
+            else printf("0\n");
+        } else if(strcmp(str, "remove")) {
+            scanf("%d", &val);
+            
+        } else if(strcmp(str, "toggle")) {
+
+        } else if(strcmp(str, "all")) {
+
+        } else {
+
+        }
+    }
 }
