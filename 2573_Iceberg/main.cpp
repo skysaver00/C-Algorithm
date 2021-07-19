252e0b07c5e2d4d7e3eb1406427b63de17915987
skysaver00 <skysaver00@naver.com>
2021-07-19 20:25:23 +0900

    not solved

--- 2573_Iceberg/main.cpp
+++ 2573_Iceberg/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
+
+int ice[301][301];
+int count[301][301];
+
+void checkDFS(int i, int j, int num) {
+
+}
+
+void makeBFS(int i, int j) {
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m; cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> ice[i][j];
+        }
+    }
+
+    while(1) {
+
+    }
+
     return 0;
 }
