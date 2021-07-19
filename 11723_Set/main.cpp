f37cf2bb49a169295bc18dc95ac3485e3db7d1cc
skysaver00 <skysaver00@naver.com>
2021-07-20 02:42:42 +0900

    solved Silver V

--- 11723_Set/main.cpp
+++ 11723_Set/main.cpp
@@ -11,22 +11,24 @@ int main() {
         char str[8];
         scanf("%s", str);
 
-        if(strcmp(str, "add")) {
+        if(str[1] == 'd') {
             scanf("%d", &val);
             num[val] = true;
-        } else if(strcmp(str, "check")) {
+        } else if(str[0] == 'c') {
             scanf("%d", &val);
             if(num[val]) printf("1\n");
             else printf("0\n");
-        } else if(strcmp(str, "remove")) {
+        } else if(str[0] == 'r') {
             scanf("%d", &val);
-            
-        } else if(strcmp(str, "toggle")) {
-
-        } else if(strcmp(str, "all")) {
-
+            num[val] = false;
+        } else if(str[0] == 't') {
+            scanf("%d", &val);
+            num[val] = !num[val];
+        } else if(str[1] == 'l') {
+            for(int i = 1; i <= 20; i++) num[i] = true;
         } else {
-
+            for(int i = 1; i <= 20; i++) num[i] = false;
         }
     }
+    return 0;
 }
