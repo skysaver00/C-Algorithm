f092e595549d0a4df5b5c13bf713a8c1b7ac4fc1
skysaver00 <skysaver00@naver.com>
2021-07-20 01:15:34 +0900

    not solved

---
+++ 11723_Set/cmake-build-debug/CMakeFiles/11723_Set.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11723_Set.exe"
+  "11723_Set.exe.manifest"
+  "11723_Set.pdb"
+  "CMakeFiles/11723_Set.dir/main.cpp.obj"
+  "lib11723_Set.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11723_Set.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11723_Set/cmake-build-debug/CMakeFiles/11723_Set.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 11723_Set.
+# This may be replaced when dependencies are built.
