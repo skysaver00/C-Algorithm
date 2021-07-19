c5639ddc022bb9b0c4d8e04c8ed72c6e4550096c
skysaver00 <skysaver00@naver.com>
2021-07-19 20:22:28 +0900

    not solved

---
+++ 2573_Iceberg/cmake-build-debug/CMakeFiles/2573_Iceberg.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2573_Iceberg.exe"
+  "2573_Iceberg.exe.manifest"
+  "2573_Iceberg.pdb"
+  "CMakeFiles/2573_Iceberg.dir/main.cpp.obj"
+  "lib2573_Iceberg.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2573_Iceberg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2573_Iceberg/cmake-build-debug/CMakeFiles/2573_Iceberg.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 2573_Iceberg.
+# This may be replaced when dependencies are built.
