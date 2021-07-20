cd49869e8aea4c201ff896e45698c0e9af8fe8f0
skysaver00 <skysaver00@naver.com>
2021-07-20 18:09:07 +0900

    not solved

--- 1021_Moving_Queue/main.cpp
+++ 1021_Moving_Queue/main.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
+#include <deque>
 using namespace std;
-int que[51];
+deque<int> deq;
+deque<int> deq2;
+int arr[51];
 
 int main() {
     int n, m; cin >> n >> m;
-    for(int i = 0; i < m; i++) cin >> que[i];
-    int start = 1;
     for(int i = 0; i < m; i++) {
-        
+        cin >> arr[i];
+        deq.push_back(i);
+    }
+
+    deq2 = deq;
+
+    int start = 1, now = n;
+    int ans = 0;
+    for(int i = 0; i < m; i++) {
+        int find = arr[i];
+
+        int count = 0;
+        while(1) {
+            if(deq.front() == find) {
+                ans += count;
+                break;
+            } else if(deq2.front() == find) {
+                ans += count;
+                break;
+            }
+
+            int val = deq.front();
+            deq.pop_front();
+            deq.push_back(val);
+
+            val = deq2.back();
+            deq2.pop_back();
+            deq2.push_front(val);
+
+            count++;
+        }
     }
 
     return 0;
