e5b7174031fcce5e4ce871fc9737551ecca8456b
skysaver00 <skysaver00@naver.com>
2021-07-20 17:56:05 +0900

    not solved

--- 1021_Moving_Queue/main.cpp
+++ 1021_Moving_Queue/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+using namespace std;
+int que[51];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m; cin >> n >> m;
+    for(int i = 0; i < m; i++) cin >> que[i];
+    int start = 1;
+    for(int i = 0; i < m; i++) {
+        
+    }
+
     return 0;
 }
