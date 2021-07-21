a7a88f04e0382c77c000135f9d73f705ef63eb76
skysaver00 <skysaver00@naver.com>
2021-07-21 18:51:45 +0900

    solved Bronze V

---
+++ 10926_Joonas/cmake-build-debug/CMakeFiles/10926_Joonas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "10926_Joonas.exe"
+  "10926_Joonas.exe.manifest"
+  "10926_Joonas.pdb"
+  "CMakeFiles/10926_Joonas.dir/main.cpp.obj"
+  "lib10926_Joonas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10926_Joonas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 10926_Joonas/cmake-build-debug/CMakeFiles/10926_Joonas.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 10926_Joonas.
+# This may be replaced when dependencies are built.
