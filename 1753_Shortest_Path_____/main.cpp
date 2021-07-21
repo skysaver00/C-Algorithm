7b3d1dd789b61553ee2ea7e6cbf1a45b8f7ae271
skysaver00 <skysaver00@naver.com>
2021-07-21 14:39:27 +0900

    solved Gold V

--- 1753_Shortest_Path_____/main.cpp
+++ 1753_Shortest_Path_____/main.cpp
@@ -9,7 +9,7 @@ int INF = 2147483647;
 
 void path(int k) {
     val[k] = 0;
-    priority_queue<pair<int, int>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     pq.push({0, k});
 
     while(!pq.empty()) {
