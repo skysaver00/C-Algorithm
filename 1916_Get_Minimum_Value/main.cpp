3d5bceb851f92c8ea70a1fec09db3007f40a5777
skysaver00 <skysaver00@naver.com>
2021-07-21 15:32:09 +0900

    not solved

--- 1916_Get_Minimum_Value/main.cpp
+++ 1916_Get_Minimum_Value/main.cpp
@@ -5,8 +5,30 @@ using namespace std;
 int INF = 2147483647;
 
 vector<pair<int, int>> vec[1001];
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> pq;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 int val[1001];
+
+void path(int k) {
+    val[k] = 0;
+    pq.push({0, k});
+
+    while(!pq.empty()) {
+        int curr = pq.top().second;
+        int dist = pq.top().first;
+        pq.pop();
+
+        int sz = vec[curr].size();
+        for(int i = 0; i < sz; i++) {
+            int next = vec[curr][i].first;
+            int nextDist = dist + vec[curr][i].second;
+
+            if(nextDist < val[next]) {
+                val[next] = nextDist;
+                pq.push({nextDist, next});
+            }
+        }
+    }
+}
 
 int main() {
     int n; cin >> n;
@@ -19,5 +41,9 @@ int main() {
         vec[from].push_back({to, len});
     }
 
+    int from, to; cin >> from >> to;
+    path(from);
+
+    cout << val[to] << "\n";
     return 0;
 }
