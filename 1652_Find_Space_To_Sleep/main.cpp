18114635f26b6871a4531c8e005c9ad16d69387a
skysaver00 <skysaver00@naver.com>
2021-07-22 22:41:01 +0900

    not solved

--- 1652_Find_Space_To_Sleep/main.cpp
+++ 1652_Find_Space_To_Sleep/main.cpp
@@ -6,7 +6,30 @@ int main() {
     scanf("%d", &n);
     for(int i = 0; i < n ; i++) {
         for(int j = 0; j < n; j++) {
-            
+            char s; scanf("%c", &s);
+            if(s == '.') arr[i][j] = 0;
+            else arr[i][j] = 1;
+        }
+    }
+
+    int hor, ver;
+    hor = ver = 0;
+    for(int i = 0; i < n; i++) {
+        int check = 0;
+        for(int j = 0; j < n; j++) {
+            if(arr[i][j] == 0) check++;
+            else check = 0;
+
+            if(check >= 2) {
+                hor++;
+                break;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+
         }
     }
     return 0;
