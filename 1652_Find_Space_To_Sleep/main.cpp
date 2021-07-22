234baace882d23d61cbedba1f467361a4cf1ed17
skysaver00 <skysaver00@naver.com>
2021-07-22 22:34:06 +0900

    not solved

--- 1652_Find_Space_To_Sleep/main.cpp
+++ 1652_Find_Space_To_Sleep/main.cpp
@@ -1,54 +1,13 @@
 #include <stdio.h>
-
 bool arr[101][101];
+int n;
 
 int main() {
-    int N;
-    scanf("%d", &N);
-
-    int x_[4] = {1, 0};
-    int y_[4] = {0, 1};
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            char X;
-            scanf(" %c", &X);
-
-            if (X == '.') arr[i][j] = true;
+    scanf("%d", &n);
+    for(int i = 0; i < n ; i++) {
+        for(int j = 0; j < n; j++) {
+            
         }
     }
-    int xmin = -9999;
-    int ymin = -9999;
-
-    int cmp1, cmp2, ans = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cmp1 = j, cmp2 = i;
-            for (int l = j; l <= N; l++) {
-                if (arr[cmp2][cmp1] == 0) {
-                    if (xmin < ans) xmin = ans;
-                    break;
-                }
-
-                ans++;
-                cmp1 += 1, cmp2 += 0;
-            }
-            ans = 0;
-            cmp1 = j, cmp2 = i;
-            for (int l = i; l <= N; l++) {
-                if (arr[cmp2][cmp1] == 0) {;
-                    if (ymin < ans) ymin = ans;
-                    break;
-                }
-
-                ans++;
-                cmp1 += 0, cmp2 += 1;
-            }
-            ans = 0;
-        }
-    }
-
-    printf("%d %d\n", xmin, ymin);
-
     return 0;
 }
