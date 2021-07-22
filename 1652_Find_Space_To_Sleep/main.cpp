f8dd882bda811da9503cbca8e077e8108a785967
skysaver00 <skysaver00@naver.com>
2021-07-22 22:47:22 +0900

    상당히 지엽적인 문제라 별로다 not solved

--- 1652_Find_Space_To_Sleep/main.cpp
+++ 1652_Find_Space_To_Sleep/main.cpp
@@ -6,7 +6,7 @@ int main() {
     scanf("%d", &n);
     for(int i = 0; i < n ; i++) {
         for(int j = 0; j < n; j++) {
-            char s; scanf("%c", &s);
+            char s; scanf(" %c", &s);
             if(s == '.') arr[i][j] = 0;
             else arr[i][j] = 1;
         }
@@ -19,18 +19,18 @@ int main() {
         for(int j = 0; j < n; j++) {
             if(arr[i][j] == 0) check++;
             else check = 0;
-
-            if(check >= 2) {
-                hor++;
-                break;
-            }
+            if(check >= 2) hor++;
         }
     }
 
     for(int i = 0; i < n; i++) {
+        int check = 0;
         for(int j = 0; j < n; j++) {
-
+            if(arr[j][i] == 0) check++;
+            else check = 0;
+            if(check >= 2) ver++;
         }
     }
+    printf("%d %d\n", hor, ver);
     return 0;
 }
