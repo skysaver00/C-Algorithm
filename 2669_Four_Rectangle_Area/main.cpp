9e1154f4d3a2dda1fa496d409c5318f09ad7356c
skysaver00 <skysaver00@naver.com>
2021-07-22 22:52:50 +0900

    solved Bronze I

--- 2669_Four_Rectangle_Area/main.cpp
+++ 2669_Four_Rectangle_Area/main.cpp
@@ -6,7 +6,7 @@ int main() {
     int x1, y1, x2, y2;
     for(int i = 0; i < 4; i++) {
         cin >> x1 >> y1 >> x2 >> y2;
-        for(int a = x1; a < x2; i++) {
+        for(int a = x1; a < x2; a++) {
             for(int b = y1; b < y2; b++) {
                 arr[a][b] = true;
             }
