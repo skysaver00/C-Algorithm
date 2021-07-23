95c9838ea34b8923a1873b2816229fcb0f6e92f1
skysaver00 <skysaver00@naver.com>
2021-07-23 14:54:12 +0900

    not solved

--- 11779_Get_Minimum_Value_2/main.cpp
+++ 11779_Get_Minimum_Value_2/main.cpp
@@ -3,3 +3,35 @@
 #include <queue>
 using namespace std;
 int INF = 2147483647;
+
+vector<pair<int, int>> vec[1001];
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+int val[1001];
+
+void path(int k) {
+    val[k] = 0;
+    pq.push({0, k});
+
+    while(!pq.empty()) {
+        int curr = pq.top().second;
+        int dist = pq.top().first;
+        pq.pop();
+
+        if(val[curr] < dist) continue;
+
+        int sz = vec[curr].size();
+        for(int i = 0; i < sz; i++) {
+            int next = vec[curr][i].first;
+            int nextDist = dist + vec[curr][i].second;
+
+            if(nextDist < val[next]) {
+                val[next] = nextDist;
+                pq.push({nextDist, next});
+            }
+        }
+    }
+}
+
+int main() {
+
+}
