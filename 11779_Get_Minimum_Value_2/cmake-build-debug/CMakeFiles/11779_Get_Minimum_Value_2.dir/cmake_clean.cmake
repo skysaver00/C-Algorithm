4271a75ac1038b7f7ad1f409dda9442d98b66d02
skysaver00 <skysaver00@naver.com>
2021-07-23 14:52:55 +0900

    not solved

---
+++ 11779_Get_Minimum_Value_2/cmake-build-debug/CMakeFiles/11779_Get_Minimum_Value_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11779_Get_Minimum_Value_2.exe"
+  "11779_Get_Minimum_Value_2.exe.manifest"
+  "11779_Get_Minimum_Value_2.pdb"
+  "CMakeFiles/11779_Get_Minimum_Value_2.dir/main.cpp.obj"
+  "lib11779_Get_Minimum_Value_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11779_Get_Minimum_Value_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11779_Get_Minimum_Value_2/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int INF = 2147483647;
