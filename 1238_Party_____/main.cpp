c53916cdf7fe9f6506051535666fc85ab076ef54
skysaver00 <skysaver00@naver.com>
2021-07-23 14:50:23 +0900

    not solved

--- 1238_Party_____/main.cpp
+++ 1238_Party_____/main.cpp
@@ -3,8 +3,12 @@
 #include <vector>
 using namespace std;
 
-vector<pair<int, int>> vec;
+vector<pair<int, int>> vec[1001];
 
 int main() {
+    int N, M, X; cin >> N >> M >> X;
 
+
+
+    return 0;
 }
