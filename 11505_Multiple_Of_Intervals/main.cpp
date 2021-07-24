1dc5e333f60ead052bc5fc2f004d896c4db5b521
skysaver00 <skysaver00@naver.com>
2021-07-24 16:06:30 +0900

    not solved

--- 11505_Multiple_Of_Intervals/main.cpp
+++ 11505_Multiple_Of_Intervals/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+using namespace std;
+
+long long numbers[1000001];
+long long tree[4000001];
+
+long long initialize(int start, int end, int node) {
+    if(start == end) return tree[node] = numbers[start];
+    int mid = (start + end) / 2;
+    return tree[node] = (initialize(start, end, node * 2) * initialize(mid + 1, end, node * 2 + 1)) % 1000000007;
+}
+
+void update(int start, int end, int node, int index, long long change) {
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m, k; cin >> n >> m >> k;
+    for(int i = 0; i < n; i++) cin >> numbers[i];
+
+    initialize(0, n - 1, 1);
+
+
     return 0;
 }
