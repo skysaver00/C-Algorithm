61b202cda5d6984043fcceaa33d8250c951bf04e
skysaver00 <skysaver00@naver.com>
2021-07-24 15:52:27 +0900

    not solved

---
+++ 11505_Multiple_Of_Intervals/cmake-build-debug/CMakeFiles/11505_Multiple_Of_Intervals.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11505_Multiple_Of_Intervals.exe"
+  "11505_Multiple_Of_Intervals.exe.manifest"
+  "11505_Multiple_Of_Intervals.pdb"
+  "CMakeFiles/11505_Multiple_Of_Intervals.dir/main.cpp.obj"
+  "lib11505_Multiple_Of_Intervals.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11505_Multiple_Of_Intervals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11505_Multiple_Of_Intervals/cmake-build-debug/CMakeFiles/11505_Multiple_Of_Intervals.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 11505_Multiple_Of_Intervals.
+# This may be replaced when dependencies are built.
