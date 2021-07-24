677d8c2d5251f6fdaed0e72054b9004cda196c85
skysaver00 <skysaver00@naver.com>
2021-07-24 22:04:19 +0900

    not solved

---
+++ 15663_N_and_M/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int num[10];
+int arr[8];
+
+void dfs(int now, int size, int )
+
+int main() {
+    int m, n; cin >> m >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> num[i];
+    sort(num, num + n);
+
+    dfs()
+    return 0;
+}
