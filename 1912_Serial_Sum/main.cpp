75d2766c37af38f5ee03721804dd9c3b264bbcc1
skysaver00 <skysaver00@naver.com>
2021-07-24 18:44:37 +0900

    not solved

--- 1912_Serial_Sum/main.cpp
+++ 1912_Serial_Sum/main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <stdio.h>
+int num[100001];
+int sum[100001];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n; scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &num[i]);
+
+    for(int i = 0; i < n - 1; i++) {
+        if(sum[i] < sum[i - 1] + num[i]) sum[i] = sum[i - 1] + num[i];
+        else sum[i] = num[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &sum[i]);
+    }
+
+
     return 0;
 }
