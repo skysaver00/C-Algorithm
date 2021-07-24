7e45a4ccee53432a5e84f1d1c2f0ecafe8be7d99
skysaver00 <skysaver00@naver.com>
2021-07-24 22:19:39 +0900

    not solved

--- 15663_N_and_M/main.cpp
+++ 15663_N_and_M/main.cpp
@@ -4,12 +4,30 @@ using namespace std;
 
 int num[10];
 int arr[10];
-int before;
+int check[10];
 
 void dfs(int idx, int cnt, int max, int size) {
+    cout << cnt << " " << max << "\n";
     if(cnt == max) {
+        bool tf = true;
+        int p = 0;
         for(int i = 0; i < size; i++) {
-            if(arr[i]) cout << num[i] << " ";
+            cout << num[i] << " " << check[p] << "\n";
+            if(arr[i]) {
+                if(num[i] != check[p++]) {
+                    tf = false;
+                }
+            }
+        }
+
+        if(tf) return;
+
+        p = 0;
+        for(int i = 0; i < size; i++) {
+            if(arr[i]) {
+                cout << num[i] << " ";
+                check[p++] = num[i];
+            }
         }cout << "\n";
         return;
     }
@@ -26,10 +44,10 @@ void dfs(int idx, int cnt, int max, int size) {
 int main() {
     int m, n; cin >> m >> n;
 
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < m; i++)
         cin >> num[i];
     sort(num, num + n);
 
-    dfs()
+    dfs(0, 0, n, m);
     return 0;
 }
