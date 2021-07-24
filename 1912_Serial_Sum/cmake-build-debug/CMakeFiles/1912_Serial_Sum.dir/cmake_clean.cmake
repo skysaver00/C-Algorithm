1b08fdff25ccb507324531aee45fe8c5189b436d
skysaver00 <skysaver00@naver.com>
2021-07-24 18:35:46 +0900

    not solved

---
+++ 1912_Serial_Sum/cmake-build-debug/CMakeFiles/1912_Serial_Sum.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1912_Serial_Sum.exe"
+  "1912_Serial_Sum.exe.manifest"
+  "1912_Serial_Sum.pdb"
+  "CMakeFiles/1912_Serial_Sum.dir/main.cpp.obj"
+  "lib1912_Serial_Sum.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1912_Serial_Sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
