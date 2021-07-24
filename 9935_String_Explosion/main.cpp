6899f7f6f5d0f3a4ccba1dabda0acee1fbcb09ad
skysaver00 <skysaver00@naver.com>
2021-07-24 17:43:31 +0900

    not solved

--- 9935_String_Explosion/main.cpp
+++ 9935_String_Explosion/main.cpp
@@ -1,36 +1,59 @@
-#include <cstdio>
-#include <cstring>
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
 
-char str[1000005];
-char bomb[40];
-char res[1000005];
+string str;
+string bomb;
+stack<char> st;
+char realStr[1000001];
+int arr[40];
 
 int main() {
-    scanf("%s %s", str, bomb);
+    cin >> str >> bomb;
 
-    int index = 0;
-    int len = strlen(str);
+    int len = str.length();
+    int blen = bomb.length();
+
+    int now = 0;
     for(int i = 0; i < len; i++) {
-        res[index] = str[i];
-        index++;
+        if(str.at(i) == bomb.at(now)) {
+            arr[now]++;
+            now++;
+            st.push(str.at(i));
+        } else if(str.at(i) == bomb.at(0)) {
+            arr[0]++;
+            now = 1;
+            st.push(str.at(i));
+        } else {
+            for(int j = 0; j < blen; j++) arr[j] = 0;
+            now = 0;
+            st.push(str.at(i));
+        }
 
-        int bLen = strlen(bomb);
-        if(res[index - 1] == bomb[bLen - 1]) {
-            if(index - bLen < 0) continue;
-            bool detect = true;
-            for(int j = 0; j < bLen; j++) {
-                if(res[index - j - 1] != bomb[bLen - j - 1]) {
-                    detect = false;
-                    break;
-                }
+        if(now == blen) {
+            for(int j = 0; j < now; j++) {
+                st.pop();
+                arr[j]--;
             }
-
-            if(detect) index = index - bLen;
+            now = 0;
+            for(int j = 0; j < blen; j++) {
+                if(arr[j] == 0) break;
+                now++;
+            }
         }
     }
 
-    if(index == 0) printf("FRULA\n");
-    else printf("%s\n", res);
-
+    if(st.empty()) {
+        cout << "FRULA" << "\n";
+        return 0;
+    }
+    int sz = st.size() - 1;
+    while(!st.empty()) {
+        realStr[sz] = st.top();
+        st.pop();
+        sz--;
+    }
+    cout << realStr << "\n";
     return 0;
 }
