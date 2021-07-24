03ca608fd9a197f2e5e0d487974e2f478316ecc8
skysaver00 <skysaver00@naver.com>
2021-07-24 22:11:04 +0900

    not solved

--- 15663_N_and_M/main.cpp
+++ 15663_N_and_M/main.cpp
@@ -3,9 +3,25 @@
 using namespace std;
 
 int num[10];
-int arr[8];
+int arr[10];
+int before;
 
-void dfs(int now, int size, int )
+void dfs(int idx, int cnt, int max, int size) {
+    if(cnt == max) {
+        for(int i = 0; i < size; i++) {
+            if(arr[i]) cout << num[i] << " ";
+        }cout << "\n";
+        return;
+    }
+
+    for(int i = idx; i < size; i++) {
+        if(arr[i]) continue;
+        arr[i] = true;
+
+        dfs(i, cnt + 1, max, size);
+        arr[i] = false;
+    }
+}
 
 int main() {
     int m, n; cin >> m >> n;
