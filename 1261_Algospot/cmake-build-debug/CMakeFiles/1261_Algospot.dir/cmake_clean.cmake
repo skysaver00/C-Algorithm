9a0ba81c5587b259ffd6c198446c546721ef20b0
skysaver00 <skysaver00@naver.com>
2021-07-25 15:43:11 +0900

    not solved

---
+++ 1261_Algospot/cmake-build-debug/CMakeFiles/1261_Algospot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1261_Algospot.exe"
+  "1261_Algospot.exe.manifest"
+  "1261_Algospot.pdb"
+  "CMakeFiles/1261_Algospot.dir/main.cpp.obj"
+  "lib1261_Algospot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1261_Algospot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1261_Algospot/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n, m;
+int wall[101][101];
+int val[101][101];
+priority_queue <pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+int INF = 2147483647;
+int x[4] = {1, 0, -1, 0};
+int y[4] = {0, 1, 0, -1};
+
+void path(int k, int l) {
+    val[k][l] = 0;
+    pq.push({0, {0, 0}});
+
+    while(!pq.empty()) {
+        int currX = pq.top().second.first;
+        int currY = pq.top().second.second;
+        int dist = pq.top().first;
+
+        pq.pop();
+        for(int i = 0; i < 4; i++) {
+            int nextX = currX + x[i];
+            int nextY = currY + y[i];
+            if(nextX >= n || nextX < 0 || nextY >= m || nextY < 0) continue;
+
+            int nextDist = dist + wall[nextX][nextY];
+
+            if(nextDist < val[nextX][nextY]) {
+                val[nextX][nextY] = nextDist;
+                pq.push({nextDist, {nextX, nextY}});
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> m >> n;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> wall[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            val[i][j] = INF;
+        }
+    }
+
+    path(0, 0);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << val[i][j] << " "
+        }cout << "\n";
+    }
+
+    return 0;
+}
