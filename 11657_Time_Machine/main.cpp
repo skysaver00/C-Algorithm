16c91f1881153cf29cb3b617906a132dd639764e
skysaver00 <skysaver00@naver.com>
2021-07-25 16:35:40 +0900

    not solved

--- 11657_Time_Machine/main.cpp
+++ 11657_Time_Machine/main.cpp
@@ -4,13 +4,45 @@ using namespace std;
 
 int n, m;
 int INF = 2147483647;
-vector<int> vec[501];
+vector<pair<int, int>> vec[501];
 int time[501];
 
+void bellman(int k) {
+    time[k] = 0;
+
+    int tf = true;
+    for(int i = 0; i < m; i++) {
+        for(int j = 1; j <= n; j++) cout << time[j] << " ";
+        cout << "\n";
+
+        for(int j = 1; j <= n; j++) {
+            int sz = vec[j].size();
+            for(int k = 0; k < sz; i++) {
+                int next = vec[j][k].first;
+                int nextDist = vec[j][k].second + time[j];
+
+                if(nextDist < time[next]) {
+                    tf = false;
+                    time[next] = nextDist;
+                }
+            }
+        }
+
+        if(!tf) break;
+    }
+}
 
 int main() {
     cin >> n >> m;
-    for(int i = 1; i <= n; i++) {
-        time[i] =
+    for(int i = 1; i <= n; i++)
+        time[i] = INF;
+
+    for(int i = 0; i < m; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        vec[a].push_back({b, c});
     }
+
+    bellman(1);
+
+    return 0;
 }
