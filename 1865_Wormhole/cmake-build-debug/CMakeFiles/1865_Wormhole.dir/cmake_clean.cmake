6dd67127290b467b7890ac5c288a0c87445f4d8e
skysaver00 <skysaver00@naver.com>
2021-07-26 17:07:27 +0900

    not solved

---
+++ 1865_Wormhole/cmake-build-debug/CMakeFiles/1865_Wormhole.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1865_Wormhole.exe"
+  "1865_Wormhole.exe.manifest"
+  "1865_Wormhole.pdb"
+  "CMakeFiles/1865_Wormhole.dir/main.cpp.obj"
+  "lib1865_Wormhole.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1865_Wormhole.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 1865_Wormhole/main.cpp
+++ 1865_Wormhole/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+vector<pair<int, int>> vec;
+int dist[501];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int tc; cin >> tc;
+    while(tc--) {
+        int n, m, w; cin >> n >> m >> w;
+        for(int i = 0; i < m + w; i++) {
+            int s, e, t; cin >> s >> e >> t;
+            
+        }
+    }
 }
