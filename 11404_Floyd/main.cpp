fc755cf8bf3a5df4904a12ef05f3550bcb6365df
skysaver00 <skysaver00@naver.com>
2021-07-27 17:23:30 +0900

    solved Gold IV

--- 11404_Floyd/main.cpp
+++ 11404_Floyd/main.cpp
@@ -6,11 +6,13 @@ long long arr[101][101];
 long long INF = 2147483647;
 
 void floyd() {
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            for(int k = 1; k <= n; k++) {
-                if(arr[i][k] + arr[k][j] < arr[i][j])
-                    arr[i][j] = arr[i][k] + arr[k][j];
+    for(int l = 0; l < n; l++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    if (arr[i][k] + arr[k][j] < arr[i][j])
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                }
             }
         }
     }
@@ -27,14 +29,15 @@ int main() {
 
     for(int i = 0; i < m; i++) {
         int a, b, c; cin >> a >> b >> c;
-        arr[a][b] = c;
+        if(arr[a][b] > c) arr[a][b] = c;
     }
 
     floyd();
 
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
-            cout << arr[i][j] << "\n";
+            if(arr[i][j] < INF) cout << arr[i][j] << " ";
+            else cout << 0 << " ";
         }cout <<"\n";
     }
 
