ffa25681fdd6e92dab0a61b80c31959ed407e9a6
skysaver00 <skysaver00@naver.com>
2021-07-27 16:59:44 +0900

    not solved

--- 11404_Floyd/main.cpp
+++ 11404_Floyd/main.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 using namespace std;
 
+int n, m;
 long long arr[101][101];
 long long INF = 2147483647;
 
+void floyd() {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            for(int k = 1; k <= n; k++) {
+                if(arr[i][k] + arr[k][j] < arr[i][j])
+                    arr[i][j] = arr[i][k] + arr[k][j];
+            }
+        }
+    }
+}
+
 int main() {
-    int n, m; cin >> n >> m;
+    cin >> n >> m;
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
             if(i == j) arr[i][j] = 0;
             else arr[i][j] = INF;
         }
     }
+
+    for(int i = 0; i < m; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        arr[a][b] = c;
+    }
+
+    floyd();
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cout << arr[i][j] << "\n";
+        }cout <<"\n";
+    }
+
+    return 0;
 }
