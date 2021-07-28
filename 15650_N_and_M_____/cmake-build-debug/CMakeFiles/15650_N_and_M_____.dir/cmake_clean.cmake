dba72ce2a68e6ab9f21a0c6457f9069c523b01c5
skysaver00 <skysaver00@naver.com>
2021-07-28 22:41:00 +0900

    not solved

---
+++ 15650_N_and_M_____/cmake-build-debug/CMakeFiles/15650_N_and_M_____.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15650_N_and_M_____.exe"
+  "15650_N_and_M_____.exe.manifest"
+  "15650_N_and_M_____.pdb"
+  "CMakeFiles/15650_N_and_M_____.dir/main.cpp.obj"
+  "lib15650_N_and_M_____.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15650_N_and_M_____.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 15650_N_and_M_____/main.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+
+int res[10];
+int visit[10];
+
+void combination(int idx, int cnt, int max, int size) {
+    if(cnt == size) {
+        for(int i = 0; i < max; i++) {
+            if(visit[i]) printf("%d ", res[i]);
+        }printf("\n");
+        return;
+    }
+
+    for(int i = idx; i < max; i++) {
+        if(visit[i]) continue;
+        visit[i] = true;
+        combination(i, cnt + 1, max, size);
+        visit[i] = false;
+    }
+}
+
+int main() {
+    int n, m; scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++) res[i] = i + 1;
+
+    combination(0, 0, n, m);
+    return 0;
+}
