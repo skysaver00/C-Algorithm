4f2cd7398eeaa2b3ac7e347811b2e069eacc1517
skysaver00 <skysaver00@naver.com>
2021-07-28 22:30:50 +0900

    not solved

--- 16395_Pascal_Triangle/main.cpp
+++ 16395_Pascal_Triangle/main.cpp
@@ -7,10 +7,17 @@ int main() {
     k -= 1, n -= 1;
 
     long long ans = 1;
-    for(int i = k; i > k - n; k--) {
-        k *= i;
-        
+    int dvd = 2;
+    for(int i = k; i >= k - n; k--) {
+        ans *= i;
+        cout << ans << " " << dvd << "\n";
+        if(i == k && i % 2 == 1) continue;
+        else {
+            ans /= dvd;
+            dvd++;
+        }
     }
 
+    cout << ans << "\n";
     return 0;
 }
