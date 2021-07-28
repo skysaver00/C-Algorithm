6e29a2f91bc3ed5a921d98183cc269978f16c3b4
skysaver00 <skysaver00@naver.com>
2021-07-28 22:26:33 +0900

    not solved

---
+++ 16395_Pascal_Triangle/cmake-build-debug/CMakeFiles/16395_Pascal_Triangle.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "16395_Pascal_Triangle.exe"
+  "16395_Pascal_Triangle.exe.manifest"
+  "16395_Pascal_Triangle.pdb"
+  "CMakeFiles/16395_Pascal_Triangle.dir/main.cpp.obj"
+  "lib16395_Pascal_Triangle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/16395_Pascal_Triangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- 16395_Pascal_Triangle/main.cpp
+++ 16395_Pascal_Triangle/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int k, n; cin >> k >> n;
+    k -= 1, n -= 1;
+
+    long long ans = 1;
+    for(int i = k; i > k - n; k--) {
+        k *= i;
+        
+    }
+
     return 0;
 }
