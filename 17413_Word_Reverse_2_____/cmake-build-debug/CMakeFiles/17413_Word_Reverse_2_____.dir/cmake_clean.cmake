7af20888944868068358274c864102febe5cd493
skysaver00 <skysaver00@naver.com>
2021-07-29 20:50:17 +0900

    solved Silver III

---
+++ 17413_Word_Reverse_2_____/cmake-build-debug/CMakeFiles/17413_Word_Reverse_2_____.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "17413_Word_Reverse_2_____.exe"
+  "17413_Word_Reverse_2_____.exe.manifest"
+  "17413_Word_Reverse_2_____.pdb"
+  "CMakeFiles/17413_Word_Reverse_2_____.dir/main.cpp.obj"
+  "lib17413_Word_Reverse_2_____.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/17413_Word_Reverse_2_____.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 17413_Word_Reverse_2_____/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stack>
+
+using namespace std;
+
+char word[100004];
+int main() {
+    scanf("%[^\n]", word);
+    stack<char> stack1;
+
+    word[strlen(word)] = '\n';
+
+    int flag = 0;
+    for(int i = 0; i < strlen(word); i++) {
+        if(word[i] == '<') {
+            while(!stack1.empty()) {
+                printf("%c", stack1.top());
+                stack1.pop();
+            }
+            printf("<");
+            flag = 1;
+        }
+        else if(word[i] == '>') {
+            printf(">");
+            flag = 0;
+        }
+        else if(flag == 1) printf("%c", word[i]);
+        else if(word[i] == ' ' || word[i] == '\n') {
+            while(!stack1.empty()) {
+                printf("%c", stack1.top());
+                stack1.pop();
+            }
+            printf(" ");
+        }
+        else {
+            stack1.push(word[i]);
+        }
+    }
+}
