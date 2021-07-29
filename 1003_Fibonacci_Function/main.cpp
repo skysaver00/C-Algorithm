67a0e38869c7f8635afa9246a3bf62168a283c5e
skysaver00 <skysaver00@naver.com>
2021-07-29 17:55:58 +0900

    solved Silver III

--- 1003_Fibonacci_Function/main.cpp
+++ 1003_Fibonacci_Function/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <stdio.h>
+int arr[40][2];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int t; scanf("%d", &t);
+    arr[0][0] = 1;
+    arr[1][1] = 1;
+    for(int i = 2; i <= 40; i++) {
+        arr[i][0] = arr[i - 1][0] + arr[i - 2][0];
+        arr[i][1] = arr[i - 1][1] + arr[i - 2][1];
+    }
+
+    while(t--) {
+        int n; scanf("%d", &n);
+        printf("%d %d\n", arr[n][0], arr[n][1]);
+    }
 }
