a682ecae15a146736866172dd2801e9cb46ebc9d
skysaver00 <skysaver00@naver.com>
2021-07-29 17:48:43 +0900

    not solved

---
+++ 9095_Add_1_2_3_____/cmake-build-debug/CMakeFiles/9095_Add_1_2_3_____.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "9095_Add_1_2_3_____.exe"
+  "9095_Add_1_2_3_____.exe.manifest"
+  "9095_Add_1_2_3_____.pdb"
+  "CMakeFiles/9095_Add_1_2_3_____.dir/main.cpp.obj"
+  "lib9095_Add_1_2_3_____.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9095_Add_1_2_3_____.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9095_Add_1_2_3_____/cmake-build-debug/CMakeFiles/9095_Add_1_2_3_____.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 9095_Add_1_2_3_____.
+# This may be replaced when dependencies are built.
