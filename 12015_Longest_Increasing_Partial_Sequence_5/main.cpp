e30484ea9a4a4475ee4e34cece0ac7662181f528
skysaver00 <skysaver00@naver.com>
2021-08-02 16:02:51 +0900

    not solved

---
+++ 12015_Longest_Increasing_Partial_Sequence_5/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int arr[1001];
+int check[1001];
+int ans[1001];
+int v[1001];
+
+int main() {
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        check[i] = 1;
+    }
+
+    for(int i = 2; i <= n; i++) {
+        for(int j = 1; j < i; j++) {
+            if(arr[i] > arr[j]) {
+                if(check[i] <= check[j]) {
+                    check[i] = check[j] + 1;
+                    v[i] = j;
+                }
+            }
+        }
+    }
+
+    int max = 0;
+    int maxLoc = 0;
+    for(int i = 1; i <= n; i++) {
+        if(max < check[i]) {
+            max = check[i];
+            maxLoc = i;
+        }
+    }
+
+    int d = max;
+    while(d >= 0) {
+        ans[d] = arr[maxLoc];
+        maxLoc = v[maxLoc];
+        d--;
+    }
+
+    cout << max << "\n";
+    for(int i = 1; i <= max; i++) {
+        cout << ans[i] << " ";
+    }cout << "\n";
+
+    return 0;
+}
