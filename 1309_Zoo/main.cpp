e88d326c258fbbfe2ab85a42089b00d1fe05dfb4
skysaver00 <skysaver00@naver.com>
2021-08-02 18:36:11 +0900

    solved Silver I

--- 1309_Zoo/main.cpp
+++ 1309_Zoo/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
-int zoo[100001][2];
+int zoo[100001][3];
 
 int main() {
     int n; cin >> n;
 
-    zoo[1][1] = 1;
+    zoo[1][2] = zoo[1][1] = zoo[1][0] = 1;
     for(int i = 2; i <= n; i++) {
-        zoo[i][1] += zoo[i - 1][1] + zoo[i - 1][0];
-        zoo[i][0] += zoo[i - 1][1] * 2 + zoo[i - 1][0];
+        zoo[i][2] += (zoo[i - 1][1] + zoo[i - 1][0]) % 9901;
+        zoo[i][1] += (zoo[i - 1][2] + zoo[i - 1][0]) % 9901;
+        zoo[i][0] += (zoo[i - 1][2] + zoo[i - 1][1] + zoo[i - 1][0]) % 9901;
     }
 
+    int ans = (zoo[n][0] + zoo[n][1] + zoo[n][2]) % 9901;
+    cout << ans << "\n";
     return 0;
 }
