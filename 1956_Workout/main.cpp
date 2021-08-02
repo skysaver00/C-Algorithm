09827f5a9f24fc35f99dba148419976e2c5613fe
skysaver00 <skysaver00@naver.com>
2021-08-03 01:26:29 +0900

    not solved

--- 1956_Workout/main.cpp
+++ 1956_Workout/main.cpp
@@ -15,6 +15,10 @@ void floyd(int v) {
 }
 
 int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int v, e; cin >> v >> e;
     for(int i = 1; i <= v; i++) {
         for(int j = 1; j <= v; j++) {
