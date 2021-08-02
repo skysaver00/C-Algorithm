5dbf2100e0573d74e52fdf59c90dcce2ea5cc0fe
skysaver00 <skysaver00@naver.com>
2021-08-02 17:29:43 +0900

    not solved

--- 14003_Longest_Increasing_Partial_Sequence_5/main.cpp
+++ 14003_Longest_Increasing_Partial_Sequence_5/main.cpp
@@ -1,51 +1,75 @@
 #include <iostream>
 using namespace std;
 
-int arr[1001];
-int check[1001];
-int ans[1001];
-int v[1001];
+int arr[1000001];
+int check[1000001];
+int check2[1000001];
+int ans[1000001];
+int v[1000001];
+int len = 0;
+int n;
+
+int solve(int i) {
+    int left = 0; int right = len - 1;
+    int mid;
+
+    int flag = 0;
+    while(left <= right) {
+        mid = (left + right) / 2;
+        if(check[mid] < arr[i]) left = mid + 1;
+        else if(check[mid] > arr[i]) right = mid - 1;
+        else if(check[mid] == arr[i]) {
+        }
+    }
+
+    cout << left << " " << mid << " " << right << " " << len << "\n";
+
+    if(left == len) {
+        check2[len] = check2[len - 1] + 1;
+        v[i] = mid;
+        check[len] = arr[i];
+        len++;
+    }
+    else if(check[left] > arr[i]) {
+        check2[i] = check[left - 1] + 1;
+        v[i] = mid;
+        check[left] = arr[i];
+    }
+
+    for(int k = 0; k <= n; k++) {
+        cout << check[k] << " || ";
+    }cout << "\n";
+
+    for(int k = 0; k <= n; k++) {
+        cout << check2[k] << " || ";
+    }cout << "\n";
+
+    for(int k = 0; k <= n; k++) {
+        cout << v[k] << " || ";
+    }cout << "\n";
+
+    return 0;
+}
 
 int main() {
-    int n; cin >> n;
+    cin >> n;
     for(int i = 1; i <= n; i++) {
         cin >> arr[i];
-        check[i] = 1;
+        check2[i] = 1;
     }
 
-
-
+    check[0] = -2000000000;
+    check[1] = arr[1];
+    len = 2;
     for(int i = 2; i <= n; i++) {
-        for(int j = 1; j < i; j++) {
-            if(arr[i] > arr[j]) {
-                if(check[i] <= check[j]) {
-                    check[i] = check[j] + 1;
-                    v[i] = j;
-                }
-            }
-        }
+        solve(i);
     }
 
-    int max = 0;
-    int maxLoc = 0;
-    for(int i = 1; i <= n; i++) {
-        if(max < check[i]) {
-            max = check[i];
-            maxLoc = i;
-        }
-    }
-
-    int d = max;
-    while(d >= 0) {
-        ans[d] = arr[maxLoc];
-        maxLoc = v[maxLoc];
-        d--;
-    }
-
-    cout << max << "\n";
-    for(int i = 1; i <= max; i++) {
-        cout << ans[i] << " ";
+    for(int k = 0; k <= len; k++) {
+        cout << check[k] << " || ";
     }cout << "\n";
+
+    cout << len - 1 << endl;
 
     return 0;
 }
