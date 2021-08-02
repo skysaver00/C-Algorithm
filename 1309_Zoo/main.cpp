fb7c2bce27b8dd18eb2eb8bff1630b45e9aad550
skysaver00 <skysaver00@naver.com>
2021-08-02 18:29:12 +0900

    not solveD

--- 1309_Zoo/main.cpp
+++ 1309_Zoo/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
-int zoo[100001];
+int zoo[100001][2];
 
 int main() {
     int n; cin >> n;
+
+    zoo[1][1] = 1;
+    for(int i = 2; i <= n; i++) {
+        zoo[i][1] += zoo[i - 1][1] + zoo[i - 1][0];
+        zoo[i][0] += zoo[i - 1][1] * 2 + zoo[i - 1][0];
+    }
 
     return 0;
 }
