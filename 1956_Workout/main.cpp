dd77cead19825e7cfdcb8c539b5c617a870fb495
skysaver00 <skysaver00@naver.com>
2021-08-03 01:25:58 +0900

    not solved

--- 1956_Workout/main.cpp
+++ 1956_Workout/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
-int vil[401][401];
+long long vil[401][401];
 long long INF = 2147483647;
+
+void floyd(int v) {
+    for(int i = 1; i <= v; i++) {
+        for(int j = 1; j <= v; j++) {
+            for(int k = 1; k <= v; k++) {
+                if(vil[i][j] + vil[j][k] < vil[i][k])
+                    vil[i][k] = vil[i][j] + vil[j][k];
+            }
+        }
+    }
+}
 
 int main() {
     int v, e; cin >> v >> e;
@@ -13,6 +24,15 @@ int main() {
 
     for(int i = 0; i < e; i++) {
         int a, b, c; cin >> a >> b >> c;
-        vil[a][b] = c;
+        if(vil[a][b] > c) vil[a][b] = c;
     }
+
+    floyd(v);
+
+    int ans = INF;
+    for(int i = 1; i <= v; i++) if(vil[i][i] < ans) ans = vil[i][i];
+    if(ans == INF) cout << "-1\n";
+    else cout << ans << "\n";
+
+    return 0;
 }
