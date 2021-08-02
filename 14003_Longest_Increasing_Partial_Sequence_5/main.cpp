9a8db23e9b71bc4c4bf271d46a7b75a604c11f2a
skysaver00 <skysaver00@naver.com>
2021-08-02 17:32:50 +0900

    not solved

--- 14003_Longest_Increasing_Partial_Sequence_5/main.cpp
+++ 14003_Longest_Increasing_Partial_Sequence_5/main.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int N;
-int Arr[1000001];
-int Index_Arr[1000001];
+int arr[1000001];
+int idx[1000001];
 vector<int> V;
 vector<int> Answer;
 
@@ -19,40 +19,40 @@ int main(void)
     cin >> N;
     for (int i = 1; i <= N; i++)
     {
-        cin >> Arr[i];
+        cin >> arr[i];
     }
 
     for (int i = 1; i <= N; i++)
     {
-        if (V.size() == 0 || V[V.size() - 1] < Arr[i])
+        if (V.size() == 0 || V[V.size() - 1] < arr[i])
         {
-            V.push_back(Arr[i]);
-            Index_Arr[i] = V.size() - 1;
+            V.push_back(arr[i]);
+            idx[i] = V.size() - 1;
         }
         else
         {
-            int Left = 0;
-            int Right = V.size() - 1;
-            while (Left < Right)
+            int left = 0;
+            int right = V.size() - 1;
+            while (left < right)
             {
-                int Mid = (Left + Right) / 2;
+                int mid = (left + right) / 2;
 
-                if (V[Mid] >= Arr[i]) Right = Mid;
-                else Left = Mid + 1;
+                if (V[mid] >= arr[i]) right = mid;
+                else left = mid + 1;
             }
-            V[Left] = Arr[i];
-            Index_Arr[i] = Left;
+            V[left] = arr[i];
+            idx[i] = left;
         }
     }
     cout << V.size() << "\n";
-    
+
     int Find_Index = V.size() - 1;
     for (int i = N; i > 0; i--)
     {
-        if (Index_Arr[i] == Find_Index)
+        if (idx[i] == Find_Index)
         {
             Find_Index--;
-            Answer.push_back(Arr[i]);
+            Answer.push_back(arr[i]);
         }
     }
     for (int i = Answer.size() - 1; i >= 0; i--) cout << Answer[i] << " ";
