e042b53d74844df84104af7e95b1d3173845dbba
skysaver00 <skysaver00@naver.com>
2021-08-03 15:13:13 +0900

    not solved

---
+++ 9012_Bracket_____2/cmake-build-debug/CMakeFiles/9012_Bracket_____2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "9012_Bracket_____2.exe"
+  "9012_Bracket_____2.exe.manifest"
+  "9012_Bracket_____2.pdb"
+  "CMakeFiles/9012_Bracket_____2.dir/main.cpp.obj"
+  "lib9012_Bracket_____2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9012_Bracket_____2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 9012_Bracket_____2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+string str;
+stack<char> stk;
+
+int main() {
+    int n; cin >> n;
+    while (n--) {
+
+    }
+
+     return 0;
+}
