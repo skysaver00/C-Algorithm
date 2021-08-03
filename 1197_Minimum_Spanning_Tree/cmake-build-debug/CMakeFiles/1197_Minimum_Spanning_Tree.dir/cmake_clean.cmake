3e166d1d67d5bfa490dc8360af03e25db6976924
skysaver00 <skysaver00@naver.com>
2021-08-03 16:13:12 +0900

    not solved

---
+++ 1197_Minimum_Spanning_Tree/cmake-build-debug/CMakeFiles/1197_Minimum_Spanning_Tree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1197_Minimum_Spanning_Tree.exe"
+  "1197_Minimum_Spanning_Tree.exe.manifest"
+  "1197_Minimum_Spanning_Tree.pdb"
+  "CMakeFiles/1197_Minimum_Spanning_Tree.dir/main.cpp.obj"
+  "lib1197_Minimum_Spanning_Tree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1197_Minimum_Spanning_Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1197_Minimum_Spanning_Tree/cmake-build-debug/CMakeFiles/1197_Minimum_Spanning_Tree.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1197_Minimum_Spanning_Tree.
+# This may be replaced when dependencies are built.
