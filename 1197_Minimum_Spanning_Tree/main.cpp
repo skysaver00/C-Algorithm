9c8d113c8218c08517bea5a5c3a6899f32b604a4
skysaver00 <skysaver00@naver.com>
2021-08-03 16:14:47 +0900

    not solveD

--- 1197_Minimum_Spanning_Tree/main.cpp
+++ 1197_Minimum_Spanning_Tree/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+vector<pair<int, int>> vec[10001];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int v, e; cin >> v >> e;
+    for(int i = 0; i < e; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        vec[a].push_back({b, c});
+    }
     return 0;
 }
