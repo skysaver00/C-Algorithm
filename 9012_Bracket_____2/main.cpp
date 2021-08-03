c82be7f18d1bcb60cbfb884950e8b4f40100a94b
skysaver00 <skysaver00@naver.com>
2021-08-03 15:18:11 +0900

    not solved

--- 9012_Bracket_____2/main.cpp
+++ 9012_Bracket_____2/main.cpp
@@ -9,7 +9,25 @@ stack<char> stk;
 int main() {
     int n; cin >> n;
     while (n--) {
+        cin >> str;
 
+        int len = str.length();
+        for(int i = 0; i < len; i++) {
+            if(str.at(i) == ')' && stk.empty()) {
+                cout << "NO\n";
+                continue;
+            } else if(str.at(i) == ')') {
+                stk.pop();
+            } else if(str.at(i) == '(') {
+                stk.push('(');
+            }
+        }
+
+        if(stk.empty()) cout << "YES\n";
+        else {
+            cout << "NO\n";
+            while(!stk.empty()) stk.pop();
+        }
     }
 
      return 0;
