76ce5d0cf6b12c0cddb4bd6595132da1cffa65dc
skysaver00 <skysaver00@naver.com>
2021-08-04 22:42:30 +0900

    not solved

--- 18352_Find_Certain_City_Distance/main.cpp
+++ 18352_Find_Certain_City_Distance/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> vec[300001];
+int arr[300001];
+
+void bfs(int x) {
+
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m, k, x; cin >> n >> m >> k >> x;
+
+    for(int i = 0; i < m; i++) {
+        int a, b; cin >> a >> b;
+        vec[a].push_back(b);
+    }
+
+    
+
     return 0;
 }
