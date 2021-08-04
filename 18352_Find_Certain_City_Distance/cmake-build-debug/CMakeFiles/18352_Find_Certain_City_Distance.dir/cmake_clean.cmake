283d06d3bf21b934a9a39dade1b487eaef013818
skysaver00 <skysaver00@naver.com>
2021-08-04 22:33:10 +0900

    not solved

---
+++ 18352_Find_Certain_City_Distance/cmake-build-debug/CMakeFiles/18352_Find_Certain_City_Distance.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "18352_Find_Certain_City_Distance.exe"
+  "18352_Find_Certain_City_Distance.exe.manifest"
+  "18352_Find_Certain_City_Distance.pdb"
+  "CMakeFiles/18352_Find_Certain_City_Distance.dir/main.cpp.obj"
+  "lib18352_Find_Certain_City_Distance.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/18352_Find_Certain_City_Distance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 18352_Find_Certain_City_Distance/cmake-build-debug/CMakeFiles/18352_Find_Certain_City_Distance.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 18352_Find_Certain_City_Distance.
+# This may be replaced when dependencies are built.
