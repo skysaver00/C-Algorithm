6366bfe955cb9269641ab8500e49658afaafa2c3
skysaver00 <skysaver00@naver.com>
2021-08-05 18:33:08 +0900

    solved Gold V

--- 13023_ABCDE/main.cpp
+++ 13023_ABCDE/main.cpp
@@ -3,25 +3,27 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> buddy[3001];
-bool check[3001];
-int depth[3001];
+vector<int> buddy[2001];
+bool check[2001];
+int cnt = 0;
 
-void dfs(int now) {
+void dfs(int now, int cnt) {
     if(check[now]) {
         return;
     }
-    check[now] = true;
+
+    if(cnt == 4) {
+        cout << "1\n";
+        exit(0);
+    }
 
     int sz = buddy[now].size();
+    check[now] = true;
     for(int i = 0; i < sz; i++) {
         if(check[buddy[now][i]]) continue;
-        depth[buddy[now][i]] = depth[now] + 1;
-
-        for(int j = 0; j < 10; j++) cout << depth[j] << " ";
-        cout << "\n";
-        dfs(buddy[now][i]);
+        dfs(buddy[now][i], cnt + 1);
     }
+    check[now] = false;
 }
 
 int main() {
@@ -35,17 +37,10 @@ int main() {
     //for(int i = 0; i < n; i++) sort(buddy[i].begin(), buddy[i].end());
 
     for(int i = 0; i <= n; i++) {
-        dfs(i);
-        for(int j = 0; j <= n; j++) {
-            if(depth[j] == 4) {
-                cout << "1\n";
-                return 0;
-            }
-        }
+        dfs(i, 0);
 
         for(int j = 0; j <= n; j++) {
             check[j] = false;
-            depth[j] = 0;
         }
     }
 
