4a9c6fd53563689baa4ab9bd3cc54003d88fef6f
skysaver00 <skysaver00@naver.com>
2021-08-05 11:26:33 +0900

    solved Silver II

--- 18352_Find_Certain_City_Distance/main.cpp
+++ 18352_Find_Certain_City_Distance/main.cpp
@@ -10,6 +10,7 @@ bool check[300001];
 
 void bfs(int x) {
     que.push(x);
+    check[x] = true;
 
     while(!que.empty()) {
         int from = que.front();
@@ -17,19 +18,21 @@ void bfs(int x) {
 
         int size = vec[from].size();
         for(int i = 0; i < size; i++) {
-            if(check[vec[from][i]]) {
+            if(!check[vec[from][i]]) {
                 dist[vec[from][i]] = dist[from] + 1;
                 que.push(vec[from][i]);
                 check[vec[from][i]] = true;
             }
         }
     }
-
-    return;
 }
 
 
 int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int n, m, k, x; cin >> n >> m >> k >> x;
 
     for(int i = 0; i < m; i++) {
@@ -39,9 +42,14 @@ int main() {
 
     bfs(x);
 
+    bool val = false;
     for(int i = 1; i <= n; i++) {
-        cout << dist[i] << " ";
-    }cout << "\n";
+        if(dist[i] == k) {
+            cout << i << "\n";
+            val = true;
+        }
+    }
 
+    if(!val) cout << "-1\n";
     return 0;
 }
