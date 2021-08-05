5b8f962c0d873c05223267700ad64997b07ad086
skysaver00 <skysaver00@naver.com>
2021-08-05 13:45:35 +0900

    solved Silver III

---
+++ 13023_ABCDE/cmake-build-debug/CMakeFiles/13023_ABCDE.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "13023_ABCDE.exe"
+  "13023_ABCDE.exe.manifest"
+  "13023_ABCDE.pdb"
+  "CMakeFiles/13023_ABCDE.dir/main.cpp.obj"
+  "lib13023_ABCDE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/13023_ABCDE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 13023_ABCDE/cmake-build-debug/CMakeFiles/13023_ABCDE.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 13023_ABCDE.
+# This may be replaced when dependencies are built.
