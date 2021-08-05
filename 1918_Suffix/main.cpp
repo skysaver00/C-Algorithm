69dd3b15b6b0e2585ff910a7ce81d51c830a1709
skysaver00 <skysaver00@naver.com>
2021-08-05 12:04:56 +0900

    not solved

--- 1918_Suffix/main.cpp
+++ 1918_Suffix/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
-int main()
-{
-    std::cout<<"Hello, World!"<<std::endl;
+stack<char> stk[101];
+string str;
+string postStr;
+
+int main() {
+    cin >> str;
+    int len = str.length();
+    int now = 0;
+
+    for(int i = 0; i < len; i++) {
+        if(str.at(i) == '(') {
+            now++;
+        } else if(str.at(i) == ')') {
+            while(!stk[now].empty()) {
+                postStr += stk[now].top();
+                stk[now].pop();
+            }
+            now--;
+        }
+
+        if(str.at(i) >= 'A' && str.at(i) <= 'Z') postStr += str.at(i);
+
+        if(str.at(i) == '+' || str.at(i) == '-' || str.at(i) == '*' || str.at(i) == '/') {
+            if(now == 0 && stk[now].size() != 0) {
+                while(!stk[now].empty()) {
+                    postStr += stk[now].top();
+                    stk[now].pop();
+                }
+            }
+            stk[now].push(str.at(i));
+        }
+    }
+    while(!stk[now].empty()) {
+        postStr += stk[now].top();
+        stk[now].pop();
+    }
+    cout << postStr << "\n";
     return 0;
 }
