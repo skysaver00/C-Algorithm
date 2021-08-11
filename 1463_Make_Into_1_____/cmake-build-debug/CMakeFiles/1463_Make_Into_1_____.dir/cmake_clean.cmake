a9e1f217bd7c914831b5bc95f09aa7b2eca5f941
skysaver00 <skysaver00@naver.com>
2021-08-12 00:37:12 +0900

    not solved

---
+++ 1463_Make_Into_1_____/cmake-build-debug/CMakeFiles/1463_Make_Into_1_____.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1463_Make_Into_1_____.exe"
+  "1463_Make_Into_1_____.exe.manifest"
+  "1463_Make_Into_1_____.pdb"
+  "CMakeFiles/1463_Make_Into_1_____.dir/main.cpp.obj"
+  "lib1463_Make_Into_1_____.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1463_Make_Into_1_____.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
