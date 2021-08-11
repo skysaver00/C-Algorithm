d7dc44d4b0477b14b2b88401e275bd33fd413eb2
skysaver00 <skysaver00@naver.com>
2021-08-11 21:29:20 +0900

    solved Silver I

--- 2156_Drinking_Wine_____/main.cpp
+++ 2156_Drinking_Wine_____/main.cpp
@@ -14,9 +14,7 @@ int main() {
     drink[1] = wine[1];
     drink[2] = wine[1] + wine[2];
 
-    drink[3] = max(wine[1] + wine[2] , max(wine[2] + wine[3], drink[2]));
-    for(int i = 4; i < n; i++) drink[i] = max(drink[i - 2] + wine[i], max(drink[i - 3] + wine[i - 1] + wine[i], drink[i - 1]));
-
-    for(int i = 1; i <= n; i++) cout << drink[i] << "\n";
+    for(int i = 3; i <= n; i++) drink[i] = max(drink[i - 2] + wine[i], max(drink[i - 3] + wine[i - 1] + wine[i], drink[i - 1]));
+    cout << drink[n] << "\n";
     return 0;
 }
