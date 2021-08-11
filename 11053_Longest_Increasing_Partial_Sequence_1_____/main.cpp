a9e006b73d6802901772ee699bf9681b55646ed4
skysaver00 <skysaver00@naver.com>
2021-08-11 22:06:32 +0900

    solved Silver II

--- 11053_Longest_Increasing_Partial_Sequence_1_____/main.cpp
+++ 11053_Longest_Increasing_Partial_Sequence_1_____/main.cpp
@@ -11,17 +11,18 @@ int main() {
     int n; cin >> n;
     for(int i = 0; i < n; i++) cin >> arr[i];
 
-    brr[0] = 1;
+    brr[0] = 0;
     for(int i = 1; i < n; i++) {
         for(int j = 0; j <= i; j++) {
             if(arr[i] > arr[j]) {
                 if(brr[j] + 1 > brr[i]) brr[i] = brr[j] + 1;
             }
         }
-
-        for(int j = 0; j <= i; j++) cout << brr[i] << " ";
-        cout << "\n";
     }
+
+    int ans = 0;
+    for(int i = 0; i < n; i++) if(ans < brr[i]) ans = brr[i];
+    cout << ans + 1 << "\n";
 
     return 0;
 }
