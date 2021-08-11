18177157bc99b5d946cd0cd548125ad3b7b661bc
skysaver00 <skysaver00@naver.com>
2021-08-11 20:35:13 +0900

    not solved

--- 1912_Continous_Sum_____/main.cpp
+++ 1912_Continous_Sum_____/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+using namespace std;
+int sum[100001];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n; cin >> n;
+
+    int node = 0;
+    bool ck = true;
+    for(int i = 0; i < n; i++) {
+        int val; cin >> val;
+        if(val >= 0 && ck == true) {
+            sum[node] += val;
+        } else if(val >= 0 && ck == false) {
+            node++;
+            ck = true;
+            sum[node] += val;
+        } else if(val <= 0 && ck == false) {
+            sum[node] += val;
+        } else {
+            node++;
+            ck = false;
+            sum[node] += val;
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << sum[i] << " ";
+    }cout << "\n";
+
     return 0;
 }
