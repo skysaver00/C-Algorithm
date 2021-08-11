9107007d36e26328fce65b9e51c9702a8644d900
skysaver00 <skysaver00@naver.com>
2021-08-12 00:46:10 +0900

    solved Silver III

--- 1463_Make_Into_1_____/main.cpp
+++ 1463_Make_Into_1_____/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
-
 int arr[1000001];
 
-int max(int i, int j) {
-    return (i > j) ? i : j;
+int min(int i, int j){
+    return (i < j) ? i : j;
 }
 
 int main() {
@@ -12,8 +11,8 @@ int main() {
 
     for(int i = n - 1; i >= 1; i--) {
         arr[i] = arr[i + 1] + 1;
-        if(i <= (n / 2)) arr[i] = arr[i * 2] + 1;
-        if(i <= (n / 3)) arr[i] = arr[i * 3] + 1;
+        if(i <= (n / 2)) arr[i] = min(arr[i * 2] + 1, arr[i]);
+        if(i <= (n / 3)) arr[i] = min(arr[i * 3] + 1, arr[i]);
     }
 
     cout << arr[1] << "\n";
