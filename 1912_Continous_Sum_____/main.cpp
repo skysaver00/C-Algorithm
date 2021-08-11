4df36c03ddb522779614a1936f0f30872bebe5ed
skysaver00 <skysaver00@naver.com>
2021-08-11 20:48:50 +0900

    not solved

--- 1912_Continous_Sum_____/main.cpp
+++ 1912_Continous_Sum_____/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-int sum[100001];
+int num[100001];
+int ans[100001];
 
 int main() {
     int n; cin >> n;
@@ -10,23 +11,42 @@ int main() {
     for(int i = 0; i < n; i++) {
         int val; cin >> val;
         if(val >= 0 && ck == true) {
-            sum[node] += val;
+            num[node] += val;
         } else if(val >= 0 && ck == false) {
             node++;
             ck = true;
-            sum[node] += val;
+            num[node] += val;
         } else if(val <= 0 && ck == false) {
-            sum[node] += val;
+            num[node] += val;
         } else {
+            if(i == 0) num[node] += val;
             node++;
             ck = false;
-            sum[node] += val;
+            num[node] += val;
         }
     }
 
     for(int i = 0; i < n; i++) {
-        cout << sum[i] << " ";
+        cout << num[i] << " ";
     }cout << "\n";
+
+    ans[0] = num[0];
+    for(int i = 1; i < n; i++) {
+        if(ans[i - 1] + num[i] > num[i]) ans[i] = ans[i - 1] + num[i];
+        else ans[i] = num[i];
+    }
+
+    for(int i = 0; i < n; i++) cout << ans[i] << " ";
+
+    int realAns = -1001;
+    for(int i = 0; i < n; i++) {
+        cout << ans[i] << "\n";
+        if(realAns < ans[i]) {
+            realAns = ans[i];
+            cout << 'flag!\n"';
+        }
+    }
+    cout << realAns << "\n";
 
     return 0;
 }
