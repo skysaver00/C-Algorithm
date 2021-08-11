1cc2b38f72cb1235e8a6b3363d1fea0bd87c317d
skysaver00 <skysaver00@naver.com>
2021-08-11 20:27:01 +0900

    not solved

---
+++ 1912_Continous_Sum_____/cmake-build-debug/CMakeFiles/1912_Continous_Sum_____.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1912_Continous_Sum_____.exe"
+  "1912_Continous_Sum_____.exe.manifest"
+  "1912_Continous_Sum_____.pdb"
+  "CMakeFiles/1912_Continous_Sum_____.dir/main.cpp.obj"
+  "lib1912_Continous_Sum_____.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1912_Continous_Sum_____.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
