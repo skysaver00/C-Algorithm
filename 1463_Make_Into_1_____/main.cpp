37e35bb002b4793407daf004777cb0f69927faf7
skysaver00 <skysaver00@naver.com>
2021-08-12 00:40:32 +0900

    not solved

--- 1463_Make_Into_1_____/main.cpp
+++ 1463_Make_Into_1_____/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
+
+int arr[1000001];
+
+int max(int i, int j) {
+    return (i > j) ? i : j;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n; cin >> n;
+
+    for(int i = n - 1; i >= 1; i--) {
+        arr[i] = arr[i + 1] + 1;
+        if(i <= (n / 2)) arr[i] = arr[i * 2] + 1;
+        if(i <= (n / 3)) arr[i] = arr[i * 3] + 1;
+    }
+
+    
     return 0;
 }
