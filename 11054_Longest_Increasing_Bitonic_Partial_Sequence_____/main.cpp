147e0a1224bbf394e5ebce04f5209b93bb9f38a0
skysaver00 <skysaver00@naver.com>
2021-08-11 22:11:02 +0900

    not solveD

--- 11054_Longest_Increasing_Bitonic_Partial_Sequence_____/main.cpp
+++ 11054_Longest_Increasing_Bitonic_Partial_Sequence_____/main.cpp
@@ -9,5 +9,18 @@ int main() {
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
+            if(arr[i] > arr[j]) {
+                if(inc[i] < inc[j] + 1) inc[i] = inc[j] + 1;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }cout << "\n";
+
     return 0;
 }
