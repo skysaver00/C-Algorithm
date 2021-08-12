7394f6d496ac60135c469330b02b0dff629eeb31
skysaver00 <skysaver00@naver.com>
2021-08-12 23:25:16 +0900

    not solved

---
+++ 1535_Hello_____/cmake-build-debug/CMakeFiles/1535_Hello_____.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1535_Hello_____.exe"
+  "1535_Hello_____.exe.manifest"
+  "1535_Hello_____.pdb"
+  "CMakeFiles/1535_Hello_____.dir/main.cpp.obj"
+  "lib1535_Hello_____.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1535_Hello_____.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1535_Hello_____/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int hello[21][100];
+
+int health[21];
+int happy[21];
+
+int max(int i , int j) {
+    return i > j ? i : j;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        cin >> health[i];
+    }
+
+    for(int i = 1; i <= n; i++) {
+        cin >> happy[i];
+    }
+
+    if(health[1] < 100) hello[1][health[1]] = happy[1];
+
+    for(int i = 2; i <= n; i++) {
+        if(health[i] < 100) hello[i][health[i]] = happy[i];
+        for(int j = 0; j < 100; j++) {
+            if(hello[i - 1][j] != 0) {
+                hello[i][j] = hello[i - 1][j];
+
+                if(j + health[i] < 100) {
+                    hello[i][j + health[i]] = hello[i - 1][j] + happy[i];
+                }
+            }
+        }
+    }
+
+    int biggest = 0;
+    for(int i = 0; i <= 20; i++) {
+        for(int j = 0; j <= 100; j++) {
+            if (hello[i][j] != 0) {
+                if (biggest < hello[i][j]) biggest = hello[i][j];
+            }
+        }
+    }
+
+    printf("%d\n", biggest);
+
+    return 0;
+}
