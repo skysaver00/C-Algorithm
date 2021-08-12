c1427b8546172f03fa99ad0934f6c6aeb21140d1
skysaver00 <skysaver00@naver.com>
2021-08-12 23:28:37 +0900

    solved Silver II

--- 1535_Hello_____/main.cpp
+++ 1535_Hello_____/main.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int N;
-int dp[22][103]; //i번째 사람의 j 체력일때의 행복 최댓값
+
+int dp[22][103];
 int happy[22];
 int health[22];
 
+int max(int i, int j) {
+    return (i > j) ? i : j;
+}
+
 int main() {
-    ios::sync_with_stdio(NULL);
-    cin.tie(NULL);
-    cin >> N;
+    int N; cin >> N;
     for (int i = 1; i <= N; i++) cin >> health[i];
     for (int i = 1; i <= N; i++) cin >> happy[i];
-    int maxnum = -1e8;
+
+    int ans = -1e9;
     for (int i = 1; i <= N; i++) {
         for (int j = 100; j >= 0; j--) {
             if (j - health[i] >= 1) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - health[i]] + happy[i]);
             else dp[i][j] = dp[i - 1][j];
-            maxnum = max(maxnum, dp[i][j]);
+            ans = max(ans, dp[i][j]);
         }
     }
-    cout << maxnum << "\n";
+    cout << ans << "\n";
 }
-[출처] [백준] 1535번 안녕 c++|작성자 류리
