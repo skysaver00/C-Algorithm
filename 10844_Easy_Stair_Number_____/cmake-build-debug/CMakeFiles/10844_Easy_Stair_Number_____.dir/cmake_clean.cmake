32c1f839df477038969d3c8fd732bc763dea93fb
skysaver00 <skysaver00@naver.com>
2021-08-12 23:47:06 +0900

    not solved

---
+++ 10844_Easy_Stair_Number_____/cmake-build-debug/CMakeFiles/10844_Easy_Stair_Number_____.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "10844_Easy_Stair_Number_____.exe"
+  "10844_Easy_Stair_Number_____.exe.manifest"
+  "10844_Easy_Stair_Number_____.pdb"
+  "CMakeFiles/10844_Easy_Stair_Number_____.dir/main.cpp.obj"
+  "lib10844_Easy_Stair_Number_____.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10844_Easy_Stair_Number_____.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 10844_Easy_Stair_Number_____/cmake-build-debug/CMakeFiles/10844_Easy_Stair_Number_____.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 10844_Easy_Stair_Number_____.
+# This may be replaced when dependencies are built.
