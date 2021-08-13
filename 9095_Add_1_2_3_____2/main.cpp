5f35bf73983d12b4faa2143a3bb8469fd21a8eb1
skysaver00 <skysaver00@naver.com>
2021-08-13 23:58:35 +0900

    solved Silver III

--- 9095_Add_1_2_3_____2/main.cpp
+++ 9095_Add_1_2_3_____2/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
+int arr[11];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n; cin >> n;
+    arr[1] = 1, arr[2] = 2, arr[3] = 4;
+    for(int i = 4; i <= 10; i++) arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1];
+
+    while(n--) {
+        int k; cin >> k;
+        cout << arr[k] << "\n";
+    }
+
     return 0;
 }
