5abb1afe78fb9b39d129ddc12ac616c0bbb98eee
skysaver00 <skysaver00@naver.com>
2021-08-14 00:27:48 +0900

    not solved

--- 10048_Moving/main.cpp
+++ 10048_Moving/main.cpp
@@ -8,26 +8,26 @@ int max(int i, int j) {
 }
 
 int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int n, m; cin >> n >> m;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) cin >> candy[i][j];
     }
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < 2 * n; i++) {
         for(int j = 0; j <= i; j++) {
+            if(i - j >= n || j >= m) continue;
             if((i - j) == 0 && j == 0) sum[i - j][j] = candy[i - j][j];
 
             if((i - j) == 0) sum[i - j][j] = sum[i - j][j - 1] + candy[i - j][j];
             else if(j == 0) sum[i - j][j] = sum[i - j - 1][j] + candy[i - j][j];
-            else {
-                sum[i - j][j] = max(sum[i - j][j - 1], max(sum[i - j - 1][j], sum[i - j - 1][j - 1])) + candy[i - j][j];
-            }
+            else sum[i - j][j] = max(sum[i - j][j - 1], max(sum[i - j - 1][j], sum[i - j - 1][j - 1])) + candy[i - j][j];
         }
     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) cout << sum[i][j] << " ";
-    }cout << "\n";
-
+    cout << sum[n - 1][m - 1] << "\n";
     return 0;
 }
