7e37b963c50fd72704a61814ecb5c6dabbb1dbf9
skysaver00 <skysaver00@naver.com>
2021-08-14 00:06:56 +0900

    not solved

--- 11051_Binomial_Coefficient_2/main.cpp
+++ 11051_Binomial_Coefficient_2/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<long long> triangle;
+vector<long long> triangle[1001];
 
 int main() {
     int n, k; cin >> n >> k;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(j == 0) triangle[i].push_back(1);
+        }
+    }
+
+    return 0;
 }
