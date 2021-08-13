f246cbece3dddac2f5a40ca2037d8d88f765b801
skysaver00 <skysaver00@naver.com>
2021-08-14 00:11:47 +0900

    solved Silver I

--- 11051_Binomial_Coefficient_2/main.cpp
+++ 11051_Binomial_Coefficient_2/main.cpp
@@ -5,11 +5,14 @@ vector<long long> triangle[1001];
 
 int main() {
     int n, k; cin >> n >> k;
-    for(int i = 1; i <= n; i++) {
+    for(int i = 1; i <= n + 1; i++) {
         for(int j = 0; j < i; j++) {
             if(j == 0) triangle[i].push_back(1);
+            else if(j == i - 1) triangle[i].push_back(1);
+            else if(i != 1) triangle[i].push_back((triangle[i - 1][j - 1] + triangle[i - 1][j]) % 10007);
         }
     }
+    cout << triangle[n + 1][k] << "\n";
 
     return 0;
 }
