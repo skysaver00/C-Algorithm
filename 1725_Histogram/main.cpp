3a1f10036cbb3ad6dfee7fe1edb4716d4c77c2da
skysaver00 <skysaver00@naver.com>
2021-08-15 01:22:31 +0900

    not solved

--- 1725_Histogram/main.cpp
+++ 1725_Histogram/main.cpp
@@ -9,24 +9,18 @@ long long solve(int left, int right) {
     if (left == right) return height[left];
     int mid = (left + right) / 2;
     long long ret = max(solve(left, mid), solve(mid + 1, right));
-    //cout << "RET..... " << ret << endl;
     int lo = mid, hi = mid + 1;
     long long hei = min(height[lo], height[hi]);
     ret = max(ret, hei * 2);
-    //cout << "RET HEI..... " << ret << " <> " << hei << endl;
 
     while (left < lo || hi < right) {
         if (hi < right && (lo == left || height[lo - 1] < height[hi + 1])) {
             ++hi;
             hei = min(hei, height[hi]);
-            //cout << "Up" << hi << " <> " << hei << endl;
         } else {
             --lo;
             hei = min(hei, height[lo]);
-            //cout << "Low" << hi << " <> " << hei << endl;
         }
-        //cout << "HI, LO is..... " << hi << " <> " << lo << endl;
-        //cout << "RET..... " << ret << endl;
         ret = max(ret, hei * (hi - lo + 1));
     }
     return ret;
@@ -44,6 +38,4 @@ int main() {
 
     long long ans = solve(0, n - 1);
     cout << ans << "\n";
-    height.clear();
-    
 }
