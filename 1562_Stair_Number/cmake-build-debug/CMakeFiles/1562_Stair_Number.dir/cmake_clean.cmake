f4473e920cb939726762ac7d085a6251fb7bfc7a
skysaver00 <skysaver00@naver.com>
2021-08-15 17:00:46 +0900

    not solved

---
+++ 1562_Stair_Number/cmake-build-debug/CMakeFiles/1562_Stair_Number.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1562_Stair_Number.exe"
+  "1562_Stair_Number.exe.manifest"
+  "1562_Stair_Number.pdb"
+  "CMakeFiles/1562_Stair_Number.dir/main.cpp.obj"
+  "lib1562_Stair_Number.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1562_Stair_Number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1562_Stair_Number/cmake-build-debug/CMakeFiles/1562_Stair_Number.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1562_Stair_Number.
+# This may be replaced when dependencies are built.
