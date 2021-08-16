fa756df9db3d4bca37dfe5b4b6be27b3bdf5e504
skysaver00 <skysaver00@naver.com>
2021-08-16 13:00:53 +0900

    not solved

---
+++ 16916_Partial_String/cmake-build-debug/CMakeFiles/16916_Partial_String.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "16916_Partial_String.exe"
+  "16916_Partial_String.exe.manifest"
+  "16916_Partial_String.pdb"
+  "CMakeFiles/16916_Partial_String.dir/main.cpp.obj"
+  "lib16916_Partial_String.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/16916_Partial_String.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
