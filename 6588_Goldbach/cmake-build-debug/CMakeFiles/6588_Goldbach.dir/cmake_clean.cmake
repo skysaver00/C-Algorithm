053e69ef35dd6ec1b1beaebaab2e5229036abb65
skysaver00 <skysaver00@naver.com>
2021-08-20 19:01:22 +0900

    not solved

---
+++ 6588_Goldbach/cmake-build-debug/CMakeFiles/6588_Goldbach.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "6588_Goldbach.exe"
+  "6588_Goldbach.exe.manifest"
+  "6588_Goldbach.pdb"
+  "CMakeFiles/6588_Goldbach.dir/main.cpp.obj"
+  "lib6588_Goldbach.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6588_Goldbach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 6588_Goldbach/cmake-build-debug/CMakeFiles/6588_Goldbach.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 6588_Goldbach.
+# This may be replaced when dependencies are built.
