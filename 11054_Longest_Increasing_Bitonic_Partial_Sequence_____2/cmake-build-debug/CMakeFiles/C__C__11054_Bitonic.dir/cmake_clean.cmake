489248ac127ee7080eb125f4225c442bae4a9743
skysaver00 <skysaver00@naver.com>
2021-08-21 19:22:53 +0900

    solved Gold III

---
+++ 11054_Longest_Increasing_Bitonic_Partial_Sequence_____2/cmake-build-debug/CMakeFiles/C__C__11054_Bitonic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__C__11054_Bitonic.dir/main.cpp.obj"
+  "C__C__11054_Bitonic.exe"
+  "C__C__11054_Bitonic.exe.manifest"
+  "C__C__11054_Bitonic.pdb"
+  "libC__C__11054_Bitonic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__C__11054_Bitonic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 11054_Longest_Increasing_Bitonic_Partial_Sequence_____2/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+int arr[1001];
+int inc[1001];
+int des[1001];
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
+            if(arr[i] > arr[j]) {
+                if(inc[i] < inc[j] + 1) inc[i] = inc[j] + 1;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) inc[i]++;
+
+    for(int i = n - 1; i >= 0; i--) {
+        for(int j = n - 1; j >= i; j--) {
+            if(arr[i] > arr[j]) {
+                if(des[i] < des[j] + 1) des[i] = des[j] + 1;
+            }
+        }
+    }
+
+    int ans = 0;
+    for(int i = 0; i < n; i++) if(ans < inc[i] + des[i]) ans = inc[i] + des[i];
+    cout << ans << "\n";
+
+    return 0;
+}
