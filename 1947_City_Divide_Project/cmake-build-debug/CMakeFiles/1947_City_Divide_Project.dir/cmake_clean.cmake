ade6779be55071b1265841eaffbf391fe502d720
skysaver00 <skysaver00@naver.com>
2021-08-21 19:46:24 +0900

    not solved

---
+++ 1947_City_Divide_Project/cmake-build-debug/CMakeFiles/1947_City_Divide_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1947_City_Divide_Project.exe"
+  "1947_City_Divide_Project.exe.manifest"
+  "1947_City_Divide_Project.pdb"
+  "CMakeFiles/1947_City_Divide_Project.dir/main.cpp.obj"
+  "lib1947_City_Divide_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1947_City_Divide_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1947_City_Divide_Project/cmake-build-debug/CMakeFiles/1947_City_Divide_Project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1947_City_Divide_Project.
+# This may be replaced when dependencies are built.
