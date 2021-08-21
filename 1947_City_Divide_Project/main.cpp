58e32c9561067e874a0514273fe28b0fd3c31ff1
skysaver00 <skysaver00@naver.com>
2021-08-21 21:47:03 +0900

    not solved

--- 1947_City_Divide_Project/main.cpp
+++ 1947_City_Divide_Project/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct edge {
+    int from;
+    int to;
+    int weight;
+};
+edge edge[100001];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m; cin >> n >> m;
+
+    for(int i = 0; i < m; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        edge[i].from = a;
+        edge[i].to = b;
+        edge[i].weight = c;
+    }
+
     return 0;
 }
