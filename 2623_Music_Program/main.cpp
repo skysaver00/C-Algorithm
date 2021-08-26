c9bb492ff9b6ca05238967b94aae91cb2a8735d9
skysaver00 <skysaver00@naver.com>
2021-08-26 14:57:23 +0900

    not solved

--- 2623_Music_Program/main.cpp
+++ 2623_Music_Program/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 using namespace std;
 vector<int> order[1001];
+int parent[1001];
+queue<int> topoQueue;
 
 int main() {
     int n, m; cin >> n >> m;
@@ -17,9 +19,15 @@ int main() {
             }
 
             order[from].push_back(to);
+            parent[to]++;
             from = to;
         }
     }
+
+    for(int i = 1; i <= n; i++)
+        if(parent[i] == 0) topoQueue.push(i);
+
+        
 
     return 0;
 }
