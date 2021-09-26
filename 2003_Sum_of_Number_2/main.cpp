c8f8aef6734f5297be42612d700df2bc2304f1c3
skysaver00 <skysaver00@naver.com>
2021-09-26 13:29:40 +0900

    solved Silver III

--- 2003_Sum_of_Number_2/main.cpp
+++ 2003_Sum_of_Number_2/main.cpp
@@ -1,34 +1,29 @@
 #include <stdio.h>
+
 int arr[10001];
 
 int main() {
-    int n, m; scanf("%d %d", &n, &m);
-    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
 
     int start, end;
     start = end = 0;
 
     int ans = 0;
     int sum = 0;
-    for(int i = 0; i < n; i++) {
-        if(sum == 0) {
+    while(end <= n) {
+        if (sum > m) {
+            sum -= arr[start];
+            start++;
+        } else if (sum < m) {
             sum += arr[end];
             end++;
+        } else {
+            ans++;
+            sum -= arr[start];
+            start++;
         }
-        else if(start <= end) {
-            if(sum > m) {
-                sum -= arr[start];
-                start++;
-            } else if(sum < m) {
-                sum += arr[end];
-                end++;
-            } else {
-                ans++;
-                sum += arr[end];
-                end++;
-            }
-        }
-        printf("%d %d %d\n", start, end, sum);
     }
 
     printf("%d\n", ans);
