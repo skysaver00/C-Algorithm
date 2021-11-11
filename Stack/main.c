12f40c0c888a8323ea51169afe53328b4353dea7
skysaver00 <skysaver00@naver.com>
2021-11-11 15:34:38 +0900

    2021-11-11

--- Stack/main.c
+++ Stack/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+int stack[100001];
 
 int main() {
-    printf("Hello, World!\n");
+    int now = -1;
+
+    while(1) {
+        char str[10];
+        printf("Push, Pop, Top, Exit: ");
+        scanf("%s", str);
+
+        if (str[0] == 'P' && str[1] == 'u') {
+            int value;
+            scanf("%d", &value);
+            if(now > 99999) printf("You cannot push. it's full\n");
+            else {
+                stack[++now] = value;
+            }
+        } else if (str[0] == 'P' && str[1] == 'o') {
+            if(now == -1) printf("You cannot pop. its empty\n");
+            else {
+                printf("%d popped.\n", stack[now--]);
+            }
+        } else if (str[0] == 'T' && str[1] == 'o') {
+            if(now == -1) printf("Empty\n");
+            else {
+                printf("%d\n", stack[now]);
+            }
+        } else if (str[0] == 'E' && str[1] == 'x') {
+            break;
+        } else {
+            printf("Cannot understand the formula.\n");
+        }
+    }
     return 0;
 }
